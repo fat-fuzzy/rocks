b3ba5dc2ba4e28d06b867818f06b3fa12297e479
patiboh <lapatiboh@gmail.com>
2022-03-13 20:03:28 +0100

    🚧 Migrate fixtures

---
+++ packages/resources/fixtures/index.js
@@ -0,0 +1,31 @@
+export default {
+	scenes: [
+		{
+			slug: 'random-rect',
+			name: 'Random',
+			emoji: '🎰',
+			duration: 2000
+		},
+		{
+			slug: 'random-rect-audio',
+			name: 'Audio',
+			emoji: '🥁',
+			audio: true,
+			duration: 4179,
+			playbackRate: 2
+		},
+		{
+			slug: '2d',
+			name: '2D',
+			emoji: '📐',
+			interactive: true,
+			webGlProps: null
+		},
+		{
+			slug: 'poop',
+			name: 'A Feature',
+			emoji: '💩',
+			type: 'test'
+		}
+	]
+}
