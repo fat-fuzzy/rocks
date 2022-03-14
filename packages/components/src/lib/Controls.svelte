9c99625d9d19436c96ba537266cc45abb7de9c5f
patiboh <lapatiboh@gmail.com>
2022-03-14 23:32:00 +0100

    🐛 Fix missing function error

--- packages/components/src/lib/Controls.svelte
+++ packages/components/src/lib/Controls.svelte
@@ -1,44 +1,49 @@
 <script>
-  import { onMount } from 'svelte'
+	import { onMount } from 'svelte';
 
-  import EmojiButton from './EmojiButton.svelte'
+	import EmojiButton from './EmojiButton.svelte';
 
-  export let play = () => {}
-  export let stop = () => {}
-  export let toggleSidebar = () => {}
-  export let showHandles
+	export let play = () => {};
+	export let stop = () => {};
+	export let toggleSidebar = () => {};
+	export let showHandles;
 
-  onMount(() => {
-    return () => {
-      uiStateUnsub()
-    }
-  })
+	let disabled = false;
+
+	onMount(() => {
+		return () => {
+			// uiStateUnsub()
+		};
+	});
 </script>
 
 <!-- handleBlur={playBlur} -->
 <div class="controls">
-  <EmojiButton
-    dataCy="btn-play"
-    buttonClass="play"
-    buttonLabel="👾 Play"
-    handleClick={() => play()}
-  />
-  <EmojiButton
-    dataCy="btn-stop"
-    buttonClass="stop"
-    buttonLabel="🏁 Stop"
-    handleClick={() => stop()}
-  />
-  {#if showHandles}
-    <EmojiButton
-      dataCy="btn-handles"
-      buttonClass="handles"
-      buttonLabel="🕹 Handles"
-      handleClick={() => toggleSidebar()}
-    />
-  {/if}
+	<EmojiButton
+		dataCy="btn-play"
+		buttonClass="play"
+		buttonLabel="👾 Play"
+		handleClick={() => play()}
+		{disabled}
+	/>
+	<EmojiButton
+		dataCy="btn-stop"
+		buttonClass="stop"
+		buttonLabel="🏁 Stop"
+		handleClick={() => stop()}
+		{disabled}
+	/>
+	{#if showHandles}
+		<EmojiButton
+			dataCy="btn-handles"
+			buttonClass="handles"
+			buttonLabel="🕹 Handles"
+			handleClick={() => toggleSidebar()}
+			{disabled}
+		/>
+	{/if}
 </div>
 
 <style lang="scss">
-  @import '../styles/components/controls.scss';
+	@import '../styles/components/controls.scss';
 </style>
