c108f0c73670b7885802511e3aba20e2804fecbb
patiboh <lapatiboh@gmail.com>
2022-03-19 19:51:53 +0100

    🚧 Use Storybook to manage component library

---
+++ packages/components/src/lib/toggle/Toggle.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { useMachine } from '@xstate/svelte';
+	import { createMachine } from 'xstate';
+
+	const toggleMachine = createMachine({
+		id: 'toggle',
+		initial: 'inactive',
+		states: {
+			inactive: {
+				on: { TOGGLE: 'active' }
+			},
+			active: {
+				on: { TOGGLE: 'inactive' }
+			}
+		}
+	});
+
+	const { state, send } = useMachine(toggleMachine);
+</script>
+
+<button on:click={() => send('TOGGLE')}>
+	{$state.value === 'inactive' ? 'Click to activate' : 'Active! Click to deactivate'}
+</button>
