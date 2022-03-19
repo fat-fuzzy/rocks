2e7c184c34330c518732a17a4aa5ef42b7d96056
patiboh <lapatiboh@gmail.com>
2022-03-19 23:20:31 +0100

    ✨ First story using state machine: Toggle

---
+++ packages/components/src/stories/Toggle.stories.svelte
@@ -0,0 +1,25 @@
+<script>
+	import {Meta, Template, Story} from '@storybook/addon-svelte-csf'
+	import Toggle from '../lib/toggle/Toggle.svelte'
+</script>
+
+<Meta
+	title="Toggle"
+	component={Toggle}
+	argTypes={{
+		label: {control: 'text'},
+		primary: {control: 'boolean'},
+		backgroundColor: {control: 'color'},
+		size: {
+			control: {type: 'select', options: ['small', 'medium', 'large']},
+		},
+		onClick: {action: 'onClick'},
+	}}
+/>
+
+<Template let:args>
+	<Toggle {...args} on:click={args.onClick} />
+</Template>
+
+<Story name="Labeled" args={{primary: true, label: 'Check me'}} />
+<Story name="Blank" args={{label: ''}} />
