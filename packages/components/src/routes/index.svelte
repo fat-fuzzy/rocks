edd79a632693990b9e4c513080c5f98bd5463883
patiboh <lapatiboh@gmail.com>
2022-03-20 11:42:07 +0100

    ✨ Add components library sandbox site

--- packages/components/src/routes/index.svelte
+++ packages/components/src/routes/index.svelte
@@ -1,11 +1,39 @@
-<script lang="ts">
-	import Playground from '$lib/Playground.svelte';
+<script context="module" lang="ts">
+	export const prerender = true
 </script>
 
-<main class="grid">
-	<Playground />
-</main>
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
 
-<style lang="scss" global>
-  @import '../styles/config/main.scss';
+<h1>Current Experiments</h1>
+
+<ul class="projects">
+	<li class="card">
+		<a sveltekit:prefetch href="/play">Play</a>
+	</li>
+	<li class="card">
+		<a sveltekit:prefetch href="/machines">Machines</a>
+	</li>
+</ul>
+
+<style lang="scss">
+	@import '../styles/config/main.scss';
+
+	.projects {
+		list-style: none;
+		padding-left: 0;
+		display: flex;
+
+		& > * + * {
+			margin-left: 0.2em;
+		}
+	}
+	.card {
+		min-width: calc(50% - 0.2em);
+		text-align: center;
+		padding: 2em 0;
+		border-radius: 3px;
+		border: 1px solid getVar(color-dark);
+	}
 </style>
