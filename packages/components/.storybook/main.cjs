4ea0257ed5eacec7681d18beb3f6f7dbbc6f8729
patiboh <lapatiboh@gmail.com>
2022-03-26 21:29:50 +0100

    🚧 Update config for sass in storybook

--- packages/components/.storybook/main.cjs
+++ packages/components/.storybook/main.cjs
@@ -1,8 +1,13 @@
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-svelte-csf'],
-	framework: '@storybook/svelte'
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/preset-scss',
+		'@storybook/addon-svelte-csf',
+	],
+	framework: '@storybook/svelte',
 	// "svelteOptions": {
 	//   "preprocess": require("../svelte.config.js").preprocess
 	// }
-};
+}
