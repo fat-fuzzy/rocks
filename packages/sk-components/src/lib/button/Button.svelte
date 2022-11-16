36e83971de047f625a03dd6856b019fdb948a234
patiboh <lapatiboh@gmail.com>
2022-11-16 07:42:18 +0100

    🚧 Test new SkelteKit project

---
+++ packages/sk-components/src/lib/button/Button.svelte
@@ -0,0 +1,23 @@
+<!-- <svelte:options accessors={true} /> -->
+<script>
+	// Inputs
+	export let size = 'md';
+	export let icon = '';
+	export let variant = 'primary';
+	export let testId = 'btn';
+	export let disabled = false;
+	export let type = 'button';
+
+	// Event Handlers
+	export let handleClick = () => {};
+
+	$: className = `${icon} ${size} ${variant}`;
+</script>
+
+<button data-test={testId} on:click={handleClick} class={className} {disabled} {type}>
+	<slot />
+</button>
+
+<style lang="scss">
+	@import '../../styles/blocks/button.scss';
+</style>
