b5a7cfc8c6c0acabedaba571ac9982d5f3395b77
patiboh <lapatiboh@gmail.com>
2022-11-18 11:21:43 +0100

    🐛 Fix imports

--- packages/ui/src/stores/gfx.ts
+++ packages/ui/src/stores/gfx.ts
@@ -1,6 +1,6 @@
 import {writable, readable} from 'svelte/store'
 
-import * as _animations from '../utils/gl/animations.js.js'
+import * as _animations from '../utils/gl/animations.js'
 
 export const animations = readable(_animations.animations)
 export const currentAnimationId = writable('random-rect')
