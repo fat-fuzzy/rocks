6fecec4497ae6a582787faf226c57113588c28de
patiboh <lapatiboh@gmail.com>
2022-11-20 15:26:02 +0100

    🚧 Restore UI lib Sandbox

---
+++ packages/ui/src/routes/+layout.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import {onMount} from 'svelte'
+	import {page} from '$app/stores'
+	import {themes} from '$lib/types/constants'
+	import {theme} from '$lib/stores/theme'
+	import Header from '$lib/blocks/header/Header.svelte'
+
+	let app
+	let currentTheme = themes[$theme]
+
+	function getClassNameFromUrl(url) {
+		return url.pathname === '/' ? 'home' : url.pathname.substr(1, url.pathname.length)
+	}
+	$: className = getClassNameFromUrl($page.url)
+
+	theme.subscribe((value) => {
+		if (app) {
+			app.classList.remove(currentTheme)
+		}
+		currentTheme = themes[value]
+		if (app) {
+			app.classList.add(currentTheme)
+		}
+	})
+
+	onMount(() => {
+		app = document.getElementById('app')
+	})
+</script>
+
+<Header className="header-app" page={$page} />
+
+<main class="l-wrapper {className}">
+	<slot />
+</main>
+
+<footer>
+	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+</footer>
+
+<style lang="scss" global>
+	@import '../lib/styles/main.scss';
+</style>
--- packages/ui/src/routes/+page.svelte
+++ packages/ui/src/routes/+page.svelte
@@ -1,3 +1,31 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import {theme} from '$lib/stores/theme'
+	let variant = ``
+
+	$: variant = $theme ? `accent` : `highlight`
+</script>
+
+<svelte:head>
+	<title>Sandbox</title>
+	<meta name="description" content="A sandbox for experimenting with web app interfaces." />
+</svelte:head>
+
+<header>
+	<h1>Welcome to the Fat Fuzzy UI library project Sandbox</h1>
+	<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the SvekteKit documentation</p>
+</header>
+
+<section class="l-wrapper u-side xxl">
+	<h2>✨ Highlights</h2>
+	<ul class="l-stack lg no-list">
+		<li class={`card ${variant}`}>
+			<div>👾</div>
+			<a data-sveltekit-prefetch href="/play"> Play</a>
+		</li>
+		<li class={`card ${variant}`}>
+			<div>🤖</div>
+			<a data-sveltekit-prefetch href="/machines"> Machines</a>
+		</li>
+	</ul>
+</section>
