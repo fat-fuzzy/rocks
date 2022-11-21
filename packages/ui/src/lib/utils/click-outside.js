bf412b98c0f9ee607a3edf32ee64afde2f69dfa0
patiboh <lapatiboh@gmail.com>
2022-11-21 23:06:21 +0100

    🐛 Test if env is browser before accessing document object

--- packages/ui/src/lib/utils/click-outside.js
+++ packages/ui/src/lib/utils/click-outside.js
@@ -1,3 +1,4 @@
+import {browser} from '$app/environment'
 /**
  * Source:
  * https://svelte.dev/repl/459e136de31f4769ad6fea8f32b30557?version=3.16.7
@@ -5,6 +6,9 @@
  *  Dispatch event on click outside of node
  */
 export function clickOutside(node) {
+	if (!browser) {
+		return
+	}
 	const handleClick = (event) => {
 		if (node && !node.contains(event.target) && !event.defaultPrevented) {
 			node.dispatchEvent(new CustomEvent('click_outside', node))
