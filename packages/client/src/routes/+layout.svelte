b9d72b27593138e2552d7db39f7903fa16d2b808
patiboh <lapatiboh@gmail.com>
2022-11-23 15:04:50 +0100

    🚧 Remove getVar() mixin - get closer to vanilla css

--- packages/client/src/routes/+layout.svelte
+++ packages/client/src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script>
-	import Header from './Header.svelte';
-	import './styles.css';
+	import Header from './Header.svelte'
 </script>
 
 <div class="app">
@@ -15,39 +14,6 @@
 	</footer>
 </div>
 
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
+<style lang="scss" global>
+	@import './styles.scss'; // TODO: get back to a css import
 </style>
