36c5bd4c18bacee21c4c9e4672871164eaf3ace4
patiboh <lapatiboh@gmail.com>
2022-11-28 16:57:38 +0100

    ✅ Check route param type (testing sveltekit options)

---
+++ packages/doc/src/routes/decisions/[id=integer]/+error.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { blocks, layouts } from '@fat-fuzzy/ui';
+	const { Sidebar } = layouts;
+	const { Menu } = blocks;
+
+	$: decisions = $page.data.decisions;
+</script>
+
+<svelte:head>
+	<title>FatFuzzy Doc | Decisions</title>
+	<meta name="description" content="Fat Fuzzy Doc - Log of architectural decisions" />
+</svelte:head>
+
+<Sidebar>
+	<div slot="side">
+		{#each decisions as { meta, path }}
+			<a data-sveltekit-prefetch href={path}>{meta.title}</a><br />
+		{/each}
+	</div>
+	<div slot="main" class="l-stack">
+		<h1>Error</h1>
+		<p>{$page.status}: {$page.error?.message}</p>
+	</div>
+</Sidebar>
