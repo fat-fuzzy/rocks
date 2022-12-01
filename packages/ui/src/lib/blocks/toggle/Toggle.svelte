ce9f8d826b8fb463a8e073085059f558cb50535a
patiboh <lapatiboh@gmail.com>
2022-12-01 09:58:35 +0100

    ✨ [ui] Style Toggle button

--- packages/ui/src/lib/blocks/toggle/Toggle.svelte
+++ packages/ui/src/lib/blocks/toggle/Toggle.svelte
@@ -6,10 +6,36 @@
 
 	export let size = 'md'
 	const {state, send} = useMachine(toggleMachine)
+	$: pressed = $state.value === 'active'
+	$: toggleClass = pressed ? 'outline accent' : 'primary accent' /* TODO: use aria state to style*/
+	$: stateClass = pressed ? 'primary accent' : 'outline accent' /* TODO: use aria state to style*/
 </script>
 
 <Fieldset slug="toggle" label="Toggle" {size}>
-	<button type="button" on:click={() => send('TOGGLE')} aria-label="Toggle button">
-		{$state.value === 'inactive' ? 'Click to activate' : 'Active! Click to deactivate'}
+	<button
+		type="button"
+		on:click={() => send('TOGGLE')}
+		aria-pressed={pressed}
+		class={`${toggleClass}`}
+	>
+		Toggle
+		<span class={`${stateClass}`}>{pressed ? 'ON' : 'OFF'} </span>
 	</button>
 </Fieldset>
+
+<style>
+	/* TODO  Move Toggle styles to lib */
+	button {
+		display: flex;
+		justify-content: space-evenly;
+		align-items: center;
+	}
+	button.primary:hover span {
+		color: var(--ui-color-white);
+		background-color: var(--ui-accent);
+	}
+	span {
+		padding: 0.25em 0.5em;
+		border-radius: var(--ui-radius-lg);
+	}
+</style>
