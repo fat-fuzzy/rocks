791804e4c57aac8ac4226bb6c5248d171191ba2d
patiboh <lapatiboh@gmail.com>
2022-12-21 23:15:53 +0100

    🔥 [doc/ui] Use Nav, remove unnecesary templates

--- packages/doc/src/routes/ui/+layout.svelte
+++ packages/doc/src/routes/ui/+layout.svelte
@@ -2,10 +2,28 @@
 	import { page } from '$app/stores';
 	import { blocks, layouts } from '@fat-fuzzy/ui';
 	const { Sidebar } = layouts;
-	const { Menu } = blocks;
+	const { Nav } = blocks;
 
 	$: components = $page.data.components;
-
+	$: path = '/ui';
+	$: items = [
+		{
+			slug: '',
+			title: 'Library',
+			items: [
+				{
+					slug: 'blocks',
+					title: 'Blocks',
+					items: components.blocks.map((c) => ({ slug: c.toLowerCase(), title: c }))
+				},
+				{
+					slug: 'layouts',
+					title: 'Layouts',
+					items: components.layouts.map((c) => ({ slug: c.toLowerCase(), title: c }))
+				}
+			]
+		}
+	];
 	// TODO: fix links
 </script>
 
@@ -16,15 +34,7 @@
 
 <Sidebar size="xs">
 	<svelte:fragment slot="side">
-		<a data-sveltekit-prefetch href={`/ui`}><h1>Library</h1></a><br />
-		<a data-sveltekit-prefetch href={`/ui/blocks`}><h2>Blocks</h2></a><br />
-		{#each components.blocks as block}
-			<a data-sveltekit-prefetch href={`/ui/blocks/${block}`}>{block}</a><br />
-		{/each}
-		<a data-sveltekit-prefetch href={`/ui/layouts`}><h2>Layouts</h2></a><br />
-		{#each components.layouts as layout}
-			<a data-sveltekit-prefetch href={`/ui/layouts/${layout}`}>{layout}</a><br />
-		{/each}
+		<Nav {items} {path} />
 	</svelte:fragment>
 	<div slot="main" class="l-stack l-text">
 		<slot />
