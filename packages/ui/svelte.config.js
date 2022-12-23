d96648843e8933d7e58747edc20e00276e0ce9dd
patiboh <lapatiboh@gmail.com>
2022-12-23 15:11:22 +0100

    🚧 [root/doc] Docker image of Doc app: prerendered pages OK

--- packages/ui/svelte.config.js
+++ packages/ui/svelte.config.js
@@ -4,10 +4,6 @@ import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	// preprocess: preprocess(),
-
 	kit: {
 		adapter: adapter({
 			fallback: 'index.html',
