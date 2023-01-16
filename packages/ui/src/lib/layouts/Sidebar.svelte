7d377ce4a2cf1ecfb90521516d659e6ef4d4302c
patiboh <lapatiboh@gmail.com>
2023-01-16 18:48:01 +0100

    💄 [ui] Adjust buttons, toggles, and layouts

--- packages/ui/src/lib/layouts/Sidebar.svelte
+++ packages/ui/src/lib/layouts/Sidebar.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	export let size = 'md'
 	export let placement = ''
+	export let theme = ''
 </script>
 
-<div class={`l-sidebar ${placement}`}>
+<div class={`l-sidebar ${placement} ${theme}`}>
 	<div class={`l-side ${size}`}>
 		<slot name="side"><!-- optional fallback --></slot>
 	</div>
--- packages/ui/src/lib/machines/toggle/machineConfig.ts
+++ packages/ui/src/lib/machines/toggle/machineConfig.ts
@@ -1,16 +1,1 @@
-import {createMachine} from 'xstate'
-const machineConfig = {
-	// todo : fix type
-	id: 'toggle',
-	initial: 'inactive',
-	states: {
-		inactive: {
-			on: {TOGGLE: 'active'},
-		},
-		active: {
-			on: {TOGGLE: 'inactive'},
-		},
-	},
-}
-const machine: any = createMachine(machineConfig)
 export default machine
