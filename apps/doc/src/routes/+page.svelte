dd047a135ba700160d080c9cc9f8c3244ae6365c
patiboh <lapatiboh@gmail.com>
2023-01-23 01:56:30 +0100

    🐛 [doc] Html fixes

--- apps/doc/src/routes/+page.svelte
+++ apps/doc/src/routes/+page.svelte
@@ -7,23 +7,21 @@
 	<meta name="description" content="Fat Fuzzy Rocks documentation home page" />
 </svelte:head>
 
-<section class="l-text:xl l-center">
-	<header>
-		<h1>Fuzzy Rocks</h1>
-		<h2>Welcome to the Fat Fuzzy Doc !</h2>
-	</header>
+<header>
+	<h1>Fat Fuzzy Rocks</h1>
+	<p>Welcome to the Fat Fuzzy Doc !</p>
+</header>
 
-	<section class="l-text:md l-center">
-		<h2>✨ Highlights</h2>
-		<ul class="l-stack xl" role="group">
-			<li class={`card ${variant}`}>
-				<span>🌈</span>
-				<a data-sveltekit-preload-data href="/ui">UI Library</a>
-			</li>
-			<li class={`card ${variant}`}>
-				<span>💡</span>
-				<a data-sveltekit-preload-data href="/decisions">Decisions</a>
-			</li>
-		</ul>
-	</section>
+<section class="l-text:md l-center">
+	<h2>✨ Highlights</h2>
+	<ul class="l-stack xl" role="group">
+		<li class={`card ${variant}`}>
+			<span>🌈</span>
+			<a data-sveltekit-preload-data href="/ui">UI Library</a>
+		</li>
+		<li class={`card ${variant}`}>
+			<span>💡</span>
+			<a data-sveltekit-preload-data href="/decisions">Decisions</a>
+		</li>
+	</ul>
 </section>
