703b3a9c04c1151e55aa067ec4c227250af77486
patiboh <lapatiboh@gmail.com>
2023-01-28 12:37:38 +0100

    💄 [ui/styles] More styles cleanup & doc

--- packages/ui/src/routes/play/+layout.server.ts
+++ packages/ui/src/routes/play/+layout.server.ts
@@ -2,9 +2,6 @@ import {sketches} from '$data/sketches'
 import type {LayoutLoad} from './$types'
 
 export const load: LayoutLoad = () => {
-	console.log('LayoutLoad - sketches')
-	console.log(sketches)
-
 	return {
 		sketches,
 	}
