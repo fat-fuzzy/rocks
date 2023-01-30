ca844531ffd35be81452eabe7dacb9dfc397ca66
patiboh <lapatiboh@gmail.com>
2023-01-31 00:28:17 +0100

    🚧 [ui] Poking at web components

--- apps/doc/svelte.config.js
+++ apps/doc/svelte.config.js
@@ -4,6 +4,16 @@ import mdsvexConfig from './mdsvex.config.js'
 import adapter from '@sveltejs/adapter-cloudflare'
 import preprocess from 'svelte-preprocess'
 
+function isWebComponentSvelte(code) {
+	const svelteOptionsIdx = code.indexOf('<svelte:options ')
+	if (svelteOptionsIdx < 0) {
+		return false
+	}
+	const tagOptionIdx = code.indexOf('tag=', svelteOptionsIdx)
+	const svelteOptionsEndIdx = code.indexOf('>', svelteOptionsIdx)
+	return tagOptionIdx > svelteOptionsIdx && tagOptionIdx < svelteOptionsEndIdx
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
@@ -23,6 +33,18 @@ const config = {
 	},
 	build: {
 		target: 'esnext',
+	},
+	vitePlugin: {
+		// To compile WC on the go: https://github.com/sveltejs/vite-plugin-svelte/issues/270#issuecomment-1033190138
+		experimental: {
+			dynamicCompileOptions({code}) {
+				if (isWebComponentSvelte(code)) {
+					return {
+						customElement: true,
+					}
+				}
+			},
+		},
 	},
 }
 
---
+++ packages/ui/src/lib/api/StyleCapsuleWebsite.wc.svelte
@@ -0,0 +1,30 @@
+<svelte:options tag="style-capsule-website" />
+
+<script>
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+
+	let styles
+	let styleElement
+
+	onMount(
+	async()=>{
+		styles = await import('../styles/app/client/_main.scss?inline')
+		if (browser) {
+			styleElement = document.createElement('style')
+			styleElement.textContent = String(styles.default)
+		}
+	})
+</script>
+
+
+{#if styles && styleElement}
+	<div id="style-this-content">
+		<button class="primary">Test Inside Style Capsule UI</button>	
+		<slot/>
+	</div>
+{/if}
+
+{#if styleElement}
+{@html styleElement}
+{/if}
