a0bfeac113d021a60af5db34636a78a4ec1f0e1b
patiboh <lapatiboh@gmail.com>
2023-02-07 15:33:50 +0100

    ⚗️ [client] Update client design tokens - fonts, color, scale

--- apps/client/src/routes/+page.svelte
+++ apps/client/src/routes/+page.svelte
@@ -1,7 +1,5 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script lang="ts">
+	const variant = 'accent'
 </script>
 
 <svelte:head>
@@ -9,51 +7,21 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+<header>
+	<h1>Fat Fuzzy Client</h1>
+	<p>Welcome to the Fat Fuzzy Doc !</p>
+</header>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+<section class="l:text:md l:center">
+	<h2>✨ Highlights</h2>
+	<ul class="l:stack xl" role="group">
+		<li class={`card:lg font:xl ${variant}`}>
+			<span>🌈</span>
+			<a data-sveltekit-preload-data href="/about">About</a>
+		</li>
+		<li class={`card:lg font:xl ${variant}`}>
+			<span>💡</span>
+			<a data-sveltekit-preload-data href="/play">Play</a>
+		</li>
+	</ul>
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
