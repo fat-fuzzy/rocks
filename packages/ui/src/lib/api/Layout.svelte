6f34433e1d19e47709ef941c6f13932ecfb5fbf5
patiboh <lapatiboh@gmail.com>
2023-02-10 17:19:05 +0100

    🚧 [ui/api] Fix reactivity of layout elements to UI api inputs

--- packages/ui/src/lib/api/Layout.svelte
+++ packages/ui/src/lib/api/Layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import type {ComponentType} from 'svelte'
-	import type {StyleFamily} from './options'
 	import {selectedStore} from '../stores/api'
 	import mocks from '../../data/mocks'
 
@@ -8,21 +7,11 @@
 	export let isPage = false
 	export let component: ComponentType
 
-	let selected: any
-	selectedStore.subscribe((data) => {
-		selected = data
-	})
-	const getFamilyOptionValue = (styleFamily: StyleFamily, styleOption: string) => {
-		// TODO: filter include / exclude in here
-		return typeof styleFamily !== 'string' ? styleFamily[styleOption] : ''
-	}
-	// TODO: improve this code - make it easier to understand ! (use store ?)
-
 	$: selected = $selectedStore
-	$: theme = $selected.settings && getFamilyOptionValue($selected.settings, 'theme')
-	$: content = $selected.content && getFamilyOptionValue($selected.content, 'content')
-	$: sideContent = $selected.content && getFamilyOptionValue($selected.content, 'side')
-	$: mainContent = $selected.content && getFamilyOptionValue($selected.content, 'main')
+	$: theme = $selected.theme
+	$: content = $selected.content?.name ?? 'card'
+	$: sideContent = $selected.side?.name ?? 'card'
+	$: mainContent = $selected.main?.name ?? 'text'
 </script>
 
 {#if !isPage}
@@ -36,7 +25,7 @@
 		{/if}
 	</svelte:component>
 {:else if title === 'Sidebar'}
-	<svelte:component this={component} id={title} {...selected}>
+	<svelte:component this={component} id={title} {...$selected}>
 		<div slot="side">
 			{#if sideContent === 'text'}
 				{mocks[sideContent]}
@@ -57,7 +46,7 @@
 		</div>
 	</svelte:component>
 {:else}
-	<svelte:component this={component} id={title} {...selected}>
+	<svelte:component this={component} id={title} {...$selected}>
 		{#if content === 'text'}
 			{mocks[content]}
 		{:else if content === 'card' || content === 'form'}
