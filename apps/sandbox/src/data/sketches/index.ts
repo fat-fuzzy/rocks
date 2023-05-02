4f1cb547c2ddc5a415b8f51a6fd0847058de9c7b
patiboh <lapatiboh@gmail.com>
2023-05-02 15:05:56 +0200

    🏗️  [sandbox] Rename client > sandbox

--- apps/sandbox/src/data/sketches/index.ts
+++ apps/sandbox/src/data/sketches/index.ts
@@ -1,30 +1,30 @@
 export const SKETCHES_FOLDER = './sketches'
 export const sketches = [
-	// {
-	// 	id: '001',
-	// 	slug: 'random-rect',
-	// 	title: 'Random',
-	// 	emoji: '🎰',
-	// 	dimensions: 'video',
-	// },
+	{
+		id: '001',
+		slug: 'random-rect',
+		title: 'Random',
+		emoji: '🎰',
+		dimensions: 'video',
+	},
 	// {
 	// 	id: '002',
 	// 	slug: 'random-rect-audio',
 	// 	title: 'Audio',
 	// 	emoji: '🥁',
 	// },
-	// {
-	// 	id: '003',
-	// 	slug: 'rectangle-2d',
-	// 	title: 'Rectangle 2D ',
-	// 	emoji: '📐',
-	// },
-	// {
-	// 	id: '004',
-	// 	slug: 'geometry-2d',
-	// 	title: 'Geometry 2D ',
-	// 	emoji: '📐',
-	// },
+	{
+		id: '003',
+		slug: 'rectangle-2d',
+		title: 'Rectangle 2D ',
+		emoji: '📐',
+	},
+	{
+		id: '004',
+		slug: 'geometry-2d',
+		title: 'Geometry 2D ',
+		emoji: '📐',
+	},
 	// {
 	// 	id: '005',
 	// 	slug: 'matrix-2d',
