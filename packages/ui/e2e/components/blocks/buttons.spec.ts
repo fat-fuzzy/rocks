a7d13cdebac44808ecbff958c48a45aeabc1feb2
patiboh <lapatiboh@gmail.com>
2023-10-13 18:11:44 +0200

    ✅ [ui] Test: blocks/buttons/toggle basic tests

--- packages/ui/e2e/components/blocks/buttons.spec.ts
+++ packages/ui/e2e/components/blocks/buttons.spec.ts
@@ -1,6 +1,8 @@
 import {test, expect} from '@playwright/experimental-ct-svelte'
 import Button from '../../../dist/components/blocks/buttons/Button.svelte'
 import ButtonMenu from '../../../dist/components/blocks/buttons/ButtonMenu.svelte'
+import Toggle from '../../../dist/components/blocks/buttons/Toggle.svelte'
+import ToggleMenu from '../../../dist/components/blocks/buttons/ToggleMenu.svelte'
 
 test.use({viewport: {width: 500, height: 500}})
 
@@ -11,5 +13,15 @@ test('Button', async ({mount}) => {
 
 test('ButtonMenu', async ({mount}) => {
 	const component = await mount(ButtonMenu)
-	await expect(component).toHaveText('👾 Vader 🐙 Ollie 🐳 Moby ')
+	await expect(component).toHaveText('👾 Vader 🐙 Ollie 🐳 Moby')
+})
+
+test('Toggle', async ({mount}) => {
+	const component = await mount(Toggle)
+	await expect(component).toHaveText('❤️ Favorite')
+})
+
+test('ToggleMenu', async ({mount}) => {
+	const component = await mount(ToggleMenu)
+	await expect(component).toHaveText('💡 Idea 🦁 Profile ❤️ Favorite')
 })
