203fb1d9fecc507efedb4f2b61f125539b0fb993
patiboh <lapatiboh@gmail.com>
2023-10-14 23:37:14 +0200

    🚧 [ui] Use fixtures for api & tests, decouple from components

--- packages/ui/e2e/components/blocks/buttons.spec.ts
+++ packages/ui/e2e/components/blocks/buttons.spec.ts
@@ -1,27 +1,31 @@
 import {test, expect} from '@playwright/experimental-ct-svelte'
 import format from '../../../dist/utils/format'
-import mocks from '../../../dist/api/fixtures/js/buttons'
+import {getProps} from '../../../dist/api/fixtures/js/fixtures-api'
 
 import Button from '../../../dist/components/blocks/buttons/Button.svelte'
 import ButtonMenu from '../../../dist/components/blocks/buttons/ButtonMenu.svelte'
 import Toggle from '../../../dist/components/blocks/buttons/Toggle.svelte'
 import ToggleMenu from '../../../dist/components/blocks/buttons/ToggleMenu.svelte'
 
-const {button, buttonMenu, toggle, toggleMenu} = mocks
 test.use({viewport: {width: 500, height: 500}})
+
+const category = 'blocks'
 
 test('Button', async ({mount}) => {
 	// 1. Init
 	let clicked = false
+	const props = getProps({category, component: 'Button'})
 	const component = await mount(Button, {
 		props: {
+			...props,
 			onClick: () => {
 				clicked = true
 			},
 		},
 	})
+
 	// 2. Test display
-	const label = format.formatLabel(button.text, button.asset)
+	const label = format.formatLabel(props.text, props.asset)
 	await expect(component).toHaveText(label)
 
 	// 3. Test behaviour
@@ -31,10 +35,11 @@ test('Button', async ({mount}) => {
 
 test('ButtonMenu', async ({mount}) => {
 	// 1. Init
-	let clicked = false
-	const component = await mount(ButtonMenu)
+	const props = getProps({category, component: 'ButtonMenu'})
+	const component = await mount(ButtonMenu, {props})
+
 	// 2. Test display
-	const labels = buttonMenu.map((button) => format.formatLabel(button.text, button.asset))
+	const labels = props.items.map((button) => format.formatLabel(button.text, button.asset))
 	await expect(component).toHaveText(labels.join(' '))
 
 	// 3. Test behaviour
@@ -44,10 +49,11 @@ test('ButtonMenu', async ({mount}) => {
 
 test('Toggle', async ({mount}) => {
 	// 1. Init
-	const component = await mount(Toggle)
+	const props = getProps({category, component: 'Toggle'})
+	const component = await mount(Toggle, {props})
 
 	// 2. Test display
-	const label = format.formatLabel(toggle.text, toggle.asset)
+	const label = format.formatLabel(props.text, props.asset)
 	await expect(component).toHaveText(label)
 
 	// 3. Test behaviour
@@ -55,10 +61,12 @@ test('Toggle', async ({mount}) => {
 })
 
 test('ToggleMenu', async ({mount}) => {
-	const component = await mount(ToggleMenu)
+	// 1. Init
+	const props = getProps({category, component: 'ToggleMenu'})
+	const component = await mount(ToggleMenu, {props})
 
 	// 2. Test display
-	const labels = toggleMenu.map((button) => format.formatLabel(button.text, button.asset))
+	const labels = props.items.map((button) => format.formatLabel(button.text, button.asset))
 	await expect(component).toHaveText(labels.join(' '))
 
 	// 3. Test behaviour
