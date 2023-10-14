41897e03002d5a92c481784c5981e3ca68c48c7d
patiboh <lapatiboh@gmail.com>
2023-10-14 16:37:31 +0200

    🌱 [ui] Buttons: fix component api, improve tests

--- packages/ui/e2e/components/blocks/buttons.spec.ts
+++ packages/ui/e2e/components/blocks/buttons.spec.ts
@@ -1,40 +1,66 @@
 import {test, expect} from '@playwright/experimental-ct-svelte'
-import mocks from '../../../dist/data/mocks'
 import format from '../../../dist/utils/format'
+import mocks from '../../../dist/api/fixtures/js/buttons'
 
 import Button from '../../../dist/components/blocks/buttons/Button.svelte'
 import ButtonMenu from '../../../dist/components/blocks/buttons/ButtonMenu.svelte'
 import Toggle from '../../../dist/components/blocks/buttons/Toggle.svelte'
 import ToggleMenu from '../../../dist/components/blocks/buttons/ToggleMenu.svelte'
 
-const {button, menu, toggle, toggleMenu} = mocks
-
+const {button, buttonMenu, toggle, toggleMenu} = mocks
 test.use({viewport: {width: 500, height: 500}})
 
-test('Button', async ({mount, page}) => {
-	const component = await mount(Button)
+test('Button', async ({mount}) => {
+	// 1. Init
+	let clicked = false
+	const component = await mount(Button, {
+		props: {
+			onClick: () => {
+				clicked = true
+			},
+		},
+	})
+	// 2. Test display
 	const label = format.formatLabel(button.text, button.asset)
-
 	await expect(component).toHaveText(label)
+
+	// 3. Test behaviour
+	await component.click()
+	expect(clicked).toBeTruthy()
 })
 
 test('ButtonMenu', async ({mount}) => {
+	// 1. Init
+	let clicked = false
 	const component = await mount(ButtonMenu)
-	const labels = menu.map((button) => format.formatLabel(button.text, button.asset)).join(' ')
+	// 2. Test display
+	const labels = buttonMenu.map((button) => format.formatLabel(button.text, button.asset))
+	await expect(component).toHaveText(labels.join(' '))
 
-	await expect(component).toHaveText(labels)
-	component.getByRole('button')
+	// 3. Test behaviour
+	await component.getByText(labels[0]).click()
+	// expect(clicked).toBeTruthy() // TODO: fix
 })
 
 test('Toggle', async ({mount}) => {
+	// 1. Init
 	const component = await mount(Toggle)
+
+	// 2. Test display
 	const label = format.formatLabel(toggle.text, toggle.asset)
 	await expect(component).toHaveText(label)
+
+	// 3. Test behaviour
+	// TODO
 })
 
 test('ToggleMenu', async ({mount}) => {
 	const component = await mount(ToggleMenu)
-	const labels = toggleMenu.map((button) => format.formatLabel(button.text, button.asset)).join(' ')
 
-	await expect(component).toHaveText(labels)
+	// 2. Test display
+	const labels = toggleMenu.map((button) => format.formatLabel(button.text, button.asset))
+	await expect(component).toHaveText(labels.join(' '))
+
+	// 3. Test behaviour
+	// TODO
 })
