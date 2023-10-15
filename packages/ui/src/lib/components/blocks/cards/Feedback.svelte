7ee070e5d60d1e2dd7f8ec72b779febd04a04b13
patiboh <lapatiboh@gmail.com>
2023-10-15 10:34:39 +0200

    ✨ [ui] Add Feedback block

--- packages/ui/src/lib/components/blocks/cards/Feedback.svelte
+++ packages/ui/src/lib/components/blocks/cards/Feedback.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
-	// UI feedback
-	export let feedback = ''
-	export let show = false
+	export let text = ''
+	export let variant = ''
+	export let context = '' // code (stacktraces), form (results), dialog (alerts)
+	export let action = ''
+	// export let show = false
 
-	$: feedbackClass = show ? 'feedback' : 'hide:rm-block'
+	// $: feedbackClass = show ? 'feedback variant' : 'hide:rm-block'
 </script>
 
-<div class={feedbackClass} data-state="" data-test="feedback">
-	<pre class="stacktrace" data-test="stacktrace">{feedback}</pre>
-</div>
+{#if context === 'code'}
+	<div class={variant} data-test={`feedback-code-${variant}`}>
+		<pre>{text}</pre>
+	</div>
+{:else if context === 'form'}
+	<output class={variant} data-test={`feedback-form-${variant}`}>
+		{text}
+	</output>
+{:else if context === 'dialog'}
+	<dialog class={variant} data-test={`feedback-dialog-${variant}`}>
+		<p>{text}</p>
+		<form method="dialog">
+			<button>{action}</button>
+		</form>
+	</dialog>
+{/if}
