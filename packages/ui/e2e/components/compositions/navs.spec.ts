e3af5709745719c2e5a66728dcb05a0efbab0d51
patiboh <lapatiboh@gmail.com>
2023-10-15 13:25:27 +0200

    ✅ [ui] Test: composition/navs basic tests

---
+++ packages/ui/e2e/components/compositions/navs.spec.ts
@@ -0,0 +1,88 @@
+import {test, expect} from '@playwright/experimental-ct-svelte'
+import format from '../../../dist/utils/format'
+import {getProps} from '../../../dist/api/fixtures/js/fixtures-api'
+import Nav from '../../../dist/components/compositions/navs/Nav.svelte'
+import LinkList from '../../../dist/components/compositions/navs/LinkList.svelte'
+import DetailsNav from '../../../dist/components/compositions/navs/DetailsNav.svelte'
+import RevealNav from '../../../dist/components/compositions/navs/RevealNav.svelte'
+
+test.use({viewport: {width: 500, height: 500}})
+
+const category = 'compositions'
+
+test('Nav', async ({mount}) => {
+	// 1. Init
+	const props = getProps({category, component: 'Nav'})
+	const component = await mount(Nav, {props})
+
+	// 2. Test display
+	const labels = props.items.map((link) => format.formatLabel(link.title, link.asset))
+	await expect(component).toHaveText(labels.join(' '))
+
+	// 3. Test behaviour
+	await component.getByText(labels[0]).click()
+	// expect(clicked).toBeTruthy() // TODO: fix
+})
+
+test('LinkList', async ({mount}) => {
+	// 1. Init
+	const props = getProps({category, component: 'LinkList'})
+	const component = await mount(LinkList, {props})
+
+	// 2. Test display
+	const labels = props.items.map((link) => format.formatLabel(link.title, link.asset))
+	await expect(component).toHaveText(labels.join(' '))
+
+	// 3. Test behaviour
+	// TODO
+})
+
+test('DetailsNav', async ({mount}) => {
+	// 1. Init
+	const props = getProps({category, component: 'DetailsNav'})
+	const component = await mount(DetailsNav, {props})
+
+	// 2. Test display
+	const labels = props.items.map((link) => format.formatLabel(link.title, link.asset))
+	await expect(component).toHaveText(`${props.title} ${labels.join(' ')}`)
+
+	// 3. Test behaviour
+	// TODO
+})
+
+test('RevealNav', async ({mount}) => {
+	// 1. Init
+	const props = getProps({category, component: 'RevealNav'})
+	const component = await mount(RevealNav, {props})
+
+	// 2. Test display
+	const labels = props.items.map((link) => getLabelsRecursive(link))
+	await expect(component).toHaveText(`${props.title} ${labels.join(' ')}`)
+
+	// 3. Test behaviour
+	// TODO
+})
+
+function getLabels(link) {
+	if (!link.items) {
+		return
+	}
+	return link.items.map((l) => format.formatLabel(l.title, l.asset)).join(' ')
+}
+
+function getLabelsRecursive(link) {
+	const linkLabels = getLabels(link)
+	if (!linkLabels) {
+		return
+	}
+	return link.items.reduce(
+		(accLabels, l) => {
+			const subLinkLabels = getLabels(l)
+			if (subLinkLabels) {
+				accLabels.concat(subLinkLabels)
+			}
+			return accLabels
+		},
+		[`${link.title} ${linkLabels}`],
+	)
+}
