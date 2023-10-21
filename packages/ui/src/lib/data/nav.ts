56ab2a9179749b4dafe6e0611e49486f4b6acc79
patiboh <lapatiboh@gmail.com>
2023-10-21 15:56:00 +0200

    🌟 [ui] Make main Header configurable

--- packages/ui/src/lib/data/nav.ts
+++ packages/ui/src/lib/data/nav.ts
@@ -1,4 +1,34 @@
-export const links = [
-	// {slug: 'play', title: 'Play'},
-	{slug: 'machines', title: 'Machines'},
-]
+export const links = {
+	main: [{slug: 'about', title: 'About'}],
+	side: [
+		{
+			id: 'button-theme',
+			title: 'Theme',
+			action: 'toggle',
+			asset: 'day',
+			type: 'emoji',
+			variant: 'round',
+		},
+		{
+			id: 'link-github',
+			title: 'GitHub icon',
+			url: 'https://github.com/fat-fuzzy/rocks',
+			asset: 'day',
+			type: 'icon',
+			variant: 'round',
+		},
+		// 	id: 'menu-lang',
+		// 	title: 'Lang',
+		// 	asset: langMenuIcon,
+		// 	type: 'emoji',
+		// 	// TODO: figure out a better way to [name / deal with] submenu items - see LinkList component
+		// 	subitems: languages.map((l) => ({
+		// 		id: l.code,
+		// 		title: l.title,
+		// 		action: setLanguage,
+		// 		asset: langEmojis[l.code],
+		// 		type: 'emoji',
+		// 	})),
+		// },
+	],
+}
