d082ae14dd5600eb691111ccf9e1483d918fb173
patiboh <lapatiboh@gmail.com>
2023-10-24 20:11:55 +0200

    🔧 [ui] Add lint config

--- packages/ui/postcss.config.cjs
+++ packages/ui/postcss.config.cjs
@@ -1,10 +1,11 @@
-// const autoprefixer = require('autoprefixer')
+const autoprefixer = require('autoprefixer')
+const stylelint = require('stylelint')
 
 const mode = process.env.NODE_ENV
 const dev = mode === 'development'
 
 const config = {
-	// plugins: [autoprefixer()],
+	plugins: [autoprefixer(), stylelint()],
 }
 
 module.exports = config
