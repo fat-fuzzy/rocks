dfa8894cbbc920a60917d456023472f2b05ecbc7
patiboh <lapatiboh@gmail.com>
2023-10-26 21:09:25 +0200

    🌱 [ui] Add design token and content components

---
+++ packages/ui/src/lib/components/tokens/Typography.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let layout = 'text'
+	export let size = 'md'
+	export let color = ''
+
+	const formats = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre']
+
+	$: cardClasses = `card  l:${layout}:${size} ${color} box`
+</script>
+
+<article class={cardClasses}>
+	{#each formats as text}
+		<svelte:element this={text}>The quick brown fox jumps over the lazy dog</svelte:element>
+	{/each}
+</article>
