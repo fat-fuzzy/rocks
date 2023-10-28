3df1e371e37faa7b769d9c516d7cecc3cf90e90b
patiboh <lapatiboh@gmail.com>
2023-10-28 17:54:51 +0200

    🌈[ui+doc] Improve visibility of buttons in night theme

--- packages/ui/src/lib/components/blocks/forms/Fieldset.svelte
+++ packages/ui/src/lib/components/blocks/forms/Fieldset.svelte
@@ -4,14 +4,16 @@
 	export let legend = ''
 	export let type = '' // input-group (radio group, checkbox group ...) Or other type TBD
 	export let size = 'sm'
+	export let background = ''
 	export let layout = 'stack'
 	export let container = 'burrito'
 	export let align = 'start'
 	export let breakpoint = ''
-	export let variant = 'box card' // intrinsic variations in design: [box/bare/layer] [card]
+	export let variant = '' // intrinsic variations in design: [box/bare/layer] [card]
 
 	$: containerClass = container ? `l:${container}:${size}` : ''
-	$: classes = `l:${layout} bp:${breakpoint} ${containerClass} ${size} ${type} ${variant} align:${align}`
+	$: backgroundClass = background ? `bg:${background}` : ''
+	$: classes = `l:${layout} bp:${breakpoint} ${containerClass} ${size} ${type} ${variant} ${backgroundClass} align:${align}`
 </script>
 
 <fieldset {id} {name} data-key={id} class={classes}>
