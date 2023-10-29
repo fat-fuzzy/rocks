e59f903f79bd4b00b5cccce91a39dab9e195e803
patiboh <lapatiboh@gmail.com>
2023-10-29 13:04:18 +0100

    🌱 [doc] Fix responsiveness of category header inputs

--- apps/doc/src/routes/ui/[category]/+page.svelte
+++ apps/doc/src/routes/ui/[category]/+page.svelte
@@ -30,7 +30,7 @@
 </svelte:head>
 
 <header class="bg:polar l:sidebar page-header">
-	<h1 class="l:main:80 card:xl">{title}</h1>
+	<h1 class="l:main:50 card:xl">{title}</h1>
 	<div class="l:side l:flex card:sm align:end">
 		<Api category="app" {title} />
 	</div>
