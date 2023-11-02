0fcb7bb2860692702cdb3c998ff7990c0d8dbf63
patiboh <lapatiboh@gmail.com>
2023-11-02 20:35:10 +0100

    🚧 [ui] Add colors for statuses, adjust theme color scheme

--- packages/ui/src/lib/components/tokens/Color.svelte
+++ packages/ui/src/lib/components/tokens/Color.svelte
@@ -3,16 +3,37 @@
 	export let size = 'md'
 	export let depth = '3'
 
-	// const colors = ['primary', 'accent', 'highlight', 'info', 'success', 'warning', 'error']
+	const statuses = ['info', 'success', 'warning', 'error']
 	// const variants = ['darker', 'dark', 'light', 'lighter']
 	const colors = ['primary', 'accent', 'highlight']
 	const variants = ['light', 'lighter']
 
-	$: cardClasses = `card  l:${layout}:${size}`
+	$: cardClasses = `card l:${layout}:${size}`
 	$: colorClasses = `card:xxl l:stack:sm`
 </script>
 
 {#each colors as color}
+	<article class={cardClasses}>
+		<header>
+			<svelte:element this={`h${depth}`} class="font:md">{color}</svelte:element>
+		</header>
+		<main class="l:grid:xxs">
+			<figure class={`${colorClasses} bg:${color}`}>
+				<figcaption>{color}</figcaption>
+			</figure>
+			{#each variants as variant}
+				<figure class={`${colorClasses} bg:${color}:${variant}`}>
+					<figcaption>{color}:{variant}</figcaption>
+				</figure>
+			{/each}
+		</main>
+		<footer>
+			<slot name="footer" />
+		</footer>
+	</article>
+{/each}
+
+{#each statuses as color}
 	<article class={cardClasses}>
 		<header>
 			<svelte:element this={`h${depth}`} class="font:md">{color}</svelte:element>
