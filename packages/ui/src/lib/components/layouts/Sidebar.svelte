8e8d6fc81fa0c3f588e60a046aa4d069ea786302
patiboh <lapatiboh@gmail.com>
2023-11-03 10:17:28 +0100

    💄 [ui+doc] Harmonize spacing of content > headers

--- packages/ui/src/lib/components/layouts/Sidebar.svelte
+++ packages/ui/src/lib/components/layouts/Sidebar.svelte
@@ -2,13 +2,16 @@
 	export let size = ''
 	export let align = ''
 	export let theme = ''
+	export let threshold: string | undefined = undefined
+
+	$: mainClass = threshold ? `l:main:${threshold}` : 'l:main'
 </script>
 
-<div class={`l:sidebar ${align} ${theme}`}>
-	<div class={`l:side:${size}`}>
+<div class={`l:sidebar:${size} ${align} ${theme}`}>
+	<div class={`l:side`}>
 		<slot name="side"><!-- optional fallback --></slot>
 	</div>
-	<div class={'l:main'}>
+	<div class={mainClass}>
 		<slot name="main"><!-- optional fallback --></slot>
 	</div>
 </div>
