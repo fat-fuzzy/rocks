b215c504fa78ffbb2bfc7f3db7bbb03392d743bc
patiboh <lapatiboh@gmail.com>
2023-11-10 15:03:20 +0100

    🌈 [ui+doc] Adjust headings text: make it short, like a heading

--- packages/ui/src/lib/components/tokens/Typography.svelte
+++ packages/ui/src/lib/components/tokens/Typography.svelte
@@ -3,13 +3,17 @@
 	export let size = 'md'
 	export let color = ''
 
-	const formats = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre']
+	const headings = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+	const body = ['p', 'pre']
 
 	$: cardClasses = `card  l:${layout}:${size} ${color}`
 </script>
 
 <article class={cardClasses}>
-	{#each formats as text}
+	{#each headings as text}
+		<svelte:element this={text}>Fox Jumps Over Dog</svelte:element>
+	{/each}
+	{#each body as text}
 		<svelte:element this={text}>The quick brown fox jumps over the lazy dog</svelte:element>
 	{/each}
 </article>
