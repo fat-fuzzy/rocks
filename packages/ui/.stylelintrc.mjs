a77f418f14c8d76e26a1a773bcc3ffb57d502fb3
patiboh <lapatiboh@gmail.com>
2023-11-17 13:33:04 +0100

    🚧 [ui] Use .mjs stylelint config to distinguish scss vs css ignores

---
+++ packages/ui/.stylelintrc.mjs
@@ -0,0 +1,23 @@
+const cssRules = {
+	'rule-empty-line-before': null,
+	'comment-empty-line-before': null,
+	'value-no-vendor-prefix': null,
+	'declaration-empty-line-before': null,
+	'property-no-vendor-prefix': null,
+	'at-rule-empty-line-before': null,
+	'custom-property-empty-line-before': null,
+	'value-no-vendor-prefix': null,
+}
+const scssRules = {
+	'selector-class-pattern': '([a-z](\\:)?)+((\\:)?[a-z])+',
+	'custom-property-pattern': '([a-z](-)?[0-9]?(-)?)+',
+	'scss/dollar-variable-pattern': '([a-z](-)?[0-9]?(-)?)+',
+	'selector-attribute-quotes': 'never',
+	'rule-empty-line-before': null,
+	'scss/load-no-partial-leading-underscore': null,
+	'function-name-case': null,
+}
+export default {
+	extends: ['stylelint-config-standard', 'stylelint-config-standard-scss'],
+	rules: {...scssRules, ...cssRules},
+}
