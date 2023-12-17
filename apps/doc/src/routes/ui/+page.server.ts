e57ec94b7c3bdbacbb97e63fb3bf0065cc491633
patiboh <lapatiboh@gmail.com>
2023-12-17 01:21:18 +0100

    🐛 [ui+doc] Fix function call

--- apps/doc/src/routes/ui/+page.server.ts
+++ apps/doc/src/routes/ui/+page.server.ts
@@ -94,7 +94,7 @@ export const actions = {
 			currentTabs = JSON.parse(serialized)
 		}
 		const tabs = new DsTabsUpdate(currentTabs)
-		if (!tabs.updateElementTab(data)) {
+		if (!tabs.updateCategoryTab(data)) {
 			return fail(400, {tabsError: true})
 		}
 		cookies.set('fat-fuzzy-ui-tabs', tabs.toString(), {path: '/'})
