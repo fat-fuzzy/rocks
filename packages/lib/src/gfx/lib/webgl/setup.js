a043581117818862179a48fc5832275326ca1764
patiboh <lapatiboh@gmail.com>
2023-12-19 23:43:37 +0100

    👾 [lib/gfx] Export graphics lib tools, fix nullpointer (wip)

--- packages/lib/src/gfx/lib/webgl/setup.js
+++ packages/lib/src/gfx/lib/webgl/setup.js
@@ -13,13 +13,13 @@
  */
 function compile(gl, type, source) {
 	const shader = gl.createShader(type)
-	gl.shaderSource(shader, source)
-	gl.compileShader(shader)
-	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-		gl.deleteShader(shader)
-		throw new Error(gl.getShaderInfoLog(shader))
+	if (shader) {
+		gl.shaderSource(shader, source)
+		gl.compileShader(shader)
+		if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+			throw new Error(gl.getShaderInfoLog(shader))
+		}
 	}
-
 	return shader
 }
 
