13e3810c3aa69d4e01afeaef50f5cc8c579421a7
patiboh <lapatiboh@gmail.com>
2023-12-23 17:50:35 +0100

    🐛 [ui+doc] Adjust center page layout (About)

--- apps/doc/src/routes/about/+page.svelte
+++ apps/doc/src/routes/about/+page.svelte
@@ -12,17 +12,19 @@
 	<h1 class="l:center card:xl">About Fat Fuzzy</h1>
 </header>
 
-<div class="l:center card:xs l:text:xl">
-	<p>
-		This is a <a href="https://kit.svelte.dev" class="font:md">SvelteKit</a> app. You can make your own
-		by typing the following into your command line and following the prompts:
-	</p>
+<div class="l:center:xxl col:center">
+	<div class="card:xl l:text:xl">
+		<p>
+			This is a <a href="https://kit.svelte.dev" class="font:md">SvelteKit</a> app. You can make your
+			own by typing the following into your command line and following the prompts:
+		</p>
 
-	<pre>npm create svelte@latest</pre>
+		<pre>npm create svelte@latest</pre>
 
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
+		<p>
+			The page you're looking at is purely static HTML, with no client-side interactivity needed.
+			Because of that, we don't need to load any JavaScript. Try viewing the page's source, or
+			opening the devtools network panel and reloading.
+		</p>
+	</div>
 </div>
--- packages/ui/src/routes/about/+page.svelte
+++ packages/ui/src/routes/about/+page.svelte
@@ -11,17 +11,19 @@
 	<h1>About Fat Fuzzy</h1>
 </header>
 
-<section class="l:center l:text:xl">
-	<p>
-		This is a <a href="https://kit.svelte.dev" class="font:md">SvelteKit</a> app. You can make your own
-		by typing the following into your command line and following the prompts:
-	</p>
+<div class="l:center:xxl col:center">
+	<div class="card:xl l:text:xl">
+		<p>
+			This is a <a href="https://kit.svelte.dev" class="font:md">SvelteKit</a> app. You can make your
+			own by typing the following into your command line and following the prompts:
+		</p>
 
-	<pre>npm create svelte@latest</pre>
+		<pre>npm create svelte@latest</pre>
 
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-</section>
+		<p>
+			The page you're looking at is purely static HTML, with no client-side interactivity needed.
+			Because of that, we don't need to load any JavaScript. Try viewing the page's source, or
+			opening the devtools network panel and reloading.
+		</p>
+	</div>
+</div>
