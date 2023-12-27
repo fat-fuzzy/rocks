93f8756bb4d569fff3fec6e300609157dd16fd0f
patiboh <lapatiboh@gmail.com>
2023-12-27 21:49:07 +0100

    🔥 [ui+doc] Remove unnecessary inputs from main doc page

--- apps/doc/src/routes/ui/+page.svelte
+++ apps/doc/src/routes/ui/+page.svelte
@@ -1,27 +1,12 @@
 <script lang="ts">
-	import {onDestroy} from 'svelte'
 	import {page} from '$app/stores'
-	import {
-		headless,
-		tokens,
-		blocks,
-		layouts,
-		recipes,
-		graphics,
-		api,
-		stores,
-		constants,
-	} from '@fat-fuzzy/ui'
+	import {headless, tokens, blocks, layouts, recipes, graphics, api} from '@fat-fuzzy/ui'
 
 	const {Head} = headless
-	const {Collection, Api} = api
-	const {Sidebar, RevealAuto} = layouts
-	const {DEFAULT_REVEAL_STATE} = constants
+	const {Collection} = api
 
 	const actionPath = '/ui'
 	const title = 'UI' // TODO : Fix title: add breadcrumb nav component ?
-
-	let revealContext = $page.data.context || DEFAULT_REVEAL_STATE
 
 	const components = [
 		{category: 'tokens', items: tokens},
@@ -32,51 +17,15 @@
 	]
 	const path = $page.url.pathname
 
-	const localStores = [
-		stores.ui.reveal.subscribe((value) => {
-			if (value) {
-				revealContext = value
-			}
-		}),
-	]
-
-	function handleToggle(event: CustomEvent) {
-		stores.ui.reveal.set(event.detail)
-	}
-
-	$: reveal = revealContext.reveal
 	$: markdowns = $page.data.markdowns
 	$: content = markdowns.categories.find(({meta}) => meta.slug === 'ui')
 	$: headerClass = 'page-header l:switcher:xs bp:xxs bg:polar'
-	// TODO: load text content to README.md
-
-	onDestroy(() => {
-		localStores.forEach((unsubscribe) => unsubscribe())
-	})
 </script>
 
 <Head page={title} description={`${title} Doc`} />
 
 <header class={headerClass}>
-	<h1 class="l:main:40 nowrap maki lg">Fat Fuzzy {title}</h1>
-	<RevealAuto
-		id="ui-app-context"
-		size="sm"
-		breakpoint="sm"
-		color="primary"
-		align="start"
-		variant="outline"
-		title="Context"
-		formaction="toggleContext"
-		{actionPath}
-		{reveal}
-		redirect={$page.url.pathname}
-		on:toggle={handleToggle}
-	>
-		<div slot="content" class="l:side shrink ui:menu">
-			<Api {path} {actionPath} redirect={$page.url.pathname} />
-		</div>
-	</RevealAuto>
+	<h1 class="card:lg">Fat Fuzzy {title}</h1>
 </header>
 
 <section class="card:md">
