e3cd99befb63e54c6000e0adbbccae6e6c3843ba
patiboh <lapatiboh@gmail.com>
2024-01-02 20:28:38 +0100

    👾 [play] New Sketch : hierarchical animation

--- apps/play/src/routes/play/+layout.server.ts
+++ apps/play/src/routes/play/+layout.server.ts
@@ -1,7 +1,6 @@
 import {sketches} from '$lib/data/sketches'
-import type {LayoutLoad} from './$types'
 
-export const load: LayoutLoad = () => {
+export const load = () => {
 	return {
 		sketches,
 	}
---
+++ packages/lib/src/gfx/sketches/006/README.md
@@ -0,0 +1,19 @@
+---
+title: Hierarchical
+date: '2024-01-02'
+slug: hierarchical
+id: '006'
+emoji: '📐'
+---
+
+## 🚧 WHat / Why
+
+TODO
+
+## 🚧 How
+
+Uses WebGL2
+
+## 🚧 Resources
+
+TODO
