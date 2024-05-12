4a18b5ba682d9ba07654e692e3c6a1bf90e43bff
patiboh <lapatiboh@gmail.com>
2024-05-12 14:53:56 +0200

    🌈 [sketch] Tone down Debug events row styles

--- packages/sketch/src/lib/components/debug/Debug.svelte
+++ packages/sketch/src/lib/components/debug/Debug.svelte
@@ -13,94 +13,92 @@
 		<thead class="bg:primary:300">
 			<tr>
 				<th class="bg:accent:300" scope="col">Debug</th>
-				<th colspan="2">Sketch</th>
-				<th colspan="2">Canvas</th>
-				<th colspan="2">Player</th>
+				<th>Sketch</th>
+				<th>Canvas</th>
+				<th>Player</th>
 				{#if meta?.type === 'matrix-2d' || meta?.type === 'matrix-3d' || meta?.type === 'texture'}
-					<th colspan="2">Controls</th>
+					<th>Controls</th>
 				{/if}
 			</tr>
 		</thead>
 		<tbody class="text:center">
-			<tr class="bg:accent:000">
-				<td class="bg:lighttext:center l:stack:2xs card:sm">
+			<tr>
+				<td class="bg:light text:center l:flex nowrap align:center card:sm">
 					<span>Event</span>
-					<div class="l:flex nowrap">
-						<span class="card:2xs bg:accent:100">Previous</span>
-						<span class="card:2xs bg:primary:100">Current</span>
+					<span class="card:2xs bg:accent:100 variant:bare">prev</span>
+					<span class="card:2xs bg:accent:000">current</span>
+				</td>
+				<td class="variant:outline">
+					<div class="bg:light text:center l:flex nowrap align:center card:sm">
+						<span class="card:2xs bg:accent:100 variant:bare">
+							{context.getEvent('previous')}
+						</span>
+						<span class="card:2xs bg:accent:000">
+							{context.getEvent('current')}
+						</span>
 					</div>
 				</td>
 				<td class="variant:outline">
-					<span class="card:2xs bg:accent:100">
-						{context.getEvent('previous')}
-					</span>
+					<div class="bg:light text:center l:flex nowrap align:center card:sm">
+						<span class="card:2xs bg:accent:100 variant:bare">
+							{context.getEvent('previous')}
+						</span>
+						<span class="card:2xs bg:accent:000">
+							{context.getEvent('current')}
+						</span>
+					</div>
 				</td>
 				<td class="variant:outline">
-					<span class="card:2xs bg:primary:100">
-						{context.getEvent('current')}
-					</span>
+					<div class="bg:light text:center l:flex nowrap align:center card:sm">
+						<span class="card:2xs bg:accent:100 variant:bare">
+							{context.getEvent('previous')}
+						</span>
+						<span class="card:2xs bg:accent:000">
+							{context.getEvent('current')}
+						</span>
+					</div>
 				</td>
 				<td class="variant:outline">
-					<span class="card:2xs bg:accent:100">
-						{context.getEvent('previous')}
-					</span>
-				</td>
-				<td class="variant:outline">
-					<span class="card:2xs bg:primary:100">
-						{context.getEvent('current')}
-					</span>
-				</td>
-				<td class="variant:outline">
-					<span class="card:2xs bg:accent:100">
-						{context.getEvent('previous')}
-					</span>
-				</td>
-				<td class="variant:outline">
-					<span class="card:2xs bg:primary:100">
-						{context.getEvent('current')}
-					</span>
-				</td>
-				<td class="variant:outline">
-					<span class="card:2xs bg:accent:100">
-						{context.getEvent('previous')}
-					</span>
-				</td>
-				<td class="variant:outline">
-					<span class="card:2xs bg:primary:100">
-						{context.getEvent('current')}
-					</span>
+					<div class="bg:light text:center l:flex nowrap align:center card:sm">
+						<span class="card:2xs bg:accent:100 variant:bare">
+							{context.getEvent('previous')}
+						</span>
+						<span class="card:2xs bg:accent:000">
+							{context.getEvent('current')}
+						</span>
+					</div>
 				</td>
 			</tr>
 			<tr class="bg:primary:000">
 				<td class="bg:primary:000 variant:outline">State</td>
-				<td class="variant:outline" colspan="2">
+				<td class="variant:outline">
 					{context.getState('sketch')}
 				</td>
-				<td class="variant:outline" colspan="2">
+				<td class="variant:outline">
 					{context.getState('canvas')}
 				</td>
-				<td class="variant:outline" colspan="2">
+				<td class="variant:outline">
 					{context.getState('player')}
 				</td>
 				{#if meta?.type === 'matrix-2d' || meta?.type === 'matrix-3d' || meta?.type === 'texture'}
-					<td class="variant:outline" colspan="2">
+					<td class="variant:outline">
 						{context.getState('controls')}
 					</td>
 				{/if}
 			</tr>
 			<tr class="bg:primary:100">
 				<td class="bg:primary:100 variant:outline">Actions</td>
-				<td class="variant:outline" colspan="2">
+				<td class="variant:outline">
 					{context.getNextActions('sketch')}
 				</td>
-				<td class="variant:outline" colspan="2">
+				<td class="variant:outline">
 					{context.getNextActions('canvas')}
 				</td>
-				<td class="variant:outline" colspan="2">
+				<td class="variant:outline">
 					{context.getNextActions('player')}
 				</td>
 				{#if meta?.type === 'matrix-2d' || meta?.type === 'matrix-3d' || meta?.type === 'texture'}
-					<td class="variant:outline" colspan="2">
+					<td class="variant:outline">
 						{context.getNextActions('controls')}
 					</td>
 				{/if}
@@ -113,7 +111,6 @@
 					class={`variant:outline ${
 						context.getErrors('sketch').length > 0 ? 'bg:danger:100' : ''
 					}`}
-					colspan="2"
 				>
 					{context.getErrors('sketch')}
 				</td>
@@ -121,15 +118,13 @@
 					class={`variant:outline ${
 						context.getErrors('sketch').length > 0 ? 'bg:danger:100' : ''
 					}`}
-					colspan="2"
 				>
 					{context.getErrors('canvas')}
 				</td>
 				<td
 					class={`variant:outline ${
-						context.getErrors('sketch').length > 0 ? 'bg:danger:100' : ''
+						context.getErrors('player').length > 0 ? 'bg:danger:100' : ''
 					}`}
-					colspan="2"
 				>
 					{context.getErrors('player')}
 				</td>
@@ -138,7 +133,6 @@
 						class={`variant:outline ${
 							context.getErrors('sketch').length > 0 ? 'bg:danger:100' : ''
 						}`}
-						colspan="2"
 					>
 						{context.getErrors('controls')}
 					</td>
