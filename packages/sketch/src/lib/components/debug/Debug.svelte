a4977220d0ebc8d31f4df39e0822515a6c58667f
patiboh <lapatiboh@gmail.com>
2024-05-20 11:05:32 +0200

    ♻️  [sketch] Clean Debug table template, add test ids

--- packages/sketch/src/lib/components/debug/Debug.svelte
+++ packages/sketch/src/lib/components/debug/Debug.svelte
@@ -1,24 +1,31 @@
 <script lang="ts">
 	import type {SceneMeta} from '$types'
+	import {onMount} from 'svelte'
 
 	type Props = {
 		meta: SceneMeta | undefined
 		context: any
 	}
 	let {meta, context}: Props = $props()
+	let sketchUi = ['sketch', 'canvas', 'player']
+	onMount(() => {
+		if (
+			meta.controls?.length > 1 ||
+			(meta.controls?.length === 1 && meta.controls[0] !== 'loop')
+		) {
+			sketchUi.push('controls')
+		}
+	})
 </script>
 
 <aside class="debug l:stack:sm maki:block size:2xs overflow:x card:dotted">
-	<table class="text:center font:sm">
+	<table class="text:center font:sm" data-testid="debug-table">
 		<thead class="bg:primary:300">
 			<tr>
 				<th class="bg:accent:300" scope="col">Debug</th>
-				<th>Sketch</th>
-				<th>Canvas</th>
-				<th>Player</th>
-				{#if meta.controls?.length > 1 || (meta.controls?.length === 1 && meta.controls[0] !== 'loop')}
-					<th>Controls</th>
-				{/if}
+				{#each sketchUi as ui}
+					<th>{ui}</th>
+				{/each}
 			</tr>
 		</thead>
 		<tbody class="text:center">
@@ -28,149 +35,62 @@
 					<span class="card:2xs bg:accent:100 variant:bare">prev</span>
 					<span class="card:2xs bg:accent:000">current</span>
 				</td>
-				<td class="variant:outline">
-					<div
-						class="bg:light justify:center l:flex nowrap align:center card:sm"
-					>
-						<span class="card:2xs bg:accent:100 variant:bare">
-							{context.getPreviousEvent()}
-						</span>
-						<span class="card:2xs bg:accent:000">
-							{context.getCurrentEvent()}
-						</span>
-					</div>
-				</td>
-				<td class="variant:outline">
-					<div
-						class="bg:light justify:center l:flex nowrap align:center card:sm"
-					>
-						<span class="card:2xs bg:accent:100 variant:bare">
-							{context.getPreviousEvent()}
-						</span>
-						<span class="card:2xs bg:accent:000">
-							{context.getCurrentEvent()}
-						</span>
-					</div>
-				</td>
-				<td class="variant:outline">
-					<div
-						class="bg:light justify:center l:flex nowrap align:center card:sm"
-					>
-						<span class="card:2xs bg:accent:100 variant:bare">
-							{context.getPreviousEvent()}
-						</span>
-						<span class="card:2xs bg:accent:000">
-							{context.getCurrentEvent()}
-						</span>
-					</div>
-				</td>
-				{#if meta.controls?.length > 1 || (meta.controls?.length === 1 && meta.controls[0] !== 'loop')}
-					<td class="variant:outline">
+				{#each sketchUi as ui}
+					<td class="variant:outline" data-testid={`debug-event-${ui}`}>
 						<div
 							class="bg:light justify:center l:flex nowrap align:center card:sm"
 						>
-							<span class="card:2xs bg:accent:100 variant:bare">
+							<span
+								class="card:2xs bg:accent:100 variant:bare"
+								data-testid="previous-event"
+							>
 								{context.getPreviousEvent()}
 							</span>
-							<span class="card:2xs bg:accent:000">
+							<span class="card:2xs bg:accent:000" data-testid="current-event">
 								{context.getCurrentEvent()}
 							</span>
 						</div>
 					</td>
-				{/if}
+				{/each}
 			</tr>
 			<tr class="bg:primary:000">
 				<td class="bg:primary:000 variant:outline">State</td>
-				<td class="variant:outline">
-					{context.getState('sketch')}
-				</td>
-				<td class="variant:outline">
-					{context.getState('canvas')}
-				</td>
-				<td class="variant:outline">
-					{context.getState('player')}
-				</td>
-				{#if meta.controls?.length > 1 || (meta.controls?.length === 1 && meta.controls[0] !== 'loop')}
-					<td class="variant:outline">
-						{context.getState('controls')}
+				{#each sketchUi as ui}
+					<td class="variant:outline" data-testid={`debug-state-${ui}`}>
+						{context.getState(ui)}
 					</td>
-				{/if}
+				{/each}
 			</tr>
 			<tr class="bg:primary:100">
 				<td class="bg:primary:100 variant:outline">Actions</td>
-				<td class="variant:outline">
-					{context.getNextActions('sketch')}
-				</td>
-				<td class="variant:outline">
-					{context.getNextActions('canvas')}
-				</td>
-				<td class="variant:outline">
-					{context.getNextActions('player')}
-				</td>
-				{#if meta.controls?.length > 1 || (meta.controls?.length === 1 && meta.controls[0] !== 'loop')}
-					<td class="variant:outline">
-						{context.getNextActions('controls')}
+				{#each sketchUi as ui}
+					<td class="variant:outline" data-testid={`debug-actions-${ui}`}>
+						{context.getNextActions(ui)}
 					</td>
-				{/if}
+				{/each}
 			</tr>
 		</tbody>
 		<tfoot>
 			<tr class="bg:accent:000">
 				<td class="variant:outline">Feedback</td>
-				<td class="variant:outline">
-					{#if context.getFeedback('sketch').length > 0}
-						{@const feedbackSketch = context.getFeedback('sketch')}
-						{#each feedbackSketch as feedback}
-							<div class={`feedback bg:${feedback.status} variant:bare`}>
-								<p class={`status emoji:${feedback.status}`}>
-									{feedback.status}
-								</p>
-								<p>{feedback.message}</p>
-							</div>
-						{/each}
-					{/if}
-				</td>
-				<td class="variant:outline">
-					{#if context.getFeedback('canvas').length > 0}
-						{@const feedbackSketch = context.getFeedback('canvas')}
-						{#each feedbackSketch as feedback}
-							<div class={`feedback bg:${feedback.status} variant:bare`}>
-								<p class={`status emoji:${feedback.status}`}>
-									{feedback.status}
-								</p>
-								<p>{feedback.message}</p>
-							</div>
-						{/each}
-					{/if}
-				</td>
-				<td class="variant:outline">
-					{#if context.getFeedback('player').length > 0}
-						{@const feedbackSketch = context.getFeedback('player')}
-						{#each feedbackSketch as feedback}
-							<div class={`feedback bg:${feedback.status} variant:bare`}>
-								<p class={`status emoji:${feedback.status}`}>
-									{feedback.status}
-								</p>
-								<p>{feedback.message}</p>
-							</div>
-						{/each}
-					{/if}
-				</td>
-				{#if meta.controls?.length > 1 || (meta.controls?.length === 1 && meta.controls[0] !== 'loop')}
-					<td class="variant:outline">
-						{#if context.getFeedback('controls').length > 0}
-							{@const feedbackSketch = context.getFeedback('controls')}
+				{#each sketchUi as ui}
+					<td class="variant:outline" data-testid={`debug-feedback-${ui}`}>
+						{#if context.getFeedback(ui).length > 0}
+							{@const feedbackSketch = context.getFeedback(ui)}
 							{#each feedbackSketch as feedback}
 								<div class={`feedback bg:${feedback.status} variant:bare`}>
-									<p class={`status emoji:${feedback.status}`}>
+									<p
+										class={`status emoji:${feedback.status}`}
+										data-testid={`feedback-status`}
+									>
 										{feedback.status}
 									</p>
-									<p>{feedback.message}</p>
+									<p data-testid={`feedback-message`}>{feedback.message}</p>
 								</div>
 							{/each}
 						{/if}
 					</td>
-				{/if}
+				{/each}
 			</tr>
 		</tfoot>
 	</table>
