d496adecd9453ffd0924cdd8b5f7f4ac0061f699
patiboh <lapatiboh@gmail.com>
2024-05-20 23:15:28 +0200

    🚧 [sketch] Outline controls tests

--- packages/sketch/tests/e2e/controls/texture.spec.ts
+++ packages/sketch/tests/e2e/controls/texture.spec.ts
@@ -1,5 +1,15 @@
 import {expect, test} from '@playwright/test'
 
+// TODO: Controls state update - should be:
+// - `hidden` on load until `play`
+// - `pristine` on `play` until user interaction
+// - `pristine` (or `cleared` ?) on `clear` until user interaction
+// - `pristine` on `stop` until user interaction
+// - `updated` on user interaction until` `clear`
+// - `paused` on `pause`, then:
+//   - `updated` or `pristine` (the state previous to `paused`) on `play`
+//   - `pristine` on `clear`, then as above
+// - `hidden` on `stop`, then as above
 test('Texture Sketch loads OK', async ({page}) => {
 	// TODO: Load a texture Sketch and check that it loads OK
 })
