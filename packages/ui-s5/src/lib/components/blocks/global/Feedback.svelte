4755d8b602cecd3f758e35bf3961309d484e99c8
patiboh <lapatiboh@gmail.com>
2024-05-31 23:11:24 +0200

    ♻️  [ui] Migrate to s5: feedback and head

--- packages/ui-s5/src/lib/components/blocks/global/Feedback.svelte
+++ packages/ui-s5/src/lib/components/blocks/global/Feedback.svelte
@@ -1,29 +1,51 @@
 <script lang="ts">
-	export let text = ''
-	export let asset = 'emoji:default'
-	export let context = '' // feedback context: code, form, dialog
-	export let status = 'default' // feedback color: info, success, warning, error
-	export let size = ''
-	export let variant = 'outline' // feedback variant: default, outline, bare
-	export let container = ''
+	import type {Snippet} from 'svelte'
 
-	$: background = context === 'code' ? '' : `bg:${status}:lighter`
-	$: containerClass = container ? `${container}:${size}` : ''
-	$: assetClass = asset.split(':').length > 1 ? asset : `emoji:${status}`
-	$: feedbackClass = `feedback ${containerClass} ${assetClass} ${status} font:${size} ${variant} ${background}`
+	type Props = {
+		asset: string
+		text?: string
+		context: string // feedback context: code, form, dialog,
+		status: string // feedback color: info, success, warning, error,
+		size: string
+		variant: string // feedback variant: default, outline, bare,
+		container: string
+		children: Snippet
+	}
+
+	let {
+		asset = 'emoji:default',
+		text,
+		context, // feedback context: code, form, dialog,
+		status, // feedback color: info, success, warning, error,
+		size,
+		variant, // feedback variant: default, outline, bare,
+		container,
+		children,
+	}: Props = $props()
+
+	let background = $derived(context === 'code' ? '' : `bg:${status}:000`)
+	let containerClass = $derived(container && context !== 'code' ? `l:${container}:${size}` : '')
+	let assetClass = $derived(asset.split(':').length > 1 ? asset : `emoji:${status}`)
+	let feedbackClass = $derived(`feedback ${containerClass} ${assetClass} status:${status} font:${size} variant:${variant} ${background}`)
 </script>
 
 {#if context === 'code'}
-	<pre class={feedbackClass} data-test={`feedback-${context}`}>{text}</pre>
+	<pre class={feedbackClass} data-testid={`feedback-${context}`}>
+		{#if children}
+			{@render children()}
+		{:else if text}
+			{text}
+		{/if}
+	</pre>
 {:else if context === 'form'}
-	<div class={feedbackClass} data-test={`feedback-${context}`}>
-		<div class="l:stack:sm">
-			{#if status !== 'default'}
-				<p class="status">{status}</p>
-			{/if}
-			<p>
-				{text}
-			</p>
-		</div>
+	<div class={feedbackClass} data-testid={`feedback-${context}`}>
+		{#if status !== 'default'}
+			<p class="status">{status}</p>
+		{/if}
+		{#if children}
+			{@render children()}
+		{:else if text}
+			{text}
+		{/if}
 	</div>
 {/if}
--- packages/ui-s5/src/lib/components/blocks/global/Head.svelte
+++ packages/ui-s5/src/lib/components/blocks/global/Head.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
-	export let page = ''
-	export let prefix = 'Fat Fuzzy'
-	export let title = ''
-	export let description = 'Page description'
-
-	$: fullPrefix = page ? `${prefix} ${page}` : `${prefix}`
-	$: fullTitle = title ? `${fullPrefix} | ${title}` : `${fullPrefix}`
+	type Props = {
+		title: string
+		description: string
+		page?: string
+		prefix?: string
+	}
+	let {
+		title,
+		description = 'Page description',
+		page,
+		prefix = 'Fat Fuzzy',
+	}: Props = $props()
+	let fullPrefix = $derived(page ? `${prefix} ${page}` : `${prefix}`)
+	let fullTitle = $derived(title ? `${fullPrefix} | ${title}` : `${fullPrefix}`)
 </script>
 
 <svelte:head>
