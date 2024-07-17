2fd8d307fcb9b41d7a9b7c4a20860b12b015a28d
patiboh <lapatiboh@gmail.com>
2024-07-17 14:12:59 +0200

    🚧 [all] Setup testing pipeline, base test scripts & specs

--- packages/ui-s5/playwright.config.ts
+++ packages/ui-s5/playwright.config.ts
@@ -4,9 +4,18 @@ const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'pnpm build && pnpm preview', // This will run tests in watch mode
 		port: 4173,
+		reuseExistingServer: true,
 	},
 	testDir: 'tests/e2e',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+	// reporter: [
+	// 	[
+	// 		'html',
+	// 		{
+	// 			open: process.env.CI ? 'never' : 'always', // if on CI then "never" otherwise "always" show
+	// 		},
+	// 	],
+	// ],
 }
 
 export default config
