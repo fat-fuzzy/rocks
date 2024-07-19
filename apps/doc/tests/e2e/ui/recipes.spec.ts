7185ee56c81350056407dc55e405be94553e44ea
patiboh <lapatiboh@gmail.com>
2024-07-19 18:18:53 +0200

    🧪 [doc+ui] Add UI page nav tests: layotus & recipes

--- apps/doc/tests/e2e/ui/recipes.spec.ts
+++ apps/doc/tests/e2e/ui/recipes.spec.ts
@@ -1,8 +1,8 @@
 import {expect, test} from '@playwright/test'
 import utils from '../../utils/constants'
 
-const {ready, draft} = utils.blocks
-const path = utils.blocks.path
+const {ready, draft} = utils.recipes
+const path = utils.recipes.path
 
 // const components = [...ready, ...draft]
 
@@ -31,6 +31,6 @@ draft.forEach((component) => {
 		await expect(
 			page.getByRole('heading', {level: 1, name: component}),
 		).toBeVisible()
-		await expect(page.getByText('Coming Soon!')).toBeVisible()
+		await expect(page.getByText('Coming Soon!', {exact: true})).toBeVisible()
 	})
 })
