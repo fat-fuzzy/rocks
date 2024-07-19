632177131f7783ff5930835c6383511a825bf3a9
patiboh <lapatiboh@gmail.com>
2024-07-19 13:48:01 +0200

    🌱 [sketch] Style gardening

--- packages/sketch/src/lib/components/menus/CameraControls.svelte
+++ packages/sketch/src/lib/components/menus/CameraControls.svelte
@@ -40,7 +40,7 @@
 	angle={cameraAngle}
 	onupdate={updateCamera}
 	{color}
-	size={`xs l:burrito:${threshold}`}
+	size="xs"
 	disabled={store.getSketchDisabled()}
 />
 
@@ -50,6 +50,6 @@
 	max={180}
 	onupdate={updateFieldOfView}
 	{color}
-	size={`xs l:burrito:${threshold}`}
+	size="xs"
 	disabled={store.getSketchDisabled()}
 />
