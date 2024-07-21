b2f1fb9c9d80c888ca51f508298953eff4bed77b
patiboh <lapatiboh@gmail.com>
2024-07-21 22:43:59 +0200

    🎙️ [doc] Put back decision items

--- apps/doc/src/routes/doc/decisions/+page.svelte
+++ apps/doc/src/routes/doc/decisions/+page.svelte
@@ -14,7 +14,22 @@
 </script>
 
 <PageMain {title} {description} size="lg">
-	<div class="l:grid:sm maki:block">
+	<div class="l:text maki:block:2xs">
+		<ul class="l:stack:lg w:full" role="group">
+			{#each markdowns as { meta }}
+				{#if meta.status !== 'draft'}
+					<a
+						class="font:lg surface:2:neutral card:md"
+						href={`${$page.url.pathname}/${meta.slug}`}
+					>
+						{Number(meta.id)} - {meta.title}
+					</a>
+				{/if}
+			{/each}
+		</ul>
+	</div>
+	<h2>Tags</h2>
+	<div class="l:text l:grid:sm maki:block">
 		{#each tags as tag}
 			<details class="card:md l:stack size:sm bg:netural varian:bare" open>
 				<summary class="surface:2:neutral card:2xs">{tag}</summary>
@@ -25,7 +40,7 @@
 								<li class={`card:3xs`}>
 									{#if meta.status !== 'draft'}
 										<a href={`${$page.url.pathname}/${meta.slug}`}>
-											{Number(meta.id)} - {meta.title}
+											{meta.title}
 										</a>
 									{/if}
 								</li>
