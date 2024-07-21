416d843314474bb1fd6d2625d36fed8725c730ce
patiboh <lapatiboh@gmail.com>
2024-07-21 22:14:31 +0200

    ♻️ [doc] Use tags to group content

--- apps/doc/src/routes/doc/decisions/+page.svelte
+++ apps/doc/src/routes/doc/decisions/+page.svelte
@@ -8,21 +8,32 @@
 
 	let title = 'Decision Log'
 	let description = 'Log of Architectural decisions'
+	let tags = new Set(
+		markdowns.reduce((acc, {meta}) => [...acc, ...meta.tags], []),
+	)
 </script>
 
 <PageMain {title} {description} size="lg">
-	<div class="l:text maki:block:2xs">
-		<ul class="l:stack:lg w:full" role="group">
-			{#each markdowns as { meta }}
-				{#if meta.status !== 'draft'}
-					<a
-						class="font:lg surface:2:neutral card:md"
-						href={`${$page.url.pathname}/${meta.slug}`}
-					>
-						{Number(meta.id)} - {meta.title}
-					</a>
-				{/if}
-			{/each}
-		</ul>
+	<div class="l:grid:sm maki:block">
+		{#each tags as tag}
+			<details class="card:md l:stack size:sm bg:netural varian:bare" open>
+				<summary class="surface:2:neutral card:2xs">{tag}</summary>
+				<div class="l:stack:xs maki:block">
+					<ul role="group">
+						{#each markdowns as { meta }}
+							{#if meta.tags.includes(tag)}
+								<li class={`card:3xs`}>
+									{#if meta.status !== 'draft'}
+										<a href={`${$page.url.pathname}/${meta.slug}`}>
+											{Number(meta.id)} - {meta.title}
+										</a>
+									{/if}
+								</li>
+							{/if}
+						{/each}
+					</ul>
+				</div>
+			</details>
+		{/each}
 	</div>
 </PageMain>
