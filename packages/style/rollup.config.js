314766dcb8402808e994a6d21a6221584bbe4741
patiboh <patiboh@users.noreply.github.com>
2024-08-07 21:51:36 +0200

    ⬆️ [lib+style+root] Reset node version, update rollup deps

--- packages/style/rollup.config.js
+++ packages/style/rollup.config.js
@@ -34,7 +34,7 @@ export default {
 					fs.mkdirSync(path.dirname(outputPath), {recursive: true})
 					fs.writeFileSync(outputPath, content)
 				})
-			}, // will output compiled styles to "src/lib/**/*.css" as separate files including the folder structure
+			}, // will output compiled styles to "dist/lib/**/*.css" as separate files including the folder structure
 		}),
 		postcss({
 			extract: true,
