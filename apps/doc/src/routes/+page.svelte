f21f04fdc951167d092d732c033dcacf5be61bb7
patiboh <patiboh@users.noreply.github.com>
2024-08-31 18:19:13 +0200

    🌟 [doc] Load home page image dynamically

--- apps/doc/src/routes/+page.svelte
+++ apps/doc/src/routes/+page.svelte
@@ -2,31 +2,42 @@
 	import {page} from '$app/stores'
 	import ui from '@fat-fuzzy/ui'
 
+	import fatFuzzyStore from '$lib/stores/stores.svelte'
+
 	const {PageMain} = ui.content
 	const {EscapeHtml} = ui.headless
+	const {Picture} = ui.blocks
 
-	// TODO; conditional import
-	import introDay from '$lib/images/day/001-intro.png' // TODO : optimize images
-	import introNight from '$lib/images/night/001-intro.png' // TODO : optimize images
-
+	let app = fatFuzzyStore.app
+	
+	let brightness = $derived(app.settings.brightness)
 	let markdown = $derived($page.data.content)
 	let title = $derived(markdown.meta.title)
 	let description = $derived(markdown.meta.description)
 	let html = $derived(markdown.html)
 
-let theme = 1
-const pageImage = {
-	src: theme === 1 ? introDay : introNight,
-	alt: `A drawing: "Creating a web app. Where to Start ?" asks a girl underneath a large cloud with a laptop connected to it and displaying an app in a browser. The cloud contains three boards: to the left is the CLIENT, which contains HTML, CSS, and JavaScript; to the right are RESOURCES which contain backend data and application logic; in the middle is the API, which connects the CLIENT to the RESOURCES using HTTP, URLs, JSON, REST, GraphQL, and enables calls to CRUD operations.`,
-}
-let variant = 'surface:2:primary'
+	let images = $derived({
+		src: !brightness || brightness === 'day' ?  '/images/day/001-intro' :  '/images/night/001-intro',
+		alt: `A drawing: "Creating a web app. Where to Start ?" asks a girl underneath a large cloud with a laptop connected to it and displaying an app in a browser. The cloud contains three boards: to the left is the CLIENT, which contains HTML, CSS, and JavaScript; to the right are RESOURCES which contain backend data and application logic; in the middle is the API, which connects the CLIENT to the RESOURCES using HTTP, URLs, JSON, REST, GraphQL, and enables calls to CRUD operations.`,
+	})
+	let variant = 'surface:2:primary'
 </script>
 
 <PageMain {title} {description} size="md" justify="center">
 	{#snippet header()}
 		<div class="l:center:md">
 			<div class="l:frame">
-				<img src={pageImage.src} alt={pageImage.alt} />
+					<Picture
+						src={images.src}
+						ext="png"
+						alt={images.alt}
+						width="2647"
+						height="1869"
+						sources={[
+							['2647', '1869', 'png'],
+							['2647', '1869', 'webp'],
+						]}
+					/>
 			</div>
 			<h1 class="card:sm text:center">Fat Fuzzy Rocks</h1>
 		</div>
