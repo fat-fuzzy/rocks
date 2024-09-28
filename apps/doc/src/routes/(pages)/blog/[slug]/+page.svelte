6435a7d4eb70801261539a00f6d7819d06bad158
patiboh <patiboh@users.noreply.github.com>
2024-09-28 21:13:37 +0200

    🌱 [doc] Improve posts: aside and content layouts

--- apps/doc/src/routes/(pages)/blog/[slug]/+page.svelte
+++ apps/doc/src/routes/(pages)/blog/[slug]/+page.svelte
@@ -7,19 +7,20 @@
 	const {EscapeHtml} = ui.headless
 
 	let post = $derived($page.data)
-	let title = $derived(post.index ? `${post.title}: Part ${post.index}` : post.title)
+	let series = $derived(post.series ? post.series.items.map((id) => $page.data.markdowns.find((post) => post.meta.id === id)) :  null)
+	let seriesData = $derived(series ? series.map((post) => ({link: post.meta.slug, title: post.meta.subtitle})): undefined)
 </script>
 
 {#key post.id}
-	<PageMain pageName="Blog" title={title} description={post.description}>
+	<PageMain pageName="Blog" title={post.title} description={post.description}>
 		{#snippet header()}
 			<div class="l:main:50 maki:block:md">
-				<h1>{title}</h1>
-				{#if post.subtitle}<p class="h4">{post.subtitle}</p>{/if}
+				<h1>{post.title}</h1>
+				{#if post.subtitle}<p class="h4">{post.index ? `Part ${post.index}: ${post.subtitle}` : post.subtitle}</p>{/if}
 				{#if post.description}<p>{post.description}</p>{/if}
 			</div>
 			<div class="l:side">
-				<Aside created={post.date_created}  updated={post.date_updated}  series={post.series} page={post.index}/>
+				<Aside created={post.date_created}  updated={post.date_updated}  series={seriesData} page={post.index}/>
 			</div>
 		{/snippet}
 		<article class="l:sidebar:md">
