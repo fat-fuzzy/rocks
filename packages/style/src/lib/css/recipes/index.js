f5a7ea7e7e5389eb14e24274c996c69b93b6645a
patiboh <patiboh@users.noreply.github.com>
2024-10-03 20:41:25 +0200

    ♻️ [style] Move api styles to playbook.css, cleanup

--- packages/style/src/lib/css/recipes/index.js
+++ packages/style/src/lib/css/recipes/index.js
@@ -16,6 +16,6 @@ import './article.css' /* Use for articles (header + section(s) + optional foote
 import './grid-header.css' /* Use for a page header layout */
 import './grid-props.css' /* Use for a props inputs layouts */
 
-import './api.css' /* Use for API components (used in Doc website) */
+import './playbook.css' /* Use for API components (used in Doc website): TODO: move to playbook package */
 import './app.css' /* Use for app layout specifics */
 import './pages.css' /* Use for page layout specifics */
