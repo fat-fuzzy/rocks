7438f937495e43feeb07ab57a9d19219e39ed213
patiboh <patiboh@users.noreply.github.com>
2024-10-30 20:23:39 +0100

    🚧 [ui] Fix nojs: revert form changes

--- packages/ui/src/lib/components/layouts/Reveal.svelte
+++ packages/ui/src/lib/components/layouts/Reveal.svelte
@@ -11,7 +11,7 @@
 	let {
 		id = 'reveal',
 		title = 'Reveal',
-		method = 'GET',
+		method = 'POST',
 		formaction,
 		actionPath,
 		redirect,
--- packages/ui/src/lib/components/recipes/content/LayoutSidebar.svelte
+++ packages/ui/src/lib/components/recipes/content/LayoutSidebar.svelte
@@ -1,26 +1,29 @@
 <script lang="ts">
 	import {type Snippet} from 'svelte'
 	import constants from '$lib/types/constants.js'
-	import type { RevealNavProps } from '$types'
+	import type {RevealNavProps} from '$types'
 	import RevealNav from '$lib/components/recipes/navs/RevealNav.svelte'
 
 	const {DEFAULT_NAV_REVEAL_STATE} = constants
 
 	// TODO: fix types
 	type Props = {
-		path: string,
-		nav: RevealNavProps,
-		redirect: string,
+		path: string
+		nav: RevealNavProps
+		redirect: string
 		app?: {settings: {[key: string]: string}}
 		children: Snippet
 	}
 	let {path, nav, redirect, app, children}: Props = $props()
 
-	let sidebarReveal: {[key: string]: string} = $state(nav.reveal ? {reveal: nav.reveal} : DEFAULT_NAV_REVEAL_STATE)
+	let sidebarReveal: {[key: string]: string} = $state(
+		nav.reveal ? {reveal: nav.reveal} : DEFAULT_NAV_REVEAL_STATE,
+	)
 	let brightness = $derived(app?.settings?.brightness)
 	let contrast = $derived(app?.settings?.contrast)
-	let settingsClass = $derived(brightness && contrast ? `settings:${brightness}:${contrast}` : ''
-)
+	let settingsClass = $derived(
+		brightness && contrast ? `settings:${brightness}:${contrast}` : '',
+	)
 	function toggleSidebar(event) {
 		sidebarReveal.reveal = event.state
 	}
@@ -36,7 +39,7 @@
 			justify="start"
 			formaction="toggleSidebar"
 			actionPath="/"
-			size='md'
+			size="md"
 			{redirect}
 			{path}
 			onclick={toggleSidebar}
