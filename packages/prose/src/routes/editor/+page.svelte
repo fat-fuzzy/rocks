b8be2816f98ebe065c86db39bf583664b89546b8
patiboh <patiboh@users.noreply.github.com>
2024-11-09 22:07:30 +0100

    🌟 [prose] Add basic editor

---
+++ packages/prose/src/routes/editor/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Tiptap from '$lib/editor/Tiptap.svelte';
+	import ui from '@fat-fuzzy/ui';
+
+	const { PageMain } = ui.content;
+
+	let title = 'Editor';
+	let description = 'A rich text editor for the web.';
+	let html = '<p>Hello World!</p>';
+</script>
+
+<PageMain {title} {description}>
+	<Tiptap {html} />
+</PageMain>
