ef739f8f1b195613f25630970245dfcf4fbb9aab
patiboh <patiboh@users.noreply.github.com>
2024-11-09 21:58:24 +0100

    ♻️ [prose] Move markdown libs to prose, remove paraglide for now

--- packages/prose/src/routes/+layout.svelte
+++ packages/prose/src/routes/+layout.svelte
@@ -1,9 +1,41 @@
 <script lang="ts">
-	import { i18n } from '$lib/i18n';
-	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import { page } from '$app/stores';
+	import ui from '@fat-fuzzy/ui';
+	import '$lib/styles/css/main.css';
+
+	const { LayoutSidebar } = ui.content;
+	// import { i18n } from '$lib/i18n';
+	// import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	let { children } = $props();
+	let path = '';
+	let items = [
+		{
+			slug: 'entries',
+			title: 'Entries',
+			items: []
+		}
+	];
+
+	let nav = {
+		path,
+		title: 'Content',
+		id: 'nav-blog',
+		items,
+		reveal: 'expanded',
+		breakpoint: 'sm',
+		size: 'md',
+		color: 'primary:600',
+		position: 'sticky',
+		place: 'left',
+		formaction: 'toggleSidebar'
+	};
 </script>
 
+<!-- 
 <ParaglideJS {i18n}>
 	{@render children()}
-</ParaglideJS>
+</ParaglideJS> -->
+
+<LayoutSidebar {path} {nav} redirect={$page.url.pathname}>
+	{@render children()}
+</LayoutSidebar>
