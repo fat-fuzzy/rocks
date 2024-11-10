d759bad8e7daf8b35856e34ef675c88bf0059b9a
patiboh <patiboh@users.noreply.github.com>
2024-11-10 12:05:26 +0100

    🔥 [prose] Remove unused config

--- packages/prose/src/lib/i18n.ts
+++ packages/prose/src/lib/i18n.ts
@@ -1,3 +1,3 @@
-import * as runtime from '$lib/paraglide/runtime';
-import { createI18n } from '@inlang/paraglide-sveltekit';
-export const i18n = createI18n(runtime);
+// import * as runtime from '$lib/paraglide/runtime';
+// import { createI18n } from '@inlang/paraglide-sveltekit';
+// export const i18n = createI18n(runtime);
