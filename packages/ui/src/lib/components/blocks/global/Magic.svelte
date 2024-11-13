80d5ef2f2f90dd4dd51dcd900e88bd4925c65495
patiboh <patiboh@users.noreply.github.com>
2024-11-13 21:23:48 +0100

    💄 [style+ui] Clean Magic component, use custom tags

--- packages/ui/src/lib/components/blocks/global/Magic.svelte
+++ packages/ui/src/lib/components/blocks/global/Magic.svelte
@@ -2,19 +2,19 @@
 	import type {MagicProps} from '$types'
 	let {text, spell, level, uno, due, size, children}: MagicProps = $props()
 
-	let genieUno = $derived(uno ? `ffr:icon magic:${spell} svg:${uno}` : '')
-	let genieDue = $derived(due ? `ffr:icon magic:${spell} svg:${due} size:${size}` : '')
+	let genieUno = $derived(uno ? `magic:${spell} svg:${uno}` : '')
+	let genieDue = $derived(due ? `magic:${spell} svg:${due} size:${size}` : '')
 </script>
 
-<span class={`${genieUno} align:center justify:between size:${size}`}>
-	<span class={`ffr:callout magic:${spell} ${level} font:${size}`}>
+<ff-magic class={`${genieUno} align:center justify:between size:${size}`}>
+	<ff-callout class={`magic:${spell} ${level} font:${size}`}>
 		{#if text}
 			{text}
 		{:else if children}
 			{@render children()}
 		{/if}
-	</span>
+	</ff-callout>
 	{#if genieDue}
-		<span class={genieDue}></span>
+		<ff-icon class={genieDue}></ff-icon>
 	{/if}
-</span>
+</ff-magic>
