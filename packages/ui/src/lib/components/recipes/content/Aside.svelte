7bde80a96bf27a302b073cbdfffb2f557c5f0c0c
patiboh <patiboh@users.noreply.github.com>
2024-11-22 21:44:43 +0100

    🌈 [style+ui] Improve dates display

--- packages/ui/src/lib/components/recipes/content/Aside.svelte
+++ packages/ui/src/lib/components/recipes/content/Aside.svelte
@@ -1,35 +1,49 @@
 <script lang="ts">
 	import type {AsideProps} from '$types'
+	import Feedback from '$lib/components/blocks/global/Feedback.svelte'
 
 	let {created, updated, series, children, page}: AsideProps = $props()
 
 	let colorClass = 'surface:1:primary'
 </script>
 
-<aside class="l:side hug l:stack:md justify:end">
+<aside class="l:side l:stack:md justify:end">
 	{#if updated && created}
-		<div
-			class="feedback:prose status:default bg:primary:100 variant:bare card:xs font:sm"
-		>
-			<p>Published: {created}</p>
-			{#if updated !== created}
-				<p>Updated: {updated}</p>
-			{/if}
+		<div class="l:burrito:lg">
+			<Feedback
+				status="default"
+				variant="bare"
+				size="sm"
+				context="prose"
+				asset="none"
+				container="stack"
+			>
+				<p class="l:flex justify:between">
+					<span class="emoji:published">Published</span>
+					<span class="maki:inline">{created}</span>
+				</p>
+				{#if updated !== created}
+					<p class="l:flex justify:between">
+						<span class="emoji:updated">Updated</span>
+						<span class="maki:inline">{updated}</span>
+					</p>
+				{/if}
+			</Feedback>
 		</div>
 	{/if}
 	<div class="l:stack:xs">
 		{#if series}
-				<p class="h5">{`Part ${page} of a ${series.length}-part series:`}</p>
-				<ol>
-					{#each series as item, index}
-						<li
-							aria-current={page && page === index + 1 ? 'page' : undefined}
-							class={page && page === index + 1 ? colorClass : ''}
-						>
-							<a href={item.link} class="card:2xs">{item.title}</a>
-						</li>
-					{/each}
-				</ol>
+			<p class="h5">{`Part ${page} of a ${series.length}-part series:`}</p>
+			<ol>
+				{#each series as item, index}
+					<li
+						aria-current={page && page === index + 1 ? 'page' : undefined}
+						class={page && page === index + 1 ? colorClass : ''}
+					>
+						<a href={item.link} class="card:2xs">{item.title}</a>
+					</li>
+				{/each}
+			</ol>
 		{/if}
 		{#if children}
 			{@render children()}
