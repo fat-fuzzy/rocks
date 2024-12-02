b85afeaa24bc0a48a692c729fa417a74999d8a16
patiboh <patiboh@users.noreply.github.com>
2024-12-02 08:40:24 +0100

    🌱 [ui] Clean types

--- apps/doc/src/lib/forms/services/ui-reveal.ts
+++ apps/doc/src/lib/forms/services/ui-reveal.ts
@@ -1,16 +1,15 @@
-import type {UiRevealState} from '$types'
 import constants from '$lib/types/constants.js'
 
 const {DEFAULT_REVEAL_STATE} = constants
 
 class UiReveal {
-	state: UiRevealState
+	state: {[key: string]: string}
 	id: string
 
 	/**
 	 * Initialize default state object or from the user's cookie values, if any
 	 */
-	constructor(state: UiRevealState | null = null, id: string) {
+	constructor(state: {[key: string]: string} | null = null, id: string) {
 		this.id = id
 		if (state) {
 			this.state = state
@@ -28,6 +27,7 @@ class UiReveal {
 		if (data.has(`state-${this.id}`)) {
 			updated = data.get(`state-${this.id}`)?.toString()
 		}
+
 		if (updated) {
 			this.state.reveal = updated
 			return {
