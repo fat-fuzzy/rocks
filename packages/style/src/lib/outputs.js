a637a43836f41ff1ee9da080658844c806ec1fa4
patiboh <patiboh@users.noreply.github.com>
2024-12-22 12:22:55 +0100

    🚧 [style+ui] Import CSS within blocks: buttons

---
+++ packages/style/src/lib/outputs.js
@@ -0,0 +1,27 @@
+/* Generated CSS variables */
+
+import './css/tokens/tokens.css'
+
+/* Global config */
+import './css/config/outputs.js'
+
+/* Resets and global styles, set initial and global styles */
+import './css/globals/outputs.js'
+
+/* Blocks (self contained, atomic components used in Recipes) */
+import './css/blocks/outputs.js'
+
+/* Layout (structural components used to organize content on a page or view) */
+import './css/layouts/outputs.js'
+
+/* Motion (animations, transitions, user-initiated movement) */
+import './css/motion/outputs.js'
+
+/* Compositions (components that implement common usage patterns using blocks and layouts) */
+import './css/recipes/outputs.js'
+
+/* Overrides */
+import './css/globals/visibility.css'
+
+/* Media Queries */
+import './css/mixins/index.css'
