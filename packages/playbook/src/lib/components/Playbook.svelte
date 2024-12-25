3283ced723617213585175a23bc78435bdb7d04f
patiboh <patiboh@users.noreply.github.com>
2024-12-25 22:24:52 +0100

    ♻️ [ui+playbook+doc] Move UI nav definitions to doc

--- packages/playbook/src/lib/components/Playbook.svelte
+++ packages/playbook/src/lib/components/Playbook.svelte
@@ -9,27 +9,11 @@
 
 	type Props = {
 		app: {settings: {[key: string]: string}}
-		path?: string
+		nav: any
 		children: Snippet
 	}
-	let {children, app, path = ''}: Props = $props()
+	let {children, app, nav}: Props = $props()
 
-	let formId = 'sidebar'
-	let reveal = $derived(
-		$page.form?.formId === formId
-			? $page.form.state
-			: $page.data.sidebar.reveal,
-	)
-
-	// TODO: move to utils / clean
-	function sortAsc(a, b) {
-		return a < b ? -1 : b < a ? 1 : 0
-	}
-
-	const tokenNames = Object.keys(fatFuzzyUi.tokens).sort(sortAsc)
-	const blockNames = Object.keys(fatFuzzyUi.blocks).sort(sortAsc)
-	const layoutNames = Object.keys(fatFuzzyUi.layouts).sort(sortAsc)
-	const recipeNames = Object.keys(fatFuzzyUi.recipes).sort(sortAsc)
 	let playbookContext: api.StylesApi = getContext('playbookContext')
 	setContext('playbookStore', playbookStore)
 
@@ -40,70 +24,6 @@
 	playbookStore.navReveal = ui?.navReveal || DEFAULT_NAV_REVEAL_STATE
 	playbookStore.settingsReveal = ui?.settingsReveal || DEFAULT_REVEAL_STATE
 	playbookStore.sidebarReveal = ui?.sidebarReveal || DEFAULT_NAV_REVEAL_STATE
-
-	let items = [
-		{
-			slug: 'ui', // root path of the Playbook
-			title: 'Library',
-			items: [
-				{
-					slug: 'tokens',
-					title: 'Tokens',
-					asset: 'tokens',
-					formaction: 'toggleTokens',
-					items: tokenNames.map((c) => ({
-						slug: c,
-						title: c,
-					})),
-				},
-				{
-					slug: 'blocks',
-					title: 'Blocks',
-					asset: 'blocks',
-					formaction: 'toggleBlocks',
-					items: blockNames.map((c) => ({
-						slug: c,
-						title: c,
-					})),
-				},
-				{
-					slug: 'layouts',
-					title: 'Layouts',
-					asset: 'layouts',
-					formaction: 'toggleLayouts',
-					items: layoutNames.map((c) => ({
-						slug: c,
-						title: c,
-					})),
-				},
-				{
-					slug: 'recipes',
-					title: 'Recipes',
-					asset: 'recipes',
-					formaction: 'toggleRecipes',
-					items: recipeNames.map((c) => ({
-						slug: c,
-						title: c,
-					})),
-				},
-			],
-		},
-	]
-
-	let nav = $derived({
-		path,
-		title: 'Content',
-		id: formId,
-		items,
-		reveal,
-		settings: {...playbookStore.app.settings},
-		breakpoint: 'sm',
-		size: 'md',
-		color: 'primary:600',
-		position: 'sticky',
-		place: 'left',
-		formaction: 'toggleSidebar',
-	})
 
 	onMount(() => {
 		if (styles) {
