562c086eccb64866fd5f9b9e74a10bd9497ae51c
patiboh <patiboh@users.noreply.github.com>
2025-01-11 10:37:12 +0100

    🐛 [doc] Fix media page actions

--- apps/doc/src/routes/(pages)/media/+page.server.ts
+++ apps/doc/src/routes/(pages)/media/+page.server.ts
@@ -1,5 +1,14 @@
 import {error} from '@sveltejs/kit'
+import uiActions from '$lib/forms/actions/ui-actions'
+import settingsActions from '$lib/forms/actions/settings-actions'
 
 export const load = async () => {
 	error(404, 'Not found')
+}
+
+export const actions = {
+	toggleNav: async (event) => uiActions.handleToggleNav(event),
+	toggleSettings: async (event) => uiActions.handleToggleSettings(event),
+	updateSettings: async (event) =>
+		settingsActions.handleUpdateAppSettings({event}),
 }
