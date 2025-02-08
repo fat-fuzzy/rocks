4222d0083a22c563ae33f6eebfebb37170d1c871
patiboh <patiboh@users.noreply.github.com>
2025-02-08 16:38:38 +0100

    🐛 [ui+doc] Revert to dompurify, detect browser for EscapeHtml

--- packages/ui/src/lib/components/blocks/global/EscapeHtml.svelte
+++ packages/ui/src/lib/components/blocks/global/EscapeHtml.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
 	import {browser} from '$app/environment'
-	import DOMPurify from 'isomorphic-dompurify'
+	import DOMPurify from 'dompurify'
 	import {onMount} from 'svelte'
 
 	let {id, html, size}: {id: string; html: string; size?: string} = $props()
+	let purify
 	let escaped = $state(html)
 	onMount(() => {
 		if (browser) {
-			escaped = DOMPurify.sanitize(html)
+			purify = DOMPurify(window)
+			escaped = purify.sanitize(html)
 		}
 	})
 </script>
 
-{#if escaped}
-	<div class={size ? `l:text:${size}` : ''} data-testid={`html-${id}`}>
-		{@html escaped}
-	</div>
-{/if}
+<div class={size ? `l:text:${size}` : ''} data-testid={`html-${id}`}>
+	{@html escaped}
+</div>
