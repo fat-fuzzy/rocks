96b88f8c82e13c29f48b448a2a4b5fdba8553a11
patiboh <patiboh@users.noreply.github.com>
2025-02-19 22:19:36 +0100

    🌈 [style+ui] Improve breacrumbs layout

--- packages/ui/src/lib/components/recipes/navs/Breadcrumbs.svelte
+++ packages/ui/src/lib/components/recipes/navs/Breadcrumbs.svelte
@@ -12,7 +12,6 @@
 		background,
 		container,
 		path,
-		breadcrumbTabs,
 	}: BreadcrumbsProps = $props()
 
 	let items: {slug: string; title: string; path: string}[] = $derived.by(() => {
@@ -45,35 +44,31 @@
 	)
 </script>
 
-<nav {id} class={`breadcrumbs w:full ${navClasses}`}>
-	<ul {id} class={`l:flex size:${size} align:center unstyled`} data-testid={id}>
+<nav {id} class={`breadcrumbs l:stack:${size} w:full ${navClasses}`}>
+	<svelte:element this={`h${level}`} id={title} class="l:flex align:center">
+		{title}
+	</svelte:element>
+	<ul
+		{id}
+		class={`l:flex variant:dotted size:${size} align:center unstyled`}
+		data-testid={id}
+	>
 		{#each items as item, i}
 			{@const font = i === items.length - 1 ? '' : 'font:xs'}
-			<li
-				aria-current={path === item.slug ? 'page' : undefined}
-				class={`l:flex nowrap align:center`}
-			>
-				<a
-					data-sveltekit-preload-data
-					href={item.path}
-					class={`l:flex ravioli:2xs align:center ${font}`}
+			{#if i < items.length - 1}
+				<li
+					aria-current={path === item.slug ? 'page' : undefined}
+					class={`l:flex nowrap align:center`}
 				>
-					{#if i === items.length - 1}
-						<svelte:element
-							this={`h${level}`}
-							id={title}
-							class="l:flex align:center"
-						>
-							{item.title}
-						</svelte:element>
-					{:else}
+					<a
+						data-sveltekit-preload-data
+						href={item.path}
+						class={`l:flex ravioli:2xs align:center ${font}`}
+					>
 						{item.title}
-					{/if}
-				</a>
-			</li>
+					</a>
+				</li>
+			{/if}
 		{/each}
 	</ul>
-	{#if breadcrumbTabs}
-		{@render breadcrumbTabs()}
-	{/if}
 </nav>
