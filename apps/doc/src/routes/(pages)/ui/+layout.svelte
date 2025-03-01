cad1a158597949f9bda24b76afdce6186c2ed0b0
patiboh <patiboh@users.noreply.github.com>
2025-03-01 23:45:15 +0100

    🌱 [doc] Cleanup

--- apps/doc/src/routes/(pages)/ui/+layout.svelte
+++ apps/doc/src/routes/(pages)/ui/+layout.svelte
@@ -3,7 +3,7 @@
 	import {onMount, setContext} from 'svelte'
 	import {page} from '$app/state'
 	import playbook from '@fat-fuzzy/playbook'
-	import PlaybookSettings from '$lib/forms/actions/page-actor.svelte'
+	import PlaybookSettings from '$lib/forms/actions/playbook-settings.svelte'
 
 	const {StylesApi, Playbook} = playbook
 
@@ -11,19 +11,16 @@
 		children: Snippet
 	}
 	let {children}: Props = $props()
+	let {styles, ui, settings} = $derived(page.data)
 
 	let playbookSettings = new PlaybookSettings()
-	let {styles, ui, settings} = $derived(page.data)
 	let context = new StylesApi()
 	setContext('playbookContext', context)
 	setContext('playbookSettings', playbookSettings)
+
 	onMount(() => {
 		if (ui) {
 			context.applyStyles(styles)
-			// context.Reveal = ui.Reveal
-			// context.RevealNav = ui.RevealNav
-			// context.HeaderRevealNav = ui.HeaderRevealNav
-			// context.HeaderRevealSettings = ui.HeaderRevealSettings
 		}
 		if (settings.brightness) {
 			playbookSettings.updateBrightness(settings.brightness)
@@ -34,6 +31,6 @@
 	})
 </script>
 
-<Playbook app={{settings}}>
+<Playbook app={playbookSettings.app}>
 	{@render children()}
 </Playbook>
