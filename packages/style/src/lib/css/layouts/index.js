659bb79b0d896fb59b0fb778e25356564949ce32
patiboh <patiboh@users.noreply.github.com>
2025-03-22 01:19:01 +0100

    🌶️ [style] Add cannelloni class, cleanup

--- packages/style/src/lib/css/layouts/index.js
+++ packages/style/src/lib/css/layouts/index.js
@@ -1,26 +1,33 @@
-/* == Base Layout == */
+/* == Layout staples == */
 import './text.css' /* Use for text content width */
 import './stack.css' /* Use for stacked column content */
 import './sidebar.css' /* Use for sidebar layouts = layouts containing two children: "main" and "side" */
 import './switcher.css' /* Use for items that switch between column and row layout when the container reaches a threshold width  */
-import './taco.css' /* Use for main content centering (wide columns with max-width set using: percentage - margin (in  em units))*/
-
-import './flex.css' /* Use for flex layouts */
-import './grid.css' /* Use for grid layouts base + gallery grid layout (equal sized areas) */
-import './align.css' /* Use for aligning items inside containers and setting widths */
-import './burrito.css' /* Use for side content centering (narrow columns with max-width set using sidebar width variables  (in ch units))*/
-import './maki.css' /* Use for side content centering without any max-width */
 import './frame.css' /* Use for displaying content with a given aspect ratio */
-import './ravioli.css' /* Use for displaying self-contained content items */
-import './shapes.css' /* Use to style geometric shapes */
-import './overflow.css' /* Use to manage content overflow*/
 
-/* == Reveal layouts == */
+/* Tortillas are for Wrapping */
+import './taco.css' /* Use for main content centering (wide columns with max-width set using: percentage - margin (in  em units))*/
+import './burrito.css' /* Use for side content centering (narrow columns with max-width set using sidebar width variables  (in ch units))*/
 
+/* Maki are for Margin */
+import './maki.css' /* Use for side content centering without any max-width */
+
+/* Pasta are for Padding */
+import './cannelloni.css' /* Use single axis padding  */
+import './ravioli.css' /* Use for all-round padding  */
+
+/* == Layout recipes == */
 import './reveal.css' /* Use for displaying hidden content with a button action */
 import './reveal-sticky.css' /* Use for displaying hidden content in response to a button action */
 import './details.css' /* Use to style <details> and <summary> elements */
-import './position.css' /* Use for to position elements */
 
-/* == Recipe layouts == */
+/* == Layout feasts  == */
 import './grid-rails.css' /* Use for base grid with page template areas and scrolling sidebars */
+
+/* == Layout spices  == */
+import './breakpoint.css' /* Breakpoint utilities */
+import './shapes.css' /* Use to style geometric shapes */
+import './flex.css' /* Use for flex layouts */
+import './grid.css' /* Use for grid layouts base + gallery grid layout (equal sized areas) */
+import './align.css' /* Use for aligning items inside containers and setting widths */
+import './position.css' /* Use for to position elements */
