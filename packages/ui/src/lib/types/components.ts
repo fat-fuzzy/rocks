cd6c5d38512d3a875de7d58a1213c850cd900879
patiboh <patiboh@users.noreply.github.com>
2025-03-26 08:16:38 +0100

    🌱 [ui] Add HeaderMetro component

--- packages/ui/src/lib/types/components.ts
+++ packages/ui/src/lib/types/components.ts
@@ -63,6 +63,7 @@ export interface Drafts {
 	Scrolly: typeof import('$lib/components/recipes/animations/scroll/Scrolly.svelte').default
 	ScrollyItem: typeof import('$lib/components/recipes/animations/scroll/ScrollyItem.svelte').default
 	HeaderNav: typeof import('$lib/components/recipes/header/HeaderNav.svelte').default
+	HeaderMetro: typeof import('$lib/components/recipes/header/HeaderMetro.svelte').default
 	Cookies: typeof import('$lib/components/recipes/forms/Cookies.svelte').default
 	Breadcrumbs: typeof import('$lib/components/recipes/navs/Breadcrumbs.svelte').default
 	PageNav: typeof import('$lib/components/recipes/navs/PageNav.svelte').default
