b1a02cd42dd27dc0572452f679fa6aba071d1740
patiboh <patiboh@users.noreply.github.com>
2025-04-06 22:11:38 +0200

    🔥 [style] Remove obsolete grid recipe styles

--- packages/style/src/lib/css/recipes/index.js
+++ packages/style/src/lib/css/recipes/index.js
@@ -4,16 +4,12 @@ import './nav.css' /* Use for navigation (navigation = links = hrefs = <a>) */
 import './breadcrumbs.css' /* Use for breadcrumbs navigation */
 import './menu.css' /* Use for menus (menus = actions -> use forms [for no-js] -> buttons = <button>) and derived elements */
 import './tabs.css' /* Use for tabs */
-import './context.css' /* Use for header context box */
 import './scrolly.css' /* Use with ScrollReveal component  */
 
 import './settings.css' /* A Settings menu component (for user input to adjust brightness, contrast, language, ...) */
 import './header-app.css' /* Use for main app header */
 import './footer.css' /* Use for main app footer */
 import './article.css' /* Use for articles (header + section(s) + optional footer) */
-
-import './grid-header.css' /* Use for a page header layout */
-import './grid-props.css' /* Use for a props inputs layouts */
 
 import './playbook.css' /* Use for API components (used in Doc website): TODO: move to playbook package */
 import './app.css' /* Use for app layout specifics */
