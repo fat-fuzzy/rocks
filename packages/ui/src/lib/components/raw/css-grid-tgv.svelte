2b8caeb0aafbe134eb8107dedde32662d4820616
patiboh <patiboh@users.noreply.github.com>
2025-04-21 23:24:38 +0200

    🌱 [style+ui] More rails grid alignment and cleanup

--- packages/ui/src/lib/components/raw/css-grid-tgv.svelte
+++ packages/ui/src/lib/components/raw/css-grid-tgv.svelte
@@ -13,43 +13,33 @@
 	<div
 		class="l:grid v-zone:2 surface:2:neutral align-self:stretch align-content:stretch"
 	>
-		<main class="l:grid v-zone:main surface:1:neutral">
+		<main class="l:grid v-zone:main surface:1:neutral text:center">
 			<div class="page-header ravioli:lg">
-				<h1 class="text:center color:neutral">🚄 TGV</h1>
+				<h1 class="color:neutral">🚄 TGV</h1>
 			</div>
 			<article class="page-main maki:inline size:lg">
 				<ul class="unstyled l:stack:lg">
-					<li
-						class="l:frame:video justify:center surface:0:neutral dotted font:2xl"
-					>
-						<p class="l:stack size:2xl emoji:default align:center">
+					<li class="l:frame:video surface:0:neutral dotted font:xl ravioli:xl">
+						<p class="l:stack size:2xl emoji:default align:center text:center">
 							Welcome to the TGV grid layout!
 						</p>
 					</li>
-					<li
-						class="l:frame:video justify:center surface:0:neutral dotted font:2xl"
-					>
+					<li class="l:frame:video surface:0:neutral dotted font:xl ravioli:xl">
 						<p class="l:stack size:2xl emoji:pause align:center">
 							Enjoy a fast and pleasant journey though the page
 						</p>
 					</li>
-					<li
-						class="l:frame:video justify:center surface:0:neutral dotted font:2xl"
-					>
+					<li class="l:frame:video surface:0:neutral dotted font:xl ravioli:xl">
 						<p class="l:stack size:2xl emoji:magic align:center">
 							Structure media content on wide sections
 						</p>
 					</li>
-					<li
-						class="l:frame:video justify:center surface:0:neutral dotted font:2xl"
-					>
+					<li class="l:frame:video surface:0:neutral dotted font:xl ravioli:xl">
 						<p class="l:stack size:2xl emoji:wave align:center justify:end">
 							Keep it optimized and simple
 						</p>
 					</li>
-					<li
-						class="l:frame:video justify:center surface:0:neutralx dotted font:2xl"
-					>
+					<li class="l:frame:video surface:0:neutral dotted font:xl ravioli:xl">
 						<p class="l:stack size:2xl emoji:rainbow align:center justify:end">
 							And enjoy the ride!
 						</p>
--- packages/ui/src/lib/components/raw/css-grid-tram.svelte
+++ packages/ui/src/lib/components/raw/css-grid-tram.svelte
@@ -47,109 +47,150 @@
 				<h1 class="color:accent">🚋 Tram</h1>
 			</div>
 			<div class="page-main maki:inline size:lg l:stack size:lg">
-				<ol class="unstyled l:grid:auto size:xs">
-					<li>
-						<div
-							class="l:frame:square surface:4:accent dotted l:stack size:md text:center"
-						>
-							<span class="emoji:recipes"></span>
-							<p>Welcome!</p>
-						</div>
-					</li>
-					<li>
-						<div
-							class="l:frame:square surface:4:accent dotted l:stack size:md text:center"
-						>
-							<span class="emoji:moby"></span>
-							<p>See the Whales</p>
-						</div>
-					</li>
-					<li>
-						<div
-							class="l:frame:square surface:4:accent dotted l:stack size:md text:center"
-						>
-							<span class="emoji:turtle"></span>
-							<p>and the Tigers</p>
-						</div>
-					</li>
-					<li>
-						<div
-							class="l:frame:square surface:4:accent dotted l:stack size:md text:center"
-						>
-							<span class="emoji:otter"></span>
-							<p>and the Otters</p>
-						</div>
-					</li>
-					<li>
-						<div
-							class="l:frame:square surface:4:accent dotted l:stack size:md text:center"
-						>
-							<span class="emoji:turtle"></span>
-							<p>and the Turtles</p>
-						</div>
-					</li>
-					<li>
-						<div
-							class="l:frame:square surface:4:accent dotted l:stack size:md text:center"
-						>
-							<span class="emoji:ollie"></span>
-							<p>and don't forget the Octopuses</p>
-						</div>
-					</li>
-					<li>
-						<div
-							class="l:frame:square surface:4:accent dotted l:stack size:md text:center"
-						>
-							<span class="emoji:tram"></span>
-							<p>on the CSS Tram</p>
-						</div>
-					</li>
-					<li>
-						<div
-							class="l:frame:square surface:4:accent dotted l:stack size:md text:center"
-						>
-							<span class="emoji:rainbow"></span>
-							<p>Enjoy the ride!</p>
-						</div>
-					</li>
-				</ol>
-				<details class="l:grid gare:nord surface:1:neutral">
-					<summary class="gare-control ravioli:3xs"> Article Context </summary>
-					<ff-reveal class="scroll:y layer:1">
-						<ol class="unstyled">
-							<li class="raviolink">Item</li>
-							<li class="raviolink">Item</li>
-							<li class="raviolink">Item</li>
-							<li class="raviolink">Item</li>
-							<li class="raviolink">Item</li>
-							<li class="raviolink">Item</li>
-							<li class="raviolink">Item</li>
-							<li class="raviolink">Item</li>
-							<li class="raviolink">Item</li>
-							<li class="raviolink">Item</li>
-						</ol>
-					</ff-reveal>
-				</details>
+				<article>
+					<h2 id="section-1" class="color:accent">Creatures Tour</h2>
+					<ol class="unstyled l:grid:auto size:xs">
+						<li>
+							<div
+								class="l:frame:square surface:4:accent dotted l:stack size:md text:center justify:center"
+							>
+								<span class="emoji:recipes"></span>
+								<p>Welcome!</p>
+							</div>
+						</li>
+						<li>
+							<div
+								class="l:frame:square surface:4:accent dotted l:stack size:md text:center justify:center"
+							>
+								<span class="emoji:moby"></span>
+								<p>See the Whales</p>
+							</div>
+						</li>
+						<li>
+							<div
+								class="l:frame:square surface:4:accent dotted l:stack size:md text:center justify:center"
+							>
+								<span class="emoji:tiger"></span>
+								<p>and the Tigers</p>
+							</div>
+						</li>
+						<li>
+							<div
+								class="l:frame:square surface:4:accent dotted l:stack size:md text:center justify:center"
+							>
+								<span class="emoji:otter"></span>
+								<p>and the Otters</p>
+							</div>
+						</li>
+						<li>
+							<div
+								class="l:frame:square surface:4:accent dotted l:stack size:md text:center justify:center"
+							>
+								<span class="emoji:turtle"></span>
+								<p>and the Turtles</p>
+							</div>
+						</li>
+						<li>
+							<div
+								class="l:frame:square surface:4:accent dotted l:stack size:md text:center justify:center"
+							>
+								<span class="emoji:ollie"></span>
+								<p>and don't forget the Octopuses</p>
+							</div>
+						</li>
+						<li>
+							<div
+								class="l:frame:square surface:4:accent dotted l:stack size:md text:center justify:center"
+							>
+								<span class="emoji:tram"></span>
+								<p>on the CSS Tram</p>
+							</div>
+						</li>
+						<li>
+							<div
+								class="l:frame:square surface:4:accent dotted l:stack size:md text:center justify:center"
+							>
+								<span class="emoji:rainbow"></span>
+								<p>Enjoy the ride!</p>
+							</div>
+						</li>
+					</ol>
+				</article>
+				<article>
+					<h2 id="section-2" class="color:warning">Halloween Tour</h2>
+					<ol class="unstyled l:grid:auto size:xs">
+						<li>
+							<div
+								class="l:frame:square surface:5:warning dotted l:stack size:md text:center justify:center"
+							>
+								<span class="emoji:fat-fuzzy"></span>
+								<p>Welcome ... if you Dare!</p>
+							</div>
+						</li>
+						<li>
+							<div
+								class="l:frame:square surface:5:warning dotted l:stack size:md text:center justify:center"
+							>
+								<span class="emoji:vader"></span>
+								<p>See the Bugs</p>
+							</div>
+						</li>
+						<li>
+							<div
+								class="l:frame:square surface:5:warning dotted l:stack size:md text:center justify:center"
+							>
+								<span class="emoji:perf"></span>
+								<p>and the Perf issues</p>
+							</div>
+						</li>
+						<li>
+							<div
+								class="l:frame:square surface:5:warning dotted l:stack size:md text:center justify:center"
+							>
+								<span class="emoji:error"></span>
+								<p>and the Errors</p>
+							</div>
+						</li>
+						<li>
+							<div
+								class="l:frame:square surface:5:warning dotted l:stack size:md text:center justify:center"
+							>
+								<span class="emoji:not-found"></span>
+								<p>and don't forget the 404s</p>
+							</div>
+						</li>
+						<li>
+							<div
+								class="l:frame:square surface:5:warning dotted l:stack size:md text:center justify:center"
+							>
+								<span class="emoji:tram"></span>
+								<p>on the CSS Tram</p>
+							</div>
+						</li>
+						<li>
+							<div
+								class="l:frame:square surface:5:warning dotted l:stack size:md text:center justify:center"
+							>
+								<span class="emoji:log"></span>
+								<p>Log an issue !</p>
+							</div>
+						</li>
+					</ol>
+				</article>
 			</div>
 		</main>
-		<aside class="l:grid page-context scroll:y surface:2:accent">
-			<details class="l:grid gare:nord surface:0:neutral">
-				<summary class="gare-control ravioli:3xs">Page Context</summary>
-				<ff-reveal class="scroll:y layer:1">
-					<ol class="unstyled">
-						<li class="raviolink">Item</li>
-						<li class="raviolink">Item</li>
-						<li class="raviolink">Item</li>
-						<li class="raviolink">Item</li>
-						<li class="raviolink">Item</li>
-						<li class="raviolink">Item</li>
-						<li class="raviolink">Item</li>
-						<li class="raviolink">Item</li>
-						<li class="raviolink">Item</li>
-						<li class="raviolink">Item</li>
-					</ol>
-				</ff-reveal>
-			</details>
+		<aside class="l:grid page-context scroll:y surface:0:accent">
+			<div class="l:stack maki:inline size:md">
+				<h4 class="maki:block">Tours:</h4>
+				<ul class="unstyled l:stack size:md">
+					<li class="ellipsis snap:center">
+						<a href="#section-1" class="raviolink">Creatures</a>
+					</li>
+					<li class="ellipsis snap:center">
+						<a href="#section-2" class="raviolink">Halloween</a>
+					</li>
+				</ul>
+			</div>
 		</aside>
 	</div>
 	<footer class="l:grid t-zone:4 surface:3:accent">
