e6609f527e432937f785605345f9ff99dc090fab
patiboh <patiboh@users.noreply.github.com>
2025-05-24 15:04:36 +0200

    🐛 [ui] Fix id

--- packages/ui/src/lib/components/blocks/global/Magic.svelte
+++ packages/ui/src/lib/components/blocks/global/Magic.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type {MagicProps} from '$types'
 	let {
+		id,
 		text,
 		spell,
 		level,
@@ -23,7 +24,7 @@
 
 <div
 	class={`ff:magic ${genieUno} align:center justify:between size:${size} ${growClass} ${variantClass}`}
-	data-testid="Magic"
+	data-testid={id}
 >
 	<ff-callout
 		class={`magic:${spell} ${levelClass} ${fontClass} ${grow ? 'grow:1 l:flex align:center justify:center' : ''}`}
