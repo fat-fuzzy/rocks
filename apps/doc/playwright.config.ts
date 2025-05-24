aa611f063cc7a1bf30972cd0de74c4e990ab00ef
patiboh <patiboh@users.noreply.github.com>
2025-05-24 12:53:53 +0200

    🔧 [config+doc] Improve e2e test runtimes

--- apps/doc/playwright.config.ts
+++ apps/doc/playwright.config.ts
@@ -1,4 +1,26 @@
+import type {PlaywrightTestConfig} from '@playwright/test'
+
 import config from '@fat-fuzzy/config'
 const {playwright} = config
 
-export default playwright
+const deviceScaleFactor = 1 // Monitor defaults
+
+const sharedOptions = {
+	deviceScaleFactor,
+}
+
+const playwrightConfig: PlaywrightTestConfig = {
+	...playwright,
+	timeout: 1000,
+	projects: [
+		{
+			name: 'Desktop',
+			testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+			use: {
+				...sharedOptions,
+			},
+		},
+	],
+}
+
+export default playwrightConfig
--- packages/config/src/playwright.config.js
+++ packages/config/src/playwright.config.js
@@ -1,12 +1,23 @@
-/** @type { import("@playwright/test").PlaywrightTestConfig } */
+const PORT = 4173
 
+/** @type { import("@playwright/test").PlaywrightTestConfig } */
 const config = {
+	timeout: 1000,
+	retries: 2,
+	workers: '50%',
+	maxFailures: undefined,
+	use: {
+		// screenshot: 'only-on-failure',
+		trace: 'retain-on-failure',
+		// Base URL to use in actions like `await page.goto('/')`.
+		baseURL: `http://localhost:${PORT}`,
+	},
 	webServer: {
 		command: 'pnpm build && pnpm preview', // This will run tests in watch mode
-		port: 4173,
+		port: PORT,
 	},
 	testDir: 'tests/e2e',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+	outputDir: 'tests/e2e/test-results',
 }
 
 export default config
