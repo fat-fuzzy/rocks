539863c8b3e1d0c9e27f97b98dd9be6d303cd177
patiboh <patiboh@users.noreply.github.com>
2025-05-29 17:28:30 +0200

    🔧 [config+sketch+prose+playbook+ui+doc] Update eslint config

--- packages/config/src/eslint.base.config.js
+++ packages/config/src/eslint.base.config.js
@@ -5,7 +5,6 @@ import svelte from 'eslint-plugin-svelte'
 import globals from 'globals'
 import {fileURLToPath} from 'node:url'
 import ts from 'typescript-eslint'
-import svelteConfig from './svelte.config.js'
 
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url))
 
@@ -13,7 +12,6 @@ export default ts.config(
 	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
 	...ts.configs.recommended,
-	...svelte.configs.recommended,
 	prettier,
 	...svelte.configs.prettier,
 	{
@@ -21,16 +19,5 @@ export default ts.config(
 			globals: {...globals.browser, ...globals.node},
 		},
 		rules: {'no-undef': 'off'},
-	},
-	{
-		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
-		languageOptions: {
-			parserOptions: {
-				projectService: true,
-				extraFileExtensions: ['.svelte'],
-				parser: ts.parser,
-				svelteConfig,
-			},
-		},
 	},
 )
