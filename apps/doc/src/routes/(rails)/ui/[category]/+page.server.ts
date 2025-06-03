a38f3e2c995f1d279072b85fc689719842fdb2b5
patiboh <patiboh@users.noreply.github.com>
2025-06-03 12:07:42 +0200

    🐛 [doc] Fix raw page layout

--- apps/doc/src/routes/(rails)/ui/[category]/+page.server.ts
+++ apps/doc/src/routes/(rails)/ui/[category]/+page.server.ts
@@ -1,9 +1,6 @@
 import {actions as parentActions} from '../+page.server'
 
-export const load = async ({params}) => {
-	if (params.category === 'raw') {
-		return {layout: 'voyager'}
-	}
+export const load = async () => {
 	return {layout: 'tram'}
 }
 
