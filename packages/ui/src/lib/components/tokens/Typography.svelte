4cf54a04e68e0bdaceb7cfbfd469a5e4b54b5838
patiboh <patiboh@users.noreply.github.com>
2025-06-04 11:18:33 +0200

    🐛 [playbook] Fix collection items' markup structure (2)

--- packages/ui/src/lib/components/tokens/Typography.svelte
+++ packages/ui/src/lib/components/tokens/Typography.svelte
@@ -1,26 +1,20 @@
 <script lang="ts">
 	type Props = {
-		layout: string
-		size: string
 		color: string
 	}
-	let {layout = 'text', size = 'md', color}: Props = $props()
+	let {color}: Props = $props()
 
 	const headings = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
 	const body = ['p', 'pre']
-
-	let ravioliClasses = `ravioli:md l:${layout}:${size}`
 </script>
 
-<article class={ravioliClasses}>
-	{#each headings as text}
-		<svelte:element this={text} class={color}>
-			Fox Jumps Over Dog
-		</svelte:element>
-	{/each}
-	{#each body as text}
-		<svelte:element this={text}>
-			The quick brown fox jumps over the lazy dog
-		</svelte:element>
-	{/each}
-</article>
+{#each headings as text, i (i)}
+	<svelte:element this={text} class={color}>
+		Fox Jumps Over Dog
+	</svelte:element>
+{/each}
+{#each body as text, i (i)}
+	<svelte:element this={text}>
+		The quick brown fox jumps over the lazy dog
+	</svelte:element>
+{/each}
