3d41f841474345712a65e8a138fa1f7bcd0c4c6f
patiboh <patiboh@users.noreply.github.com>
2025-06-15 08:18:47 +0200

    🔥 [style] Clean unused styles

--- packages/style/src/lib/css/recipes/index.js
+++ packages/style/src/lib/css/recipes/index.js
@@ -8,7 +8,6 @@ import './scrolly.css' /* Use with ScrollReveal component  */
 
 import './settings.css' /* A Settings menu component (for user input to adjust brightness, contrast, language, ...) */
 import './header-app.css' /* Use for main app header */
-import './footer.css' /* Use for main app footer */
 import './article.css' /* Use for articles (header + section(s) + optional footer) */
 
 import './playbook.css' /* Use for API components (used in Doc website): TODO: move to playbook package */
