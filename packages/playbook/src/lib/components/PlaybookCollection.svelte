11d09a15bb53092c59fc4ab6d5b526a822a84b4c
patiboh <patiboh@users.noreply.github.com>
2025-07-14 15:07:28 +0200

    🐛 [doc+playbook] Fix UI page + collection pages: use of sections

--- packages/playbook/src/lib/components/PlaybookCollection.svelte
+++ packages/playbook/src/lib/components/PlaybookCollection.svelte
@@ -156,7 +156,13 @@
 	>
 		{#snippet main()}
 			<div class="l:stack:2xl">
-				<EscapeHtml id="doc" html={content.html || ''} size="md" font="md" />
+				<EscapeHtml
+					id="doc"
+					html={content.html || ''}
+					size="md"
+					font="md"
+					element="section"
+				/>
 
 				<section id="playbook" class="l:stack:2xl">
 					<div class="w:full l:flex justify:center">
