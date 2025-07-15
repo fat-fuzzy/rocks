2d8447cf50cd6764c9a9c1bb0b0b34905b93442d
patiboh <patiboh@users.noreply.github.com>
2025-07-15 09:22:10 +0200

    🚧 [gfx] Add Wing Feathers base

---
+++ packages/gfx/src/lib/sketches/fat-fuzzy/002-wing-feathers/index.js
@@ -0,0 +1,159 @@
+// @ts-check
+/**
+ ***********************
+ * DRAW FUNCTIONS
+ ***********************
+ */
+import dom from '../../../dom'
+import Wing from './wing-002.js'
+import props from '../props'
+import setup from '../../../webgl/setup'
+import {drawScene} from './draw-scene'
+import {initBuffers} from '../../../webgl/buffers/geometry-2d'
+
+import {frag} from './shaders/fragment-shader'
+import {vert} from './shaders/vertex-shader-2d'
+
+let gl
+let program
+let buffers
+let vertexShader
+let fragmentShader
+let error
+let programInfo = {
+	errors: [],
+}
+let bgColor = [0.0298, 0.02089, 0.1233]
+let {WING, BONES, FEATHERS, COLORS} = props
+
+// Initialize the wing here to maintain color across main calls
+// TODO: chose color mode
+let wing
+
+let meta = {
+	project: 'fat-fuzzy',
+	id: '002',
+	slug: 'wing-feathers',
+	title: 'Wing Feathers',
+	asset: 'feather',
+	// status: 'draft',
+	categories: ['Projects'],
+	tags: ['2D', 'webgl', 'matrix', 'sketches', 'wing', 'fat-fuzzy'],
+	controls: ['speed', 'color', 'grid', 'loop'],
+}
+
+function init(canvas) {
+	// Initialize the GL context
+	gl = canvas.getContext('webgl2', {preserveDrawingBuffer: true})
+
+	// Only continue if WebGL is available and working
+	if (gl === null) {
+		throw Error(
+			'Unable to initialize WebGL. Your browser or machine may not support it.',
+		)
+	}
+}
+
+async function main(canvas) {
+	init(canvas)
+	clear()
+	programInfo = await Promise.resolve(loadProgram(canvas))
+	bgColor = programInfo.context.background
+	return programInfo.context
+}
+
+function loadProgram(canvas) {
+	vertexShader = setup.compile(gl, gl.VERTEX_SHADER, vert)
+	fragmentShader = setup.compile(gl, gl.FRAGMENT_SHADER, frag)
+
+	if (vertexShader && fragmentShader) {
+		program = setup.link(gl, vertexShader, fragmentShader)
+		if (program) {
+			gl.useProgram(program)
+		}
+	}
+	dom.resize(canvas)
+
+	wing = new Wing({
+		name: 'wing-002',
+		position: [0, 0],
+		direction: WING.direction,
+		step: WING.currentStep,
+		layers: 1,
+		steps: WING.steps,
+		pause: WING.pause,
+		bones: BONES,
+		feathers: FEATHERS,
+		colors: COLORS,
+		drawFeathers: true,
+		canvasWidth: canvas.width,
+		canvasHeight: canvas.height,
+	})
+
+	wing.init(canvas.width, canvas.height)
+
+	// Collect all the info needed to use the shader program.
+	// Look up which attribute our shader program is using
+	// for aVertexPosition and look up uniform locations.
+	let _programInfo = {
+		program,
+		attribLocations: {
+			a_position: gl.getAttribLocation(program, 'a_position'),
+		},
+		uniformLocations: {
+			// bind u_color
+			u_color: gl.getUniformLocation(program, 'u_color'),
+			// bind u_resolution
+			u_resolution: gl.getUniformLocation(program, 'u_resolution'),
+			// bind u_translation
+			u_matrix: gl.getUniformLocation(program, 'u_matrix'),
+		},
+		context: wing.getGeometryCoords(),
+		errors: [],
+	}
+
+	clear()
+
+	buffers = initBuffers(gl, _programInfo)
+	error = gl.getError()
+	if (error !== gl.NO_ERROR) {
+		_programInfo.errors.push(error)
+	}
+	return _programInfo
+}
+
+function draw() {
+	update()
+	drawScene(gl, programInfo, buffers)
+}
+
+function update() {
+	wing.updateWingState()
+	programInfo.context = wing.getGeometryCoords()
+	buffers = initBuffers(gl, programInfo)
+}
+
+function clear() {
+	if (!gl) {
+		// TODO: handle error
+		return
+	}
+	gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
+	// Set the clear color to darkish green.
+	gl.clearColor(...bgColor, 1.0)
+	// Clear the context with the newly set color. This is
+	// the function call that actually does the drawing.
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+}
+
+function stop() {
+	clear()
+	if (buffers) {
+		if (buffers.position) gl.deleteBuffer(buffers.position)
+	}
+	if (vertexShader) gl.deleteShader(vertexShader)
+	if (fragmentShader) gl.deleteShader(fragmentShader)
+	if (programInfo.program) gl.deleteProgram(programInfo.program)
+}
+
+export default {init, meta, main, draw, update, clear, stop}
---
+++ packages/gfx/src/lib/sketches/fat-fuzzy/002-wing-feathers/wing-002.js
@@ -0,0 +1,88 @@
+import vectors from '../../../math/vectors'
+import Wing from '../wing.js'
+
+class Wing002 extends Wing {
+	constructor({
+		name,
+		position,
+		direction,
+		step,
+		layers,
+		steps,
+		pause,
+		bones,
+		feathers,
+		colors,
+		drawFeathers = false,
+		canvasWidth,
+		canvasHeight,
+	}) {
+		super({
+			name,
+			position,
+			direction,
+			step,
+			layers,
+			steps,
+			pause,
+			bones,
+			feathers,
+			colors,
+			drawFeathers,
+			canvasWidth,
+			canvasHeight,
+		})
+	}
+
+	getFeatherVertices(angle, bone, magnitude, origin, vectorVertices) {
+		let [x, y] = origin
+
+		let featherMagnitude = this.magnitudes.feathers[bone - 1].beginning
+		let insertionOrigin
+		let insertionDistance = 0
+		let featherCount = this.magnitudes.feathers[bone - 1].featherCount
+		let featherAngles = this.angles.feathers[this.currentTime]
+		let featherAngle = featherAngles[bone - 1]
+
+		let distance = magnitude / featherCount
+
+		for (let step = 0; step < featherCount; step++) {
+			// Save current coordinate system
+			let angleStep = step + 1
+			if (bone === 1) {
+				insertionDistance = distance * step
+				featherMagnitude = 30 + step * step
+				featherAngle = featherAngle + angleStep
+			}
+			if (bone === 2) {
+				insertionDistance = distance * step
+				featherMagnitude = 20 + step * step
+				featherAngle = featherAngle
+			}
+			if (bone === 3) {
+				insertionDistance = distance * step
+				featherMagnitude = 50 + step * step
+				featherAngle = featherAngle + angleStep * 1.5
+			}
+
+			insertionOrigin = vectors.getIntersectionPoint(
+				x,
+				y,
+				insertionDistance,
+				magnitude,
+			)
+
+			// New Wing Coordinates
+
+			let featherX = insertionOrigin[0] + featherMagnitude * featherAngle
+			let featherY = insertionOrigin[1] + featherMagnitude * featherAngle
+
+			// Draw the feather
+			vectorVertices.push(featherX, featherY)
+			// Draw the feather
+			vectorVertices.push(origin[0], origin[1])
+		}
+	}
+}
+
+export default Wing002
