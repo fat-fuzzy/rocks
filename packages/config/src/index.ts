997c7ede3609057438d7a7f908f2847af281c0c6
patiboh <patiboh@users.noreply.github.com>
2025-07-27 18:28:08 +0200

    ♻️ [config] Group frontend specific configs

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,3 +1,3 @@
 export { default as prettier } from './prettier.config.js';
-export { default as playwright } from './playwright.config.js';
-export { default as stylelint } from './stylelint.config.js';
+export { default as playwright } from './front/playwright.config.js';
+export { default as stylelint } from './front/stylelint.config.js';
