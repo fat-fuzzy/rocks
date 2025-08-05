922060c684505fa6ea8ae32feafefa53daf1d7a4
patiboh <patiboh@users.noreply.github.com>
2025-08-05 12:56:23 +0200

    🐛 [style] Test new rollup config to fix concurrency bug

--- packages/style/rollup.config.js
+++ packages/style/rollup.config.js
@@ -6,6 +6,10 @@ import postcssMinify from '@csstools/postcss-minify'
 import fs from 'node:fs'
 import path from 'node:path'
 
+import mainConfig from './rollup.config.main.js'
+import libConfig from './rollup.config.lib.js'
+import scssConfig from './rollup.config.scss.js'
+
 const production = process.env.NODE_ENV === 'production'
 const inDir = path.resolve('src/lib')
 const outDir = path.resolve('dist')
@@ -16,27 +20,6 @@ fs.rmSync(outDir, {recursive: true, force: true})
 fs.mkdirSync(outDir)
 
 /**
- * This config will preserve folder structure of source files (scss and css, not json) and output a css library in "dist/lib/"
+ * This config will run each config in order and output a css library in "dist/lib/"
  */
-export default {
-	input: `${inDir}/index.js`,
-	output: {dir: outDir, format: 'esm'},
-	plugins: [
-		scss({
-			input: cssFiles,
-			output: function (styles, styleNodes) {
-				Object.entries(styleNodes).forEach(([id, content]) => {
-					const outputPath = path.join('dist', path.relative('src', id))
-					fs.mkdirSync(path.dirname(outputPath), {recursive: true})
-					fs.writeFileSync(outputPath, content)
-				})
-			}, // will output compiled styles to "dist/lib/**/*.css" as separate files including the folder structure. Use in conjunction with exports in package.json to expose css files
-		}),
-		postcss({
-			extract: true,
-			plugins: [postcssMinify(), autoprefixer()],
-			minimize: production,
-			sourceMap: !production,
-		}),
-	],
-}
+export default [scssConfig, mainConfig, libConfig]
---
+++ packages/style/rollup.config.lib.js
@@ -0,0 +1,42 @@
+import * as glob from 'glob'
+import postcss from 'rollup-plugin-postcss'
+import scss from 'rollup-plugin-scss'
+import autoprefixer from 'autoprefixer'
+import postcssMinify from '@csstools/postcss-minify'
+import fs from 'node:fs'
+import path from 'node:path'
+
+const production = process.env.NODE_ENV === 'production'
+const inDir = path.resolve('src/lib')
+const outDir = path.resolve('dist')
+const cssFiles = glob.sync('src/lib/css/**/*.css')
+
+// Always clean the dist folder before building.
+fs.rmSync(outDir, {recursive: true, force: true})
+fs.mkdirSync(outDir)
+
+/**
+ * This config will preserve folder structure of source files (scss and css, not json) and output a css library in "dist/lib/"
+ */
+export default {
+	input: `${inDir}/index.js`,
+	output: {dir: outDir, format: 'esm'},
+	plugins: [
+		scss({
+			input: cssFiles,
+			output: function (styles, styleNodes) {
+				Object.entries(styleNodes).forEach(([id, content]) => {
+					const outputPath = path.join('dist', path.relative('src', id))
+					fs.mkdirSync(path.dirname(outputPath), {recursive: true})
+					fs.writeFileSync(outputPath, content)
+				})
+			}, // will output compiled styles to "dist/lib/**/*.css" as separate files including the folder structure. Use in conjunction with exports in package.json to expose css files
+		}),
+		postcss({
+			extract: true,
+			plugins: [postcssMinify(), autoprefixer()],
+			minimize: production,
+			sourceMap: !production,
+		}),
+	],
+}
