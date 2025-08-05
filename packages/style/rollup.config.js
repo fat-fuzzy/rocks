068cf987fe69f5b4b8be9a8dedfb8ad2f914b6bc
patiboh <patiboh@users.noreply.github.com>
2025-08-05 12:59:25 +0200

    🔥 [style] Cleanup

--- packages/style/rollup.config.js
+++ packages/style/rollup.config.js
@@ -1,8 +1,3 @@
-import * as glob from 'glob'
-import postcss from 'rollup-plugin-postcss'
-import scss from 'rollup-plugin-scss'
-import autoprefixer from 'autoprefixer'
-import postcssMinify from '@csstools/postcss-minify'
 import fs from 'node:fs'
 import path from 'node:path'
 
@@ -10,10 +5,7 @@ import mainConfig from './rollup.config.main.js'
 import libConfig from './rollup.config.lib.js'
 import scssConfig from './rollup.config.scss.js'
 
-const production = process.env.NODE_ENV === 'production'
-const inDir = path.resolve('src/lib')
 const outDir = path.resolve('dist')
-const cssFiles = glob.sync('src/lib/css/**/*.css')
 
 // Always clean the dist folder before building.
 fs.rmSync(outDir, {recursive: true, force: true})
