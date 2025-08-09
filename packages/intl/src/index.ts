023a4983b635c3f8579c9ebc9a4a5bf48ba8b9aa
patiboh <patiboh@users.noreply.github.com>
2025-08-09 20:19:04 +0200

    🌟 [intl+sketch] New DateTimeFormatter helper, use for snaps

--- packages/intl/src/index.ts
+++ packages/intl/src/index.ts
@@ -1,4 +1,5 @@
 /// <reference path="./types/index.d.ts" />
-import L10nFormatter from './L10n/index.js'
+import L10nFormatter from './L10n/formatter-L10n.js'
+import DatetimeFormatter from './L10n/formatter-datetime.js'
 
-export default {L10nFormatter}
+export default {L10nFormatter, DatetimeFormatter}
