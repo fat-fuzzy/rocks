125b5106823c86ae9b16e0b3bc63f8cd42766775
patiboh <patiboh@users.noreply.github.com>
2025-08-16 23:39:12 +0200

    🚧 [doc] Set Permissions-Policy

--- apps/doc/src/hooks.server.ts
+++ apps/doc/src/hooks.server.ts
@@ -1,10 +1,13 @@
 import type {Handle} from '@sveltejs/kit'
 import {sequence} from '@sveltejs/kit/hooks'
-
 import {setStyles} from '$lib/server/hooks/setStyles'
-import {setSecHeaders} from '$lib/server/hooks/setSecHeaders'
+import {
+	setSecHeaders,
+	setPermissionsPolicy,
+} from '$lib/server/hooks/setSecHeaders'
 
 export const handle: Handle = sequence(
 	setSecHeaders(),
+	setPermissionsPolicy(),
 	setStyles(),
 ) satisfies Handle
--- apps/doc/src/lib/server/hooks/setSecHeaders.ts
+++ apps/doc/src/lib/server/hooks/setSecHeaders.ts
@@ -6,6 +6,56 @@
  */
 import type {Handle} from '@sveltejs/kit'
 
+const PERMISSIONS_POLICY_DIRECTIVES = [
+	'accelerometer=()',
+	'ambient-light-sensor=()',
+	'attribute-reporting=()',
+	'autoplay=()',
+	'bluetooth=()',
+	'browsing-topics=()',
+	'camera=()',
+	'captured-surface-control=()',
+	'compute-pressure=()',
+	// 'cross-origin-isolated=()',
+	// 'deferred-fetch=(self)',
+	// 'deferred-fetch-minimal=(self)',
+	// 'display-capture=(self)',
+	'encrypted-media=()',
+	'fullscreen=()',
+	'geolocation=()',
+	'gyroscope=()',
+	'hid=()',
+	// 'identity-credentials-get=()',
+	// 'idle-detection=()',
+	'local-fonts=(self)',
+	'magnetometer=()',
+	'microphone=()',
+	'midi=()',
+	// 'otp-credentials=()',
+	'payment=()',
+	'picture-in-picture=()',
+	// 'publickey-credentials-create=()',
+	// 'publickey-credentials-get=()',
+	'screen-wake-lock=()',
+	'serial=()',
+	// 'storage-access=(self)',
+	// 'summarizer=()',
+	'magnetometer=()',
+	'usb=()',
+	// 'web-share=(self)',
+	// 'window-management=()',
+	'xr-spatial-tracking=()',
+]
+
+export const setPermissionsPolicy =
+	(): Handle =>
+	async ({event, resolve}) => {
+		event.setHeaders({
+			'Permissions-Policy': PERMISSIONS_POLICY_DIRECTIVES.join('; '),
+		})
+		return resolve(event)
+	}
+
 export const setSecHeaders =
 	(): Handle =>
 	async ({event, resolve}) => {
