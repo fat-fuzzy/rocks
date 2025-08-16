92677ce591586efe8178a4ff9774199538df3648
patiboh <patiboh@users.noreply.github.com>
2025-08-16 21:07:05 +0200

    🐛 [doc] Revert Beacon to js

--- apps/doc/src/lib/ui/Beacon.svelte
+++ apps/doc/src/lib/ui/Beacon.svelte
@@ -1,8 +1,7 @@
-<style>
-	:global(body:hover) {
-		border-image: url('https://fat-fuzzy.goatcounter.com/count?p=/test');
-		position: absolute;
-		top: 0;
-		left: 0;
-	}
-</style>
+<svelte:element
+	this={'script'}
+	async
+	src="//gc.zgo.at/count.js"
+	data-goatcounter="https://fat-fuzzy.goatcounter.com/count"
+>
+</svelte:element>
