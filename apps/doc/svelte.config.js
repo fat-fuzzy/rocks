4c1c816aec029563707693babf8f9122d9fdce5c
patiboh <patiboh@users.noreply.github.com>
2025-08-17 09:19:15 +0200

    ✏️ [doc] Add TODOs & known issues

--- apps/doc/svelte.config.js
+++ apps/doc/svelte.config.js
@@ -45,7 +45,13 @@ const config = {
 		 * - https://scotthelme.co.uk/content-security-policy-an-introduction/
 		 * - https://svelte.dev/docs/kit/configuration#csp
 		 *
-		 * TODO: Fix CSP-related issues: tests fail, safari issues
+		 * TODO:
+		 * Known SvelteKit Issues with TrustedType:
+		 * - https://github.com/sveltejs/svelte/issues/10826
+		 * - https://github.com/sveltejs/svelte/issues/14438
+		 * Plus:
+		 * - tests fail
+		 * - safari has issues in dev environment
 		 */
 		csp: {
 			mode: 'auto',
