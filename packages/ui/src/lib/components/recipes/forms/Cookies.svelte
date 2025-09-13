f4f2e4c87ab8c515013f04bd30bcfbc04e1af20b
patiboh <patiboh@users.noreply.github.com>
2025-09-13 17:01:39 +0200

    🎙️ [doc] Clarify cookies options

--- packages/ui/src/lib/components/recipes/forms/Cookies.svelte
+++ packages/ui/src/lib/components/recipes/forms/Cookies.svelte
@@ -45,7 +45,7 @@
 	let submitDisabled: boolean | undefined = $state(undefined)
 	let title = 'Cookies'
 	let description = '🍪 This website uses cookies 🍪'
-	let successMessage = `Your cookie preferences have been saved!`
+	let successMessage = `Your cookie preferences have been saved! Your can change them any time the footer 👇`
 	let fixed = $derived(!consent ? 'bottom-right' : undefined)
 
 	// TODO: Integrate inputTypes into validator from schema
