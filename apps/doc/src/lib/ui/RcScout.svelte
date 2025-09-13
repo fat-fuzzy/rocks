38cc7c3a3dc84161594fde510a99dc0ec9d3256e
patiboh <patiboh@users.noreply.github.com>
2025-09-13 18:45:18 +0200

    🌈 [doc] Restore RC logo & referrer

--- apps/doc/src/lib/ui/RcScout.svelte
+++ apps/doc/src/lib/ui/RcScout.svelte
@@ -2,6 +2,10 @@
 	Want to become a better programmer?
 	<div class="l:flex nowrap align:center justify:center">
 		<ff-icon class="rc-scout__logo size:lg"></ff-icon>
-		<a href="https://www.recurse.com/">Join the Recurse Center!</a>
+		<a
+			href="https://www.recurse.com/scout/click?t=ada7a603d0f8b6821628c0d99bffe90f"
+		>
+			Join the Recurse Center!
+		</a>
 	</div>
 </div>
