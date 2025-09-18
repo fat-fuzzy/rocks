426a61761f4ac16ea0f6b091c1d65b39ec3b91d6
patiboh <patiboh@users.noreply.github.com>
2025-09-18 12:59:35 +0200

    ♻️ [ui+doc] Move theme meta tags to html base

--- packages/ui/src/lib/components/blocks/global/Head.svelte
+++ packages/ui/src/lib/components/blocks/global/Head.svelte
@@ -17,14 +17,4 @@
 <svelte:head>
 	<title>{fullTitle}</title>
 	<meta name="description" content={description} />
-	<meta
-		name="theme-color"
-		media="(prefers-color-scheme: light)"
-		content="day"
-	/>
-	<meta
-		name="theme-color"
-		media="(prefers-color-scheme: dark)"
-		content="night"
-	/>
 </svelte:head>
