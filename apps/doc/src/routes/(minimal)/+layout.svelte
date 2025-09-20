d3bbc3fbd94166822c1585fb1ad9413edf631d7f
patiboh <patiboh@users.noreply.github.com>
2025-09-20 19:36:24 +0200

    🌟 [doc] Enable full-screen mode

--- apps/doc/src/routes/(minimal)/+layout.svelte
+++ apps/doc/src/routes/(minimal)/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type {Snippet} from 'svelte'
+	import {onMount, type Snippet} from 'svelte'
 
 	import {page} from '$app/state'
 	import ui from '@fat-fuzzy/ui'
@@ -12,11 +12,41 @@
 	const {LayoutSidebar} = ui.content
 
 	let appContext = $derived(page.data.appContext)
+	let isTalkPage = $derived(page.params.talk)
+	let zoneContent: HTMLElement | undefined = $state()
+
+	function toggleFullScreen() {
+		if (!isTalkPage || !zoneContent) return
+
+		if (zoneContent.requestFullscreen) {
+			zoneContent.requestFullscreen()
+		} else {
+			// Otherwise exit the full screen
+			document.exitFullscreen?.()
+		}
+	}
+
+	onMount(() => {
+		if (!isTalkPage || !zoneContent) return
+
+		// On pressing ENTER call toggleFullScreen method
+		document.addEventListener('keydown', (e) => {
+			if (e.key === 'Enter') {
+				toggleFullScreen()
+			}
+		})
+
+		return () => {
+			document?.removeEventListener('keydown', () => {})
+		}
+	})
 </script>
 
 <LayoutSidebar app={appContext}>
 	{#if children}
-		{@render children()}
+		<div bind:this={zoneContent} class="zone-content">
+			{@render children()}
+		</div>
 	{:else}
 		<p class="feedback bare emoji:default">Coming Soon!</p>
 	{/if}
