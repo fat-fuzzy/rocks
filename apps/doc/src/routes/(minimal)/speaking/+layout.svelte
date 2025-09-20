8845e32f68871e1794f274f843f7a4eff2169d2e
patiboh <patiboh@users.noreply.github.com>
2025-09-20 19:21:27 +0200

    🌱 [doc] Adapt rails layout for slides

---
+++ apps/doc/src/routes/(minimal)/speaking/+layout.svelte
@@ -0,0 +1,150 @@
+<script lang="ts">
+	import type {Snippet} from 'svelte'
+
+	import ui from '@fat-fuzzy/ui'
+	import {page} from '$app/state'
+	import Footer from '$lib/ui/Footer.svelte'
+	import Socials from '$lib/ui/Socials.svelte'
+
+	const {Cookies} = ui.drafts
+	const {RevealNav} = ui.recipes
+	const {LayoutGrid} = ui.content
+	const {Magic} = ui.blocks
+	import {linksSocials} from '$data/nav'
+
+	type Props = {
+		children: Snippet
+	}
+	let {children}: Props = $props()
+
+	let sidenav = $derived(page.data.sidebar)
+	let layout = $derived(page.data.layout ?? sidenav.layout)
+	let appContext = $derived(page.data.appContext)
+	let slide = $derived(page.data.content)
+
+	type AreaZone = {
+		zone: Snippet
+		grid?: boolean
+		gare?: string
+		scroll?: string
+		exchange?: boolean
+		hug?: boolean
+		tag?: string
+	}
+
+	const areas: AreaZone[] = $derived([
+		{
+			zone: zoneHeader,
+			grid: true,
+			exchange: true,
+			tag: 'header',
+		},
+		{
+			zone: zoneContent,
+			// hug: layout === 'steam' || layout === 'tram' || layout === 'urbanist',
+			grid: true,
+		},
+		{
+			zone: zoneFooter,
+			grid: true,
+		},
+	])
+
+	let series = $derived(
+		slide.meta.series && slide.meta.index > 0
+			? slide.meta.series.items.map((id) =>
+					page.data.markdowns.find((slide) => slide.meta.id === id),
+				)
+			: null,
+	)
+</script>
+
+<LayoutGrid
+	{layout}
+	{areas}
+	{sidenav}
+	size="3xs"
+	app={appContext}
+	path={page.url.pathname}
+/>
+
+{#snippet zoneHeader()}
+	<div
+		class={`${sidenav.reveal ?? ''} sidebar surface:0:neutral l:grid size:3xs align:center width:lg height:sm`}
+	>
+		{#if sidenav.layout === 'tgv'}
+			<div class="app-name">
+				<Magic spell="fuzzy" size="3xs" mask="text">
+					<div class="l:flex align:center justify:center">
+						<ff-icon class="emoji:home font:lg"></ff-icon>
+						<p class="font:h1 font:lg">Fat Fuzzy</p>
+					</div>
+				</Magic>
+			</div>
+		{:else}
+			<RevealNav
+				{...sidenav}
+				position={false}
+				area="gare"
+				place="ouest"
+				layout="rails"
+				scroll="y"
+				layer={1}
+				justify="evenly"
+				size="3xs"
+				font="xs"
+				width="lg"
+				height="sm"
+				background="neutral"
+				dismiss="outside"
+			/>
+		{/if}
+	</div>
+{/snippet}
+
+{#snippet zoneContent()}
+	{#if children}
+		{@render children()}
+	{:else}
+		<p class="feedback bare emoji:default">Coming Soon!</p>
+	{/if}
+{/snippet}
+
+{#snippet zoneFooter()}
+	<Footer>
+		<nav id="slides-nav" class="l:flex align:center justify:end w:full">
+			{#if slide.meta.index === 0}
+				<a
+					href={`/speaking/${slide.meta.talk}/slide-1`}
+					class="emoji:start justify:start"
+				>
+					{`Start`}
+				</a>
+			{:else}
+				{#if slide.meta.index === 1}
+					<p class="emoji:default justify:start">Intro</p>
+				{/if}
+				{#if slide.meta.index > 1}
+					<a
+						href={`/speaking/${slide.meta.talk}/slide-${slide.meta.index - 1}`}
+						class="emoji:point-left justify:start"
+					>
+						{`Slide ${slide.meta.index - 1}`}
+					</a>
+				{/if}
+				/
+				{#if slide.meta.index < series?.length}
+					<a
+						href={`/speaking/${slide.meta.talk}/slide-${slide.meta.index + 1}`}
+						class="emoji:point-right justify:end"
+					>
+						{`Slide ${slide.meta.index + 1}`}
+					</a>
+				{/if}
+				{#if slide.meta.index === series?.length}
+					<p class="emoji:end justify:start">La Fin!</p>
+				{/if}
+			{/if}
+		</nav>
+	</Footer>
+{/snippet}
--- apps/doc/src/routes/(minimal)/speaking/[talk]/[slug]/+page.svelte
+++ apps/doc/src/routes/(minimal)/speaking/[talk]/[slug]/+page.svelte
@@ -2,29 +2,21 @@
 	import {page} from '$app/state'
 	import ui from '@fat-fuzzy/ui'
 
-	const {PageRails, PageContext} = ui.content
+	const {PageRails} = ui.content
 	const {EscapeHtml} = ui.headless
 
 	let appContext = $derived(page.data.appContext)
 	let slide = $derived(page.data.content)
-	let title = $derived(slide.title)
-	let description = $derived(slide.description)
+	let title = $derived(slide.meta.title)
+	let description = $derived(slide.meta.description)
 	let html = $derived(slide.html)
 	let slug = $derived(slide.slug)
 	let series = $derived(
-		slide.series
-			? slide.series.items.map((id) =>
+		slide.meta.series
+			? slide.meta.series.items.map((id) =>
 					page.data.markdowns.find((slide) => slide.meta.id === id),
 				)
 			: null,
-	)
-	let seriesData = $derived(
-		series
-			? series.map((slide) => ({
-					link: slide.meta.slug,
-					title: slide.meta.subtitle,
-				}))
-			: undefined,
 	)
 </script>
 
@@ -37,23 +29,11 @@
 		path={page.url.pathname}
 		nav={page.data.nav}
 		app={appContext}
-		layout="metro"
+		layout="steam"
 	>
 		{#snippet main()}
-			<p
-				class="feedback:prose status:default bg:default:100 size:md variant:bare maki:block l:text:xl"
-			>
-				<span class="font:sm">{description}</span>
-			</p>
 			<EscapeHtml id={slug} {html} size="xl" />
 		{/snippet}
-		{#snippet aside()}
-			<PageContext
-				created={slide.date_created}
-				updated={slide.date_updated}
-				series={seriesData}
-				page={slide.index}
-			/>
-		{/snippet}
+		{#snippet aside()}{/snippet}
 	</PageRails>
 {/key}
