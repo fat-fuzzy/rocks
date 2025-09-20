7d72f17100ffd384b312d264a41c539c9d6d40e0
patiboh <patiboh@users.noreply.github.com>
2025-09-20 17:37:20 +0200

    🐛 [doc] Fix media page error handling

--- apps/doc/src/routes/(minimal)/media/[slug]/+page.server.ts
+++ apps/doc/src/routes/(minimal)/media/[slug]/+page.server.ts
@@ -4,21 +4,22 @@ import {actions as parentActions} from '../+page.server'
 
 export const load = async ({params}) => {
 	const {slug} = params
+	let imageData
 
 	try {
-		const imageData = await images.getImageData('media', slug)
-
-		if (!imageData?.json.sources) {
-			error(404, 'Not found')
-		}
-
-		return {
-			src: `/${imageData.json.path}/${slug}`,
-			...imageData.json,
-			sources: imageData.json.sources,
-		}
+		imageData = await images.getImageData('media', slug)
 	} catch (e) {
 		error(500, 'Error loading image data')
+	}
+
+	if (!imageData?.json.sources) {
+		error(404, 'Not found')
+	}
+
+	return {
+		src: `/${imageData.json.path}/${slug}`,
+		...imageData.json,
+		sources: imageData.json.sources,
 	}
 }
 
