c0032d2eaffc837dafac669f2d63e26759c4af8a
patiboh <patiboh@users.noreply.github.com>
2025-09-23 00:15:44 +0200

    🌟 [style+ui+doc] Enable splash image for presentations

--- apps/doc/src/routes/(minimal)/speaking/[talk]/+page.server.ts
+++ apps/doc/src/routes/(minimal)/speaking/[talk]/+page.server.ts
@@ -1,5 +1,6 @@
 import {error} from '@sveltejs/kit'
 import {commonActions} from '$lib/server/actions/page-actions'
+import images from '$data/images'
 
 export const load = async ({parent, params}) => {
 	const {talks} = await parent()
@@ -10,8 +11,28 @@ export const load = async ({parent, params}) => {
 		error(404, {message: 'Not found'})
 	}
 
+	let image
+	if (content.meta.image) {
+		try {
+			let imageData = await images.getImageData('media', content.meta.image)
+
+			if (!imageData?.json.sources) {
+				throw Error(`Image not found: ${JSON.stringify(content.meta.image)}:`)
+			}
+
+			image = {
+				src: `/${imageData.json.id}`,
+				...imageData.json,
+				sources: imageData.json.sources,
+			}
+		} catch (e) {
+			error(500, `Error loading image data: ${e}`)
+		}
+	}
+
 	return {
 		content,
+		image,
 	}
 }
 
--- apps/doc/src/routes/(minimal)/speaking/[talk]/[slug]/+page.server.ts
+++ apps/doc/src/routes/(minimal)/speaking/[talk]/[slug]/+page.server.ts
@@ -1,5 +1,6 @@
 import {error} from '@sveltejs/kit'
 import {commonActions} from '$lib/server/actions/page-actions'
+import images from '$data/images'
 
 /**
  * Load data from markdown file based on route parameters
@@ -9,20 +10,39 @@ import {commonActions} from '$lib/server/actions/page-actions'
 export const load = async ({params, parent}) => {
 	const {slug} = params
 	const {talks, speakerNotes} = await parent()
-	const markdown = talks.find(
+	const content = talks.find(
 		(v) => v.meta.status !== 'draft' && v.meta.slug === slug,
 	)
 	const notes = speakerNotes?.find((p) => p.meta.slug === slug)
 
-	if (!markdown) {
+	if (!content) {
 		error(404, 'Not found')
+	}
+
+	let image
+	// Load image data if an image is specified in the frontmatter
+	if (content.meta.image) {
+		try {
+			let imageData = await images.getImageData('media', content.meta.image)
+
+			if (!imageData?.json.sources) {
+				throw Error(`Image not found: ${JSON.stringify(content.meta.image)}:`)
+			}
+
+			image = {
+				src: `/${imageData.json.id}`,
+				...imageData.json,
+				sources: imageData.json.sources,
+			}
+		} catch (e) {
+			error(500, `Error loading image data: ${e}`)
+		}
 	}
 
 	return {
 		notes,
-		content: {
-			...markdown,
-		},
+		image,
+		content,
 	}
 }
 
