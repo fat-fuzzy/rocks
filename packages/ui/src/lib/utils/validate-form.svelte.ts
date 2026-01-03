4ca95b60e708dccdc31b7088ecbeabaa76c38f99
patiboh <patiboh@users.noreply.github.com>
2026-01-03 14:21:09 +0100

    🍱 [ui] Clean types, clean comments

--- packages/ui/src/lib/utils/validate-form.svelte.ts
+++ packages/ui/src/lib/utils/validate-form.svelte.ts
@@ -6,13 +6,14 @@ const {sanitize, validate} = validations
  * Use this Class to provide frontend validation capabilities to a form rendered by the server.
  * @param {string} formId The id of the <form> element
  * @param {FormData} formData The form data object
- * @param {string} validationFunctionName The name of the AJV validation function to use, generated using the package `@fat-fuzzy/validation` (`SignUpValidationFunction` is the default validation function for the sample SignUp form component). To Change the default validation function or add a new function, you must modify the `@fat-fuzzy/validation` package (TODO: define schemas in apps): add the schema of the form to `ajv.schema.forms.js` and rebuild the package
+ * @param {string} validationFunctionName The name of the AJV validation function to use (see package `@fat-fuzzy/validation`'s README)
  * @returns a validation class with utility methods to validate the form and fields, check and manage field statuses, and manage feedback messages
  */
 class FormValidator implements IFormValidator {
 	form: FormToValidate = $state({})
 	inputTypes: InputTypes = $state({}) // Map of input names to their types
 	errors: string[] = $state([])
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	ajvValidate: any = $state(() => ({}))
 
 	constructor(validationFunctionName: string) {
@@ -24,7 +25,11 @@ class FormValidator implements IFormValidator {
 
 	validationHandler() {
 		return {
-			set: (target: FormToValidate, prop: string, value: any) => {
+			set: (
+				target: FormToValidate,
+				prop: string,
+				value: FormDataEntryValue,
+			) => {
 				const sanitized = sanitize.sanitizeForm(prop, value, this.inputTypes)
 
 				if (sanitized) {
@@ -72,10 +77,10 @@ class FormValidator implements IFormValidator {
 	}
 
 	public validate() {
-		let fields = Object.keys(this.form)
+		const fields = Object.keys(this.form)
 		// Reset error states before validation
 		this.errors = []
-		fields.forEach((field: any) => {
+		fields.forEach((field: string) => {
 			if (this.form[field].changed) {
 				this.form[field].is_valid = true
 				this.form[field].feedback['error'] = undefined
@@ -88,15 +93,17 @@ class FormValidator implements IFormValidator {
 				validateMap[field] = this.form[field].value
 			}
 		})
-		let valid = this.ajvValidate(validateMap)
+		const valid = this.ajvValidate(validateMap)
 
 		if (!valid) {
 			this.errors = this.ajvValidate.errors
 		}
 
-		fields.forEach((name: any) => {
-			let inputErrors = this.errors
+		fields.forEach((name: string) => {
+			const inputErrors = this.errors
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
 				.filter((error: any) => error.instancePath.substring(1) === name)
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
 				.map((error: any) => error.message)
 			if (inputErrors.length) {
 				this.form[name].feedback['error'] = inputErrors
@@ -118,7 +125,7 @@ class FormValidator implements IFormValidator {
 	}
 
 	public getFieldErrors(name: string): string[] | undefined {
-		let errors = this.form[name]?.feedback['error']
+		const errors = this.form[name]?.feedback['error']
 		return errors
 	}
 
