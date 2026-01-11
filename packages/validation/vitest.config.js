c249ef8e51c20e480a7146c4d896d45f3171c504
patiboh <patiboh@users.noreply.github.com>
2026-01-11 22:49:14 +0100

    🔧 [validation] Setup vitest config

--- packages/validation/vitest.config.js
+++ packages/validation/vitest.config.js
@@ -2,6 +2,13 @@ import {defineConfig} from 'vitest/config'
 
 export default defineConfig({
 	test: {
-		// ...
+		reporters: ['html'],
+		include: ['src/**/*.{test,spec}.{js,ts}', 'tests/**/*.{test,spec}.{js,ts}'],
+		coverage: {
+			enabled: true,
+			provider: 'v8',
+			include: ['src/**/*.{js,ts}'],
+			exclude: ['src/**/definitions.{js,ts}', 'src/ajv/out/*.{js,ts}'],
+		},
 	},
 })
