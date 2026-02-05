ed903667e785b614e9c6fc0bdcec67b2fa6bf19a
patiboh <patiboh@users.noreply.github.com>
2026-02-05 08:48:30 +0100

    🌱 [ui] Clean ToggleMenuActor: factor out buildGroups fn, cleanup

--- packages/ui/src/lib/components/recipes/menus/ToggleMenu/actor.svelte.ts
+++ packages/ui/src/lib/components/recipes/menus/ToggleMenu/actor.svelte.ts
@@ -5,36 +5,18 @@ class ToggleMenuActor implements FuzzySystem {
 	groups: Map<string, Map<string, FuzzyPayload>> = $state(new Map())
 	mode = 'radio'
 
-	public init({mode, items}: {mode: string; items: ToggleProps[]}) {
+	public reset() {
+		this.state = new Map()
+		this.groups = new Map()
+		this.mode = 'radio'
+	}
+
+	public init({mode, items}: {mode?: string; items: ToggleProps[]}) {
 		if (mode) {
 			this.mode = mode
 		}
 
-		const gridMenuItems = items?.reduce(
-			(groups: Map<string, ToggleProps[]>, {id, label, group}) => {
-				let groupLabel = String(group)
-				if (!groupLabel) {
-					groupLabel = 'default'
-				}
-
-				let groupItems = groups.get(groupLabel)
-				if (!groupItems) {
-					groupItems = []
-				}
-				groupItems.push({
-					id,
-					label,
-					name: id,
-					group: id,
-					title: '',
-					value: id,
-				})
-
-				groups.set(groupLabel, groupItems)
-				return groups
-			},
-			new Map(),
-		)
+		const gridMenuItems = items?.reduce(this.buildGroups, new Map())
 
 		this.groups = new Map(
 			Array.from(gridMenuItems.entries()).map(([group, items]) => {
@@ -46,6 +28,42 @@ class ToggleMenuActor implements FuzzySystem {
 		this.state = this.buildStates(items)
 	}
 
+	/**
+	 * Helper function to build a suite of toggle menus by reducing them from a list of toggle props
+	 * @param groups accumulator for toggle menu groups
+	 * @param toggleProps current toggle prop to process
+	 * @returns a Map of toggle groups
+	 */
+	buildGroups(
+		groups: Map<string, ToggleProps[]>,
+		toggleProps: ToggleProps,
+	): Map<string, ToggleProps[]> {
+		const {id, label, group} = toggleProps
+		const groupLabel = group ? String(group) : 'default'
+
+		let groupItems = groups.get(groupLabel)
+		if (!groupItems) {
+			groupItems = []
+		}
+
+		groupItems.push({
+			id,
+			label,
+			name: id,
+			group: groupLabel,
+			title: '',
+			value: id,
+		})
+
+		groups.set(groupLabel, groupItems)
+		return groups
+	}
+
+	/**
+	 * Helper function to set up initial state for a toggle menu
+	 * @param items a list of toggle props
+	 * @returns a map of toggle menu items states
+	 */
 	buildStates(items: ToggleProps[]): Map<string, FuzzyPayload> {
 		return new Map(
 			items.map((item: ToggleProps) => [
@@ -56,7 +74,7 @@ class ToggleMenuActor implements FuzzySystem {
 	}
 
 	public getSelected(): FuzzyPayload[] {
-		let selected: FuzzyPayload[] = []
+		const selected: FuzzyPayload[] = []
 		this.state.forEach((item) => {
 			if (item.state === 'active') {
 				selected.push(item)
