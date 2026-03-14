ebe8669ccbc6486b9737897fe72e1597458d9592
patiboh <patiboh@users.noreply.github.com>
2026-03-14 16:05:14 +0100

    🧪 [ui] Add TextContext component to apply styles

---
+++ packages/ui/tests/browser/TestContext.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import '@fat-fuzzy/style/css'
+	import type {Snippet} from 'svelte'
+
+	let {children}: {children: Snippet} = $props()
+</script>
+
+{@render children()}
+
+<style>
+	*,
+	*::before,
+	*::after {
+		animation-duration: 0s !important;
+		animation-delay: 0s !important;
+		transition-duration: 0s !important;
+		transition-delay: 0s !important;
+	}
+</style>
