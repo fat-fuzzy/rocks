6e87bbe2c8c33da2c66fc327e61463c60cd56fe1
patiboh <patiboh@users.noreply.github.com>
2026-03-16 09:29:12 +0100

    🧪 [ui+playbook+prose+sketch+doc] Update & harmonize test configs

--- packages/prose/vite.config.ts
+++ packages/prose/vite.config.ts
@@ -3,8 +3,22 @@ import {sveltekit} from '@sveltejs/kit/vite'
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}'],
+		reporters: ['html'],
+		include: ['tests/unit/**/*.{test,spec}.{js,ts}'],
+		coverage: {
+			enabled: true,
+			provider: 'v8',
+			include: ['src/**/*.{js,ts}'],
+			exclude: [
+				'src/app.d.ts',
+				'src/**/browser/*.{js,ts}',
+				'src/**/definitions.{js,ts}',
+				'src/lib/types/*.{js,ts}',
+				'src/lib/**/*.browser.ts',
+				'src/lib/index.ts',
+				'src/lib/editor/',
+			],
+		},
 	},
 })
