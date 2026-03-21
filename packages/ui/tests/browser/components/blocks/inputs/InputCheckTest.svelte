66c647fd91e0952ef35ec652fcc2eba021d40f5e
patiboh <patiboh@users.noreply.github.com>
2026-03-21 09:16:40 +0100

    🧪 [ui] Add InputCheck test

---
+++ packages/ui/tests/browser/components/blocks/inputs/InputCheckTest.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import type {UiColor} from '$types'
+
+	import FormValidator from '$lib/utils/browser/FormValidator.svelte'
+
+	import InputCheck from '$lib/components/blocks/inputs/InputCheck.svelte'
+	import TestContext from '$tests/browser/TestContext.svelte'
+	import {INPUTS} from '$tests/fixtures/form-inputs'
+	import {page} from '$app/state'
+
+	let {id}: {id: string} = $props()
+
+	let validator = new FormValidator('TestFormValidationFunction')
+	let inputProps = $derived(INPUTS[id])
+
+	let isDisabled = $derived(inputProps.value.valid === 'disabled')
+	let color: UiColor = $derived(
+		validator.getFieldErrors(inputProps.name)?.length ? 'error' : 'primary',
+	)
+
+	function handleFocus(event: Event) {
+		validator.touchInput(event)
+	}
+
+	function handleBlur(event: Event) {
+		validator.validateInput(event)
+	}
+
+	function handleInput(event: Event) {
+		validator.changeInput(event)
+		validator.validateInput(event)
+	}
+</script>
+
+<TestContext>
+	<form data-testid="test-form" action={page.url.pathname}>
+		{validator.getFieldErrors(inputProps.name)}<InputCheck
+			{...inputProps}
+			name={inputProps.name}
+			id={`${inputProps.name}.${inputProps.value.valid}`}
+			value={inputProps.value.valid}
+			checked={false}
+			{validator}
+			disabled={isDisabled}
+			onfocus={handleFocus}
+			onblur={handleBlur}
+			oninput={handleInput}
+			{color}
+		/>
+	</form>
+</TestContext>
