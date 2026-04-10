013ee1598f4e9ce5e7673865da914d2bbca5d5fb
patiboh <patiboh@users.noreply.github.com>
2026-04-10 20:01:35 +0200

    ♻️ [prose] Move tags to generic menus snippet

--- packages/prose/src/lib/editor/EditorMenu.svelte
+++ packages/prose/src/lib/editor/EditorMenu.svelte
@@ -1,49 +1,88 @@
 <script lang="ts">
 	import type {UiColor, UiSize, UiVariant} from '@fat-fuzzy/ui'
+	import type {Snippet} from 'svelte'
 
 	import {Editor} from '@tiptap/core'
+	import ui from '@fat-fuzzy/ui'
+
 	import FontLevel from '$lib/editor/menus/FontLevel.svelte'
 	import FontStyle from '$lib/editor/menus/FontStyle.svelte'
 	import Links from '$lib/editor/menus/Links.svelte'
 	import Format from '$lib/editor/menus/Format.svelte'
-	import Unformat from '$lib/editor/menus/Unformat.svelte'
+	import Clear from '$lib/editor/menus/Clear.svelte'
 	import FlowControl from '$lib/editor/menus/FlowControl.svelte'
 
+	const {ToggleReveal} = ui.drafts
+
 	let {
+		id,
 		editor,
 		commands,
 		preset,
 		size = 'xs',
 		color,
 		variant,
+		menus,
 	}: {
+		id: string
 		editor: Editor
 		commands: {[key: string]: boolean}
 		preset?: string
 		size?: UiSize
 		color: UiColor
 		variant?: UiVariant
+		menus?: Snippet[]
 	} = $props()
 </script>
 
 <menu
-	class={`editor-menu l:flex size:3xs ravioli:2xs surface:1:${color} align:start justify:between`}
+	class={`editor-menu l:flex:3xs ravioli:3xs surface:1:${color} align:start relative`}
 >
-	<div class="w:full l:flex size:3xs justify:between">
-		<FontLevel {editor} {commands} {size} {color} {variant} />
+	<FontLevel {editor} {commands} {size} {color} {variant} />
 
-		<FontStyle {editor} {commands} {size} {color} {variant} />
+	<FontStyle {editor} {commands} {size} {color} {variant} />
 
-		<Format {editor} {commands} {size} {color} {variant} preset="full" />
+	<Format {editor} {commands} {size} {color} {variant} preset="full" />
 
-		<Links {editor} {commands} {size} {color} {variant} />
+	<Links {editor} {commands} {size} {color} {variant} />
 
-		<FlowControl {editor} {commands} {size} {color} {variant} />
-	</div>
+	<FlowControl {editor} {commands} {size} {color} {variant} />
 
-	{#if preset === 'full'}
-		<div class="w:full l:flex justify:end">
-			<Unformat {editor} {commands} {size} {color} {variant} />
-		</div>
+	{#if preset === 'full' || menus}
+		<ToggleReveal
+			id={`toggle-options-${id}`}
+			label="More"
+			asset="chevron-down"
+			assetType="svg"
+			{color}
+			size="3xs"
+			variant="bare"
+			shape="square"
+			layer="1"
+			layout="flex"
+			depth={0}
+			place="nord"
+			position="absolute"
+			align="center"
+			justify="end"
+		>
+			<div class="ravioli:3xs l:stack:3xs">
+				{#if preset === 'full'}
+					<Clear
+						{editor}
+						{commands}
+						{size}
+						{color}
+						{variant}
+						layout={menus ? 'flex' : undefined}
+					/>
+				{/if}
+				{#if menus}
+					{#each menus as children, i (i)}
+						{@render children()}
+					{/each}
+				{/if}
+			</div>
+		</ToggleReveal>
 	{/if}
 </menu>
---
+++ packages/prose/src/lib/editor/menus/Clear.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import type {UiColor, UiLayout, UiSize, UiVariant} from '@fat-fuzzy/ui'
+
+	import {Editor} from '@tiptap/core'
+
+	let {
+		editor,
+		commands,
+		size = 'xs',
+		color = 'primary',
+		variant = 'outline',
+		layout = 'stack',
+	}: {
+		editor: Editor
+		commands: {[key: string]: boolean}
+		size?: UiSize
+		color?: UiColor
+		variant?: UiVariant
+		layout?: UiLayout
+	} = $props()
+</script>
+
+<div>
+	<div class={`l:${layout}:${size} button-group`}>
+		<button
+			onclick={() => editor.chain().focus().unsetAllMarks().run()}
+			aria-pressed={commands.marks ? 'true' : undefined}
+			class={`toggle color:${color} variant:${variant} size:${size}`}
+		>
+			Clear Marks
+		</button>
+		<button
+			onclick={() => editor.chain().focus().clearNodes().run()}
+			aria-pressed={commands.nodes ? 'true' : undefined}
+			class={`toggle color:${color} variant:${variant} size:${size}`}
+		>
+			Clear Nodes
+		</button>
+	</div>
+</div>
