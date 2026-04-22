b7d432627fa258db8e46c7eefc962558a387e14f
patiboh <patiboh@users.noreply.github.com>
2026-04-22 11:03:56 +0200

    🔥 [ui] Cleanup

--- packages/ui/src/routes/tests/recipes/+page.svelte
+++ packages/ui/src/routes/tests/recipes/+page.svelte
@@ -1,76 +1,15 @@
-<script lang="ts">
-	import {tick} from 'svelte'
-	import {page} from '$app/state'
-
-	import FormValidator from '$lib/utils/browser/FormValidator.svelte'
-	import InputGroup from '$lib/components/blocks/inputs/InputGroup.svelte'
-	import {INPUTS} from '$tests/fixtures/form-inputs'
-
-	let validator = new FormValidator('TestFormValidationFunction')
-	let form: HTMLFormElement
-
-	const id = 'sample_checkbox_group_select_all'
-	const id2 = 'sample_checkbox_group'
-	let inputProps = $derived(INPUTS[id])
-	let inputProps2 = $derived(INPUTS[id2])
-
-	let items = $derived(
-		(INPUTS[id].items || []).map((i) => ({
-			...i,
-			name: inputProps.name,
-			disabled: false,
-			validator,
-		})),
-	)
-
-	let items2 = $derived(
-		(INPUTS[id2].items || []).map((i) => ({
-			...i,
-			name: inputProps2.name,
-			label: `${i.label} Bis`,
-			disabled: false,
-			validator,
-		})),
-	)
-
-	async function handleInput() {
-		await tick()
-		form.requestSubmit()
-	}
+<script>
+	import {resolve} from '$app/paths'
 </script>
 
-<form action={page.url.pathname} bind:this={form} class="l:stack:md">
-	<section class="l:stack align:start justify:between">
-		<div class="l:flex w:full size:2xs align:baseline justify:between">
-			<h3>Main Blocks</h3>
-		</div>
-		<div class="l:flex size:2xs align:start justify:between">
-			<InputGroup
-				id={INPUTS[id].name}
-				name={INPUTS[id].name}
-				legend={INPUTS[id].legend}
-				type="checkbox"
-				value={page.url.searchParams.getAll(INPUTS[id].name)}
-				size="2xs"
-				color="primary"
-				variant="outline"
-				{items}
-				oninput={handleInput}
-				{validator}
-			/>
-			<InputGroup
-				id={INPUTS[id2].name}
-				name={INPUTS[id2].name}
-				legend={INPUTS[id2].legend}
-				type="checkbox"
-				value={page.url.searchParams.getAll(INPUTS[id2].name)}
-				size="2xs"
-				color="primary"
-				variant="outline"
-				items={items2}
-				oninput={handleInput}
-				{validator}
-			/>
-		</div>
-	</section>
-</form>
+<main>
+	<h1 class="ravioli:2xl font:heading surface:1:neutral">Fat Fuzzy Tests</h1>
+	<ul>
+		<li><a href={resolve('/tests/recipes/tabs')}>recipes/tabs</a></li>
+		<li>
+			<a href={resolve('/tests/recipes/content/page-tabs')}>
+				recipes/content/page-tabs
+			</a>
+		</li>
+	</ul>
+</main>
