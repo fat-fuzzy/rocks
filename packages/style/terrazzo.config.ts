40028788e8d11055e6aa250e5d623e7686650a77
patiboh <patiboh@users.noreply.github.com>
2026-04-24 11:29:52 +0200

    🌱 [style] Clean imports, clean terrazzo config (todo ts issues)

--- packages/style/terrazzo.config.ts
+++ packages/style/terrazzo.config.ts
@@ -5,25 +5,28 @@ import pluginAssetsEmoji from './src/lib/tokens/plugins/assets-emoji.js'
 import pluginAssetsSvg from './src/lib/tokens/plugins/assets-svg.js'
 
 const paths = {
-	in: './src/lib/tokens/definitions/',
-	out: './src/lib/css/variables/',
+	in: './src/lib/tokens/definitions',
+	out: './src/lib/css/variables',
 }
 
 const files = [
+	'assets-emoji.json',
+	'assets-svg.json',
+	'scale.json',
+	'settings.json',
+	'font.json',
 	'border.json',
 	'color.json',
 	'icons.json',
 	'layout.json',
 	'pages.json',
-	'scale.json',
-	'font.json',
-	'settings.json',
-	'assets-emoji.json',
-	'assets-svg.json',
+	'motion.json',
 ]
 
+const tokens = files.map((file) => `${paths.in}/${file}`)
+
 export default defineConfig({
-	tokens: files.map((file) => `${paths.in}${file}`),
+	tokens: tokens,
 	plugins: [
 		pluginCSS({
 			transform(token) {
@@ -44,6 +47,25 @@ export default defineConfig({
 	],
 	outDir: paths.out,
 	lint: {
-		/** @see https://terrazzo.app/docs/cli/lint */
+		build: {enabled: true},
+		rules: {
+			'core/valid-color': 'error',
+			'core/valid-dimension': 'error',
+			'core/valid-font-family': 'error',
+			'core/valid-font-weight': 'error',
+			'core/valid-duration': 'error',
+			'core/valid-cubic-bezier': 'error',
+			'core/valid-number': 'error',
+			'core/valid-link': 'error',
+			'core/valid-boolean': 'error',
+			'core/valid-string': 'error',
+			'core/valid-stroke-style': 'error',
+			'core/valid-border': 'error',
+			'core/valid-transition': 'error',
+			'core/valid-shadow': 'error',
+			'core/valid-gradient': 'error',
+			'core/valid-typography': 'error',
+			'core/consistent-naming': 'warn',
+		},
 	},
 })
