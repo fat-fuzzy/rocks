ea88e0e2e0535f5a6513e7c50a25688c020984dc
patiboh <patiboh@users.noreply.github.com>
2026-04-27 19:24:37 +0200

    🧪 [validation] Update sanitization tests, form prop tests

--- packages/validation/tests/utils/constants.ts
+++ packages/validation/tests/utils/constants.ts
@@ -1,17 +1,12 @@
 const schemas = [
 	{
-		name: 'SignUp Form',
-		fn: 'SignUpValidationFunction',
-		properties: [
-			'sample_username',
-			'sample_email',
-			'sample_password',
-			'confirm_password',
-		],
+		name: 'Form',
+		fn: 'FormValidationFunction',
+		properties: ['username', 'email', 'password', 'confirm_password'],
 		valid: {
-			sample_username: 'a.6',
-			sample_email: 'a.6@example.com',
-			sample_password: 'ThisIsNotSecure!!!123',
+			username: 'a.6',
+			email: 'a.6@example.com',
+			password: 'ThisIsNotSecure!!!123',
 			confirm_password: 'ThisIsNotSecure!!!123',
 		},
 	},
