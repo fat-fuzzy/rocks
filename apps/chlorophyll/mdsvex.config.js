f0be4d7d385d57336f9ecc80b40e3babf7ceafea
patiboh <patiboh@users.noreply.github.com>
2026-08-02 18:57:04 +0200

    ➕ [chlorophyll] Add remark plugin script

--- apps/chlorophyll/mdsvex.config.js
+++ apps/chlorophyll/mdsvex.config.js
@@ -1,11 +1,12 @@
 import {defineMDSveXConfig as defineConfig} from 'mdsvex'
+import extractComments from './scripts/remark/extract-comments.js'
 
 const config = defineConfig({
 	extensions: ['.md', '.svx'],
 	smartypants: {
 		dashes: 'oldschool',
 	},
-	remarkPlugins: [],
+	remarkPlugins: [extractComments],
 	rehypePlugins: [],
 })
 
