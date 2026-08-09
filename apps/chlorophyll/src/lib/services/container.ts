3f39932fd86cd782944dfb50075f5810f47220fc
patiboh <patiboh@users.noreply.github.com>
2026-08-09 19:36:35 +0200

    🐛 [chlorophyll] Fix missing service

--- apps/chlorophyll/src/lib/services/container.ts
+++ apps/chlorophyll/src/lib/services/container.ts
@@ -5,8 +5,8 @@ import ExportService from '$lib/services/export-service'
 import PresetService from '$lib/services/storage/preset-service.svelte'
 
 export function createServices() {
-	const documentService = new DocumentService()
 	const seedService = new SeedService()
+	const documentService = new DocumentService()
 	const presetService = new PresetService()
 	const exportService = new ExportService()
 	const tagService = new TagService(documentService)
