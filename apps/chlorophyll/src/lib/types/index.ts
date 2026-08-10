6f2d80bd99f3c1cff669d307fb1cc029648aabc5
patiboh <patiboh@users.noreply.github.com>
2026-08-10 16:29:46 +0200

    🍱 [chlorophyll] Re-org type declarations: simplify

--- apps/chlorophyll/src/lib/types/index.ts
+++ apps/chlorophyll/src/lib/types/index.ts
@@ -1,11 +1,12 @@
-export * from '$lib/types/domain/ui'
-export * from '$lib/types/domain/actions'
-export * from '$lib/types/services/tag'
+export * from '$lib/types/actions'
+export * from '$lib/types/intl'
+export * from '$lib/types/ui'
+export * from '$lib/types/messages'
+
+// Storage
 export * from '$lib/types/storage/fs-base'
 export * from '$lib/types/storage/fs-storage'
 export * from '$lib/types/storage/fs-markdowns'
-export * from '$lib/types/transfer/messages'
-export * from '$lib/types/intl'
 
 // Service Interfaces
 export * from '$lib/types/services/seed'
@@ -34,7 +35,7 @@ export * from '$lib/generated/types/FormTag'
  ******* OVERRIDE duplicate generated types ******
  ********* ( src: always domain/identity ) *******/
 
-export type {Query} from '$lib/types/transfer/messages'
+export type {Query} from '$lib/types/messages'
 export type {Username, Date} from '$lib/generated/types/Human'
 export type {SeedType} from '$lib/generated/types/SeedDocument'
 export type {Title} from '$lib/generated/types/FormSection'
