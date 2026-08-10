0ba1a2969da15fd8452f8d7dbb1737c6a490abd9
patiboh <patiboh@users.noreply.github.com>
2026-08-10 13:19:03 +0200

    🧪 [chlorophyll] Setup basic Tag test

---
+++ apps/chlorophyll/tests/browser/ui/controls/TagsTest.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Tags from '$lib/ui/controls/tags/Tags.svelte'
+	import TestContext from '$tests/browser/TestContext.svelte'
+	import {TAGS_PROPS} from '$tests/fixtures/control-props'
+</script>
+
+<TestContext>
+	{#each TAGS_PROPS as { props }, i (i)}
+		<Tags {...props} />
+	{/each}
+</TestContext>
