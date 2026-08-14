89ce5c0c849fd810fff66e211df85f8c097edc8d
patiboh <patiboh@users.noreply.github.com>
2026-08-14 12:26:43 +0200

    🌈 [chlorophyll] Clean feedback: re-adjust color

--- apps/chlorophyll/src/lib/ui/editor/SectionEditor.svelte
+++ apps/chlorophyll/src/lib/ui/editor/SectionEditor.svelte
@@ -160,8 +160,8 @@
 						{@const subsectionIcon = tagsFound.length === 0 ? missingIcon : ''}
 
 						{#if subsections.length > 1}
-							<h3 class="font:bold raviolink shape:mellow maki:block">
-								<span class={`${subsectionIcon} maki:inline:md'`}>
+							<h3 class="raviolink shape:mellow maki:block surface:0:primary">
+								<span class={`${subsectionIcon} maki:inline:md font:heading`}>
 									{subsection.name}
 								</span>
 							</h3>
