1b8204cdd9028240df2c14d58a8caa392c51043d
patiboh <patiboh@users.noreply.github.com>
2026-08-15 10:59:18 +0200

    🚧 [chlorophyll] Enable add new language: add dialog & form

--- apps/chlorophyll/src/lib/ui/controls/settings/Settings.svelte
+++ apps/chlorophyll/src/lib/ui/controls/settings/Settings.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-	import type {UiColor, UiShape, UiVariant, InputProps} from '@fat-fuzzy/ui'
+	import type {UiColor, UiVariant, InputProps} from '@fat-fuzzy/ui'
 	import type {IDocService} from '$types'
 
 	import {getContext} from 'svelte'
 	import {page} from '$app/state'
 	import ui from '@fat-fuzzy/ui'
+
+	import DialogSaveLanguage from '$lib/ui/controls/settings/DialogSaveLanguage.svelte'
 
 	const {InputGroup} = ui.blocks
 
@@ -20,14 +22,15 @@
 
 	let docService: IDocService = getContext('docService')
 
+	let cta = $derived(page.params.page)
 	let base = $derived(docService.base)
 
-	let language = $derived.by(() => {
+	let currentLanguage = $derived.by(() => {
 		const lang = page.url.searchParams.get('language')
 		return lang ? [lang] : ['en']
 	})
 
-	let format = $derived.by(() => {
+	let currentFormat = $derived.by(() => {
 		const fmt = page.url.searchParams.get('format')
 		return fmt ? [fmt] : ['long']
 	})
@@ -44,7 +47,6 @@
 				label: i,
 				title: i,
 				variant: 'bare' as UiVariant,
-				shape: 'pill' as UiShape,
 			}
 		}),
 	)
@@ -61,7 +63,6 @@
 				label: i,
 				title: i,
 				variant: 'bare' as UiVariant,
-				shape: 'pill' as UiShape,
 			}
 		}),
 	)
@@ -72,25 +73,40 @@
 	}
 </script>
 
-<InputGroup
-	id="language"
-	name="language"
-	legend="Language"
-	value={language}
-	size="2xs"
-	{color}
-	{variant}
-	items={languageItems}
-	{oninput}
-/>
-<InputGroup
-	id="format"
-	name="format"
-	legend="Format"
-	value={format}
-	size="2xs"
-	{color}
-	{variant}
-	items={formatItems}
-	{oninput}
-/>
+<div class="l:stack">
+	<InputGroup
+		id="language"
+		name="language"
+		legend="Language"
+		value={currentLanguage}
+		size="2xs"
+		{color}
+		{variant}
+		items={languageItems}
+		{oninput}
+	/>
+
+	{#if cta === 'edit' || cta === 'build'}
+		<DialogSaveLanguage
+			id="cta-preset-add-new"
+			label="Add Language"
+			asset="plus"
+			assetType="svg"
+			cta="save"
+		/>
+	{/if}
+</div>
+<br />
+<div class="l:stack">
+	<InputGroup
+		id="format"
+		name="format"
+		legend="Format"
+		value={currentFormat}
+		size="2xs"
+		{color}
+		{variant}
+		items={formatItems}
+		{oninput}
+	/>
+</div>
