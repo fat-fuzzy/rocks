e5ae6a21d459f463f49c7cd9a51a1bb98cf436f0
patiboh <patiboh@users.noreply.github.com>
2026-08-26 11:45:42 +0200

    ♻️ [chlorophyll] Rename preview > compare, cleanup

--- apps/chlorophyll/src/lib/ui/controls/ContentActions.svelte
+++ apps/chlorophyll/src/lib/ui/controls/ContentActions.svelte
@@ -4,6 +4,7 @@
 	import {page} from '$app/state'
 	import {resolve} from '$app/paths'
 
+	import {CTA_TO_ACTION} from '$lib/intl/l10n'
 	import MenuSections from '$lib/ui/controls/section/MenuSections.svelte'
 	import MenuSettings from '$lib/ui/controls/settings/MenuSettings.svelte'
 	import MenuData from '$lib/ui/controls/data/MenuData.svelte'
@@ -14,20 +15,13 @@
 	let cta = $derived(page.params.page)
 	let query = $derived(page.url.search)
 
-	const CTA_OPTIONS = {
-		edit: 'Edit',
-		build: 'Build',
-		preview: 'Preview',
-		print: 'Print',
-	}
-
 	let linkStyles = $state('font:xs font:semibold font:heading w:full')
 </script>
 
 <div class="l:stack maki:inline:4xs">
 	<nav id="secondary-nav">
 		<ul class={`unstyled input-group l:${layout}:3xs w:full justify:between`}>
-			{#each Object.entries(CTA_OPTIONS) as [key, value], i (i)}
+			{#each Object.entries(CTA_TO_ACTION) as [key, value], i (i)}
 				{@const classes =
 					key === cta ? linkStyles : `${linkStyles} ink:primary`}
 				<li
@@ -50,7 +44,7 @@
 		</div>
 		<div class="l:flex:2xs justify:between hug">
 			<MenuSettings {oninput} color="accent" variant="outline" />
-			<MenuData id="button-import" label="Data" color="accent" {oninput} />
+			<MenuData id="button-import" color="accent" />
 		</div>
 	</div>
 </div>
